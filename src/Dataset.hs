4e5fa250229c0efc978e6f298e15dd11c87308ba
Han Joosten <han.joosten.han@gmail.com>
2009-03-28 14:50:42 +0000

    In Dataset.hs zit volgens mij een fout. Moet Stef maar eens even naar kijken.
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@287 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Dataset.hs
+++ src/Dataset.hs
@@ -1,3 +1,4 @@
+  {-# XXXOPTIONS_GHC -Wall #-}
   module Dataset (makeDataset
                  ,makeDatasets
                  ,datasetMor)
@@ -6,20 +7,39 @@
    import Adl
 
 {- Datasets zijn bedoeld voor functiepuntentellingen en voor mogelijke efficiency-redenen in SQL-implementaties.
-   Ze brengen een aantal relaties bijeen die zich als ��n SQL-tabel laten implementeren.
+   Ze brengen een aantal relaties bijeen die zich als één SQL-tabel laten implementeren.
    Om praktische redenen krijgt een dataset geen eigen zelfstandige type, maar wordt het weergegeven als een ObjectDef.
    De volgende drie functies, makeDataset, makeDatasets en datasetMor, horen bij elkaar
    en moeten onderling consistent blijven.
 -}
 
+ {- Bericht aan Stef van Han:
+    In onderstaande codes wordt momenteel erg veel variabelen geshadowd. Dit maakt de code ondoorzichtig, en
+    het toepassen er van zorgt ook voor fouten, zoals onderstaand voorbeeld volgens mij aantoont. 
+    Het voorkomen van shadowing is eenvoudig: Geef binnen de scope van een variabele v elke andere variabele een naam die afwijkt van de naam van v. 
+    Het pragma bovenaan deze module kan je hiermee helpen. (even de XXX weghalen en de pragma werkt...) Succes!
+ -}
+
    makeDataset :: Context -> Concept -> ObjectDef
-   makeDataset context c
-    = Obj (name c) posNone (v (one,c)) [Obj (name m++name(target m)) posNone (Tm m) [] []| m<-dss] []
+   makeDataset context c    -- TODO FOUT?? WAAROM Stef, de c wordt niet gebruikt. Ik kan me niet voorstellen dat dat goed is.....
+    = Obj { objnm   = name c
+          , objpos  = posNone
+          , objctx  = v (one,c)
+          , objats  = [Obj { objnm   = name mph++name(target mph)
+                           , objpos  = posNone
+                           , objctx  = Tm mph
+                           , objats  = []
+                           , objstrs = []
+                           }| mph<-dss]
+          , objstrs = []
+          }
+--was:    = Obj (name c) posNone (v (one,c)) [Obj (name m++name(target m)) posNone (Tm m) [] []| m<-dss] []
       where
        c  = minimum [g|g<-concs context,g<=head cl]
-       cl = head ([cl| cl<-eqClass bi (concs context), c `elem` cl]++error ("!Fatal (module Fspec>dataset): cannot determine dataset for concept "++name c))
-       c `bi` c' = not (null [m| m<-declarations context, isFunction m, isFunction (flp m)
-                               , source m<=c && target m<=c'  ||  source m<=c' && target m<=c])
+       cl = head ([cl'| cl'<-eqClass bi (concs context), c `elem` cl']
+              ++error ("!Fatal (module Fspec>dataset): cannot determine dataset for concept "++name c))
+       c `bi` c' = not (null [mph| mph<-declarations context, isFunction mph, isFunction (flp mph)
+                               , source mph<=c && target mph<=c'  ||  source mph<=c' && target mph<=c])
        dss = [     makeMph d | d<-declarations context, isFunction      d , source d `elem` cl]++
              [flp (makeMph d)| d<-declarations context, isFunction (flp d), target d `elem` cl]
 
--- src/Version.hs
+++ src/Version.hs
@@ -6,5 +6,5 @@ versionbanner :: String
 versionbanner = "ADL vs. 0.8.10-285" -- the number behind the last digit is the SVN revision number.
 
 {- SVN Version text:
-Aanpassing aan options: Per optie kan nu worden aangegeven of die zichtbaar is voor de gebruiker of niet.
+In Dataset.hs zit volgens mij een fout. Moet Stef maar eens even naar kijken.
 -}
