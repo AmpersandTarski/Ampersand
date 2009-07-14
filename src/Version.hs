2cce13f92535dd71b893dba207c1b592325cd300
Stef Joosten <stef.joosten@ou.nl>
2009-07-14 21:36:34 +0000

    Test bestand "OUNL.adl" toegevoegd
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@344 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -3,8 +3,8 @@ module Version (versionbanner)
 where
 
 versionbanner :: String
-versionbanner = "ADL vs. 0.8.10-342" -- the number behind the last digit is the SVN revision number.
+versionbanner = "ADL vs. 0.8.10-344" -- the number behind the last digit is the SVN revision number.
 
 {- SVN Version text:
-makeFdecl verhuisd van context naar Fspc.
+Test bestand "OUNL.adl" toegevoegd
 -}
