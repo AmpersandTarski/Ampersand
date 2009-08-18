91d02dc623291fcf498967d405c116d892987495
Sebastiaan Joosten <sjcjoosten@gmail.com>
2009-08-18 10:47:19 +0000

    Version.hs ...
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@379 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -3,10 +3,8 @@ module Version (versionbanner)
 where
 
 versionbanner :: String
-versionbanner = "ADL vs. 0.8.10-377" -- the number behind the last digit is the SVN revision number.
+versionbanner = "ADL vs. 0.8.10-378" -- the number behind the last digit is the SVN revision number.
 
 {- SVN Version text:
-Kleine bug verwijderd uit Options.hs.
-Het syptoom was: als er geen output redirection plaatsvindt, maar de huidige directory-string bevat per ongeluk een spatie, dan ging het hartstikke fout omdat de string met de huidige directory als twee argumenten wordt opgevat.
-Oplossing was: als er geen output redirection plaatsvindt, dan wordt (in regel 75 van Options.hs) dirOutput = ""    (in plaats van dirOutput = currdir)
+Grote verandering: nieuwe interface.. Voortaan eerst Installer.php draaien
 -}
