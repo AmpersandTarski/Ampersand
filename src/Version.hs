399c25ad74480d9fe5dc92aa66b63ac3c0b41222
Sebastiaan Joosten <sjcjoosten@gmail.com>
2009-08-19 11:02:58 +0000

    Gezorgd dat de TAB ook in Firefox werkt, en dat de Save knop beter werkt (veranderingen in edit.js).
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@386 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -3,8 +3,8 @@ module Version (versionbanner)
 where
 
 versionbanner :: String
-versionbanner = "ADL vs. 0.8.10-384" -- the number behind the last digit is the SVN revision number.
+versionbanner = "ADL vs. 0.8.10-385" -- the number behind the last digit is the SVN revision number.
 
 {- SVN Version text:
-Eerste twee foutjes die Stef al snel gevonden had opgelost: getId() en PHP_SELF fouten
+Gezorgd dat de TAB ook in Firefox werkt, en dat de Save knop beter werkt (veranderingen in edit.js).
 -}
