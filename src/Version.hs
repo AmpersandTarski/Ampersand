de1b32739df86458b41e5681117c1de0f65a5e1d
Stef Joosten <stef.joosten@ou.nl>
2009-10-11 20:57:09 +0000

    Brede tabellen zijn nu ingevoerd.
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@442 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -3,11 +3,8 @@ module Version (versionbanner)
 where
 
 versionbanner :: String
-versionbanner = "ADL vs. 0.8.10-441" -- the number behind the last digit is the SVN revision number.
+versionbanner = "ADL vs. 0.8.10-442" -- the number behind the last digit is the SVN revision number.
 
 {- SVN Version text:
-Ik heb nieuwe plugs gedefinieerd, maar nog niet geactiveerd. Bas, even kijken in ADL2Fspec.hs.
-Vlak boven de definitie van "allplugs" heb ik een definitie van "newplugs" neergezet.
-Als ik newplugs gebruik in plaats van allplugs (allplugs wordt oldplugs en newplugs wordt allplugs...)
-dan krijg ik rommel in de database.
+Brede tabellen zijn nu ingevoerd.
 -}
