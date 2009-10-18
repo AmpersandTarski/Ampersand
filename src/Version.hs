1299b44611a375e9b64e6ea6de6bfea0054c6feb
Stef Joosten <stef.joosten@ou.nl>
2009-10-18 10:26:39 +0000

    Foutje met edit-functionaliteit in de prototypes opgelost.
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@451 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -3,8 +3,8 @@ module Version (versionbanner)
 where
 
 versionbanner :: String
-versionbanner = "ADL vs. 0.8.10-449" -- the number behind the last digit is the SVN revision number.
+versionbanner = "ADL vs. 0.8.10-451" -- the number behind the last digit is the SVN revision number.
 
 {- SVN Version text:
-Schoonheidsfoutje bij generatie van plaatjes met --crowfoot opgelost. 
+Foutje met edit-functionaliteit in de prototypes opgelost.
 -}
