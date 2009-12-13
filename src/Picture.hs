bf16677ab4cb53b06936b7edc19014f8877d5dc0
Han Joosten <han.joosten.han@gmail.com>
2009-12-13 22:53:51 +0000

    Plaatjes weer met neato, zodat ze er fraaier uit komen te zien.
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@485 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Picture.hs
+++ src/Picture.hs
@@ -12,6 +12,7 @@ data Picture = Pict { reference :: String    -- used to reference the picture in
                     , dotSource :: String    -- the string representing the .dot 
                     , fullPng   :: FilePath  --the full file path where the .png file resides
                     , fullDot   :: FilePath  --the full file path where the .dot file resides
+                    , dotProgName :: String  -- the name of the program to use  ("dot" or "neato" )
                     , figlabel  :: String    -- the label of a picture
                     , title     :: String    -- a human readable name of this picture
                     }
@@ -28,6 +29,11 @@ makePicture flags name pTyp dotsource
            , fullDot    = replaceExtension fullName "dot"
            , fullPng    = replaceExtension fullName "png"
            , figlabel   = "fig:" ++ cdName
+           , dotProgName = case pTyp of
+                     PTClassDiagram -> "dot"
+                     PTPattern      -> "neato"
+                     PTConcept      -> "neato"
+                     PTSwitchBoard  -> "neato"
            , title      = case (pTyp,language flags) of
                             (PTClassDiagram,English) -> "Class Diagram of " ++ name
                             (PTClassDiagram,Dutch  ) -> "Klassediagram van " ++ name
@@ -51,8 +57,8 @@ writePicture :: Options -> Picture -> IO()
 writePicture flags pict
     = do verboseLn flags ("Generating picture of "++title pict++" ... :")
          writeFile (fullDot pict) (dotSource pict)
-         verboseLn flags   ("dot -Tpng "++(fullDot pict)++" -o "++(fullPng pict))
-         result <- system $ "dot -Tpng "++(fullDot pict)++" -o "++(fullPng pict)
+         verboseLn flags   (dotProgName pict++" -Tpng "++(fullDot pict)++" -o "++(fullPng pict))
+         result <- system $ (dotProgName pict)++" -Tpng "++(fullDot pict)++" -o "++(fullPng pict)
          case result of 
              ExitSuccess   -> verboseLn flags (fullPng pict++" written.")
              ExitFailure x -> putStrLn ("Failure: " ++ show x)         
--- src/Version.hs
+++ src/Version.hs
@@ -3,8 +3,8 @@ module Version (versionbanner)
 where
 
 versionbanner :: String
-versionbanner = "ADL vs. 0.8.10-479" -- the number behind the last digit is the SVN revision number.
+versionbanner = "ADL vs. 0.8.10-485" -- the number behind the last digit is the SVN revision number.
 
 {- SVN Version text:
-Foutje uit Picture.hs gehaald. De plaatjes zien er wel raar uit, maar dat fix ik binnenkort wel even. Ik weet wat er aan de hand is.
+Plaatjes weer met neato, zodat ze er fraaier uit komen te zien. 
 -}
