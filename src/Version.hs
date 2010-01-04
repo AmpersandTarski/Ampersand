c9f7b8124548208ad9dba41d8fe89e62db060c4b
Gerard Michels <gerard.michels@ou.nl>
2010-01-04 15:19:24 +0000

    Atlas dingtjes.
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@516 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -3,9 +3,9 @@ module Version (versionbanner)
 where
 
 versionbanner :: String
-versionbanner = "ADL vs. 0.8.10-515" -- the number behind the last digit is the SVN revision number.
+versionbanner = "ADL vs. 0.8.10-516" -- the number behind the last digit is the SVN revision number.
 
 {-
 SVN Version text:
-Kleine aanpassing aangebracht rond genereren andere documentformaten.
+Atlas dingtjes.
 -}
