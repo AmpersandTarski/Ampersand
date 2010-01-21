e2ffde9e2b74b903db357525d2a49f43fd3fa154
Gerard Michels <gerard.michels@ou.nl>
2010-01-21 15:47:08 +0000

    atlas: violations, signals
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@556 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Atlas/Atlas.hs
+++ src/Atlas/Atlas.hs
@@ -27,12 +27,15 @@ data ATableId =
   |ATConcept
   |ATContains
   |ATContainsConcept
+  |ATContainsExpr
+  |ATContainsSignal
   |ATExplanation
---  |ATExpression
+  |ATSubExpression
   |ATHomoRule
   |ATIsa
   |ATPicture
   |ATMorphisms
+  |ATMorphismsSignal
   |ATMultRule
   |ATPair
   |ATPattern
@@ -41,6 +44,7 @@ data ATableId =
   |ATRelVar
   |ATRule
   |ATService
+  |ATSignal
   |ATType
   |ATUserRule
   |ATViolRule
@@ -54,12 +58,15 @@ tables =
    ,ATable ATConcept "concept" ["i","user","script","display"] 
    ,ATable ATContains "contains" ["relation","pair"] 
    ,ATable ATContainsConcept "containsconcept" ["concept","atom"] 
+   ,ATable ATContainsExpr "containssubexpression" ["subexpression","pair"] 
+   ,ATable ATContainsSignal "containssignal" ["signal","pair"] 
    ,ATable ATExplanation "explanation" ["i","user","script","display"] 
-  -- ,ATable ATExpression "expression" ["i","source","target"] 
+   ,ATable ATSubExpression "subexpression" ["i","subexpressionof","user","script","display"] 
    ,ATable ATHomoRule "homogeneousrule" ["i","property","on","type","explanation","pattern","user","script","display"] 
    ,ATable ATIsa "isarelation" ["i","specific","general","pattern","user","script","display"] 
    ,ATable ATPicture "picture" ["i","user","script","display"] 
    ,ATable ATMorphisms "morphisms" ["userrule","relation"] 
+   ,ATable ATMorphismsSignal "morphismssignal" ["signal","relation"] 
    ,ATable ATMultRule "multiplicityrule" ["i","property","on","type","explanation","pattern","user","script","display"] 
    ,ATable ATPair "pair" ["i","user","script","display"] 
    ,ATable ATPattern "pattern" ["i","picture","user","script","display"] 
@@ -68,6 +75,7 @@ tables =
    ,ATable ATRelVar "relvar" ["relation","type"]
    ,ATable ATRule "rule" ["i","type","explanation","pattern","user","script","display"] 
    ,ATable ATService "service" ["i","picture","user","script","display"] 
+   ,ATable ATSignal "signal" ["i","type","explanation","pattern","next","previous","user","script","display"] 
    ,ATable ATType "type" ["i","source","target","user","script","display"] 
    ,ATable ATUserRule "userrule" ["i","type","explanation","picture","pattern","next","previous","user","script","display"] 
    ,ATable ATViolRule "violates" ["violation","rule"]
@@ -184,13 +192,16 @@ insertpops conn fSpec flags (tbl:tbls) pics =
    pop' ATAtom = [[x]|(_,x)<-cptsets]
    pop' ATConcept = [[name x]|x<-cpts]
    pop' ATContains = [[relpred x,show y]| x<-declarations fSpec,decusr x, y<-contents x]
-   pop' ATContainsConcept = [[x,y]|(x,y)<-cptsets]
+   pop' ATContainsConcept = [[x,y]|(x,y)<-cptsets] 
+   pop' ATContainsExpr = [[cptexpr x,show y]| vr<-violateduserrules, x<-subexprs vr, y<-contents x]
+   pop' ATContainsSignal = [[cptrule x,show y]| x<-signalrules, y<-contents (Cp$ruleexpr x)]
    pop' ATExplanation = [[explainRule flags x]|x<-atlasrules]
- --  pop' ATExpression = [] --TODO - generalisation must be fixed first in -p of atlas
+   pop' ATSubExpression = [[cptexpr y,cptrule x]|x<-violateduserrules, y<-subexprs x] 
    pop' ATHomoRule = [(\(Just (p,d))->[cptrule x,show p,relpred d,cpttype x,explainRule flags x,r_pat x])$rrdcl x |x@Ru{}<-homorules]
    pop' ATIsa = [[show x,show(genspc x), show(gengen x),name p]|p<-patterns fSpec, x<-gens p]
    pop' ATPicture = [[x]|(_,x)<-pics]
    pop' ATMorphisms = [[cptrule x, mphpred y]|x<-userrules, y<-mors x]
+   pop' ATMorphismsSignal = [[cptrule x, mphpred y]|x<-signalrules, y<-mors x]
    pop' ATMultRule = [(\(Just (p,d))->[cptrule x,show p,relpred d,cpttype x,explainRule flags x,r_pat x])$rrdcl x |x@Ru{}<-multrls]
    pop' ATPair = [[show y]| x<-declarations fSpec,decusr x, y<-contents x]
    pop' ATPattern = [[name x,pic]| x<-patterns fSpec,(PicPat pn,pic)<-pics, pn==name x]
@@ -199,38 +210,49 @@ insertpops conn fSpec flags (tbl:tbls) pics =
    pop' ATRelVar = [[relpred x,cpttype x]|x<-declarations fSpec,decusr x]
    pop' ATRule = [[cptrule x,cpttype x,explainRule flags x,r_pat x]|x<-atlasrules]
    pop' ATService = [[name fSpec,x]|(PicFS,x)<-pics]
+   pop' ATSignal = [[cptrule x,cpttype x,explainRule flags x,r_pat x,cptrule$nextrule x signalrules,cptrule$prevrule x signalrules]|x<-signalrules]
    pop' ATType = [t x|x<-declarations fSpec,decusr x] ++ [t x|x<-atlasrules]
         where t x = [cpttype x, name$source x, name$target x]
-   pop' ATUserRule = [[cptrule x,cpttype x,explainRule flags x,pic,r_pat x,cptrule$prevrule x userrules,cptrule$nextrule x userrules]|x<-userrules,(PicRule rn,pic)<-pics, rn==name x]
-   --convert pair to violation message
-   --There is overhead in the violates* tables, but that does not matter for the result. Fix SQL and generalisation instead.
-   --then there will be only one table "violates" for all rules.
+   pop' ATUserRule = [[cptrule x,cpttype x,explainRule flags x,pic,r_pat x,cptrule$nextrule x userrules,cptrule$prevrule x userrules]|x<-userrules,(PicRule rn,pic)<-pics, rn==name x]
    pop' ATViolRule = [[ y, x] | (x,y)<-identifiedviols]
-   pop' ATViolHomoRule = [[ y, x] | (x,y)<-identifiedviols]
-   pop' ATViolMultRule = [[ y, x] | (x,y)<-identifiedviols]
-   pop' ATViolUserRule = [[ y, x] | (x,y)<-identifiedviols]
+   pop' ATViolHomoRule = [[ y, x] | (x,y)<-identifiedviols, elem x (map cptrule homorules)]
+   pop' ATViolMultRule = [[ y, x] | (x,y)<-identifiedviols, elem x (map cptrule multrls)]
+   pop' ATViolUserRule = [[ y, x] | (x,y)<-identifiedviols, elem x (map cptrule userrules)]
    pop' ATViolation = [[ y] | (_,y)<-identifiedviols]
    --------------------------------------------------------
    --picturelink =  "./img/" ++ name fSpec++".png"
-   identifiedviols = [(cptrule x,show i++show y) |(i,(x,y))<-zip naturals (violations fSpec)]
+   identifiedviols = [(cptrule x,"violation"++show i++" "++show y) |(i,(x,y))<-zip naturals (violations fSpec)]
      where naturals :: [Integer]
            naturals = [1..]
+   violateduserrules = [r|r<-userrules, elem (cptrule r) [x|(x,_)<-identifiedviols]]
+   subexprs x | rrsrt x==Implication = [rrant x,rrcon x]
+              | rrsrt x==Equivalence = [rrant x,rrcon x]
+              | rrsrt x==Truth = [rrcon x]
+              | otherwise = []
+   ruleexpr x | rrsrt x==Implication = Fu [Cp(rrant x), rrcon x]
+              | rrsrt x==Equivalence = Fi [Fu [Cp(rrant x), rrcon x], Fu [Cp(rrcon x), rrant x]]
+              | rrsrt x==Truth = rrcon x
+              | otherwise = v (NOthing,NOthing) 
    nextrule r [] = r
-   nextrule r rs = if null nxt then head rs else head nxt 
-      where nxt = [r'|r'<-rs, runum r'==(runum r)+1]
+   nextrule r (_:[]) = r
+   nextrule r (r':r'':rs) | runum r'==runum r = r'' 
+                      | otherwise = nextrule r (r'':rs)
+   --prevrule _ rs = error$show [runum r|r<-rs]
    prevrule r [] = r
-   prevrule r rs = if null prev then last rs else head prev 
-      where prev = [r'|r'<-rs, runum r'==(runum r)-1]
+   prevrule r (_:[]) = r
+   prevrule r (r':r'':rs) | runum r''==runum r = r' 
+                          | otherwise = prevrule r (r'':rs)
    relpred x@(Sgn{}) = name x ++ "::" ++ cpttype x 
    relpred x = name x
    mphpred x@(Mph{}) = relpred (mphdcl x) 
    mphpred x = name x
+   cptexpr = showADLcode fSpec 
    cpts = (\(Isa isas cs) -> rd$[c|c@(C{})<-cs]++[c|(c,_)<-isas]++[c|(_,c)<-isas]) (isa fSpec)
    cptsets = [(name c,x)|c@(C{})<-cpts, x<-cptos c]
    cptrule x | isSignal x =  "SIGNAL: " ++ (cptrule$ x{r_sgl=False})
-             | rrsrt x==Implication = showADLcode fSpec (rrant x) ++ " |- " ++ showADLcode fSpec (rrcon x)
-             | rrsrt x==Equivalence = showADLcode fSpec (rrant x) ++ " = " ++ showADLcode fSpec (rrcon x)
-             | rrsrt x==Truth = showADLcode fSpec (rrcon x)
+             | rrsrt x==Implication = cptexpr (rrant x) ++ " |- " ++ cptexpr (rrcon x)
+             | rrsrt x==Equivalence = cptexpr (rrant x) ++ " = " ++ cptexpr (rrcon x)
+             | rrsrt x==Truth = cptexpr (rrcon x)
              | otherwise = []
    cpttype x = name(source x)++"*"++(name$target x)
    --DESCR -> userrules are user-defined rules, 
@@ -239,12 +261,13 @@ insertpops conn fSpec flags (tbl:tbls) pics =
    --         the rule from an ISA declaration (I[spec] |- I[gen]) is not presented as a rule in the atlas
    --TODO -> key rules (they have been put in pattern "")
    --TODO -> rulefromProp has not been type inferred, p.e. generates I[Anything]. Will there be violations on them in fSpec????? 
-   atlasrules = userrules ++ multrls ++ homorules
-   userrules = sort [x|x@Ru{}<-rules fSpec++signals fSpec, rrdcl x==Nothing, not (isIsaRule x), not(r_pat x=="")]
+   atlasrules = userrules ++ multrls ++ homorules ++ signalrules
+   userrules = sort [x|x@Ru{}<-rules fSpec, rrdcl x==Nothing, not (isIsaRule x), not(r_pat x=="")]
       where 
       isIsaRule x = rrsrt x==Implication && (isI$rrant x) && (isI$rrcon x)
       isI (Tm (I{})) = True
       isI _ = False
+   signalrules =  sort [x|x<-signals fSpec, not(r_pat x=="")]
    multrls = [rulefromProp p d |d<-declarations fSpec, p<-multiplicities d, elem p [Uni,Tot,Inj,Sur]] 
    homorules =  [rulefromProp p d|d<-declarations fSpec, p<-multiplicities d, elem p [Rfx,Sym,Asy,Trn] ]
 
--- src/Version.hs
+++ src/Version.hs
@@ -3,9 +3,9 @@ module Version (versionbanner)
 where
 
 versionbanner :: String
-versionbanner = "ADL vs. 0.8.10-554" -- the number behind the last digit is the SVN revision number.
+versionbanner = "ADL vs. 0.8.10-556" -- the number behind the last digit is the SVN revision number.
 
 {-
 SVN Version text:
-Changed place of FPA.hs. & got rid of FspecDef.hs
+Atlas dingen.
 -}
