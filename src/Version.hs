266951a5c7950eeb1dab586809e4c6e15a28f668
Han Joosten <han.joosten.han@gmail.com>
2010-01-27 12:52:46 +0000

    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@564 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -3,7 +3,7 @@ module Version (versionbanner)
 where
 
 versionbanner :: String
-versionbanner = "ADL vs. 0.8.10-562" -- the number behind the last digit is the SVN revision number.
+versionbanner = "ADL vs. 0.8.10-564" -- the number behind the last digit is the SVN revision number.
 
 {-
 SVN Version text:
