865774aed9761c390dfbf598068a5f81b42608c4
Gerard Michels <gerard.michels@ou.nl>
2010-03-16 10:17:38 +0000

    Atlas opnieuw gegenereerd met vernieuwde plugs
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@640 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -6,8 +6,8 @@ versionbanner :: String
 versionbanner = "ADL vs. "++versionNumber 
 
 versionNumber :: String
-versionNumber = "1.1-639" -- #1.#2-#3 : #1 major version; #2 student release version; #3 SVN revision number.
+versionNumber = "1.1-640" -- #1.#2-#3 : #1 major version; #2 student release version; #3 SVN revision number.
 {-
 SVN Version text:
-Fouten in SQLplugs verholpen. Er wordt nu een applicatie gegenereerd, maar deze is allerminst foutvrij!
+Atlas opnieuw gegenereerd met vernieuwde plugs
 -}
