c86ffe08b26db0fa3d499f9ad85ba0cd74a8532c
Han Joosten <han.joosten.han@gmail.com>
2010-04-18 14:07:05 +0000

    ShowHS verder uitgewerkt. Begin gemaakt om ook inferentie bomen te kunnen showHSsen.
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@655 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -6,8 +6,8 @@ versionbanner :: String
 versionbanner = "ADL vs. "++versionNumber 
 
 versionNumber :: String
-versionNumber = "1.1-653" -- #1.#2-#3 : #1 major version; #2 student release version; #3 SVN revision number.
+versionNumber = "1.1-655" -- #1.#2-#3 : #1 major version; #2 student release version; #3 SVN revision number.
 {-
 SVN Version text:
-ShowHS zo goed mogelijk neergezet. Er ontbreken nog de berekende signaaldeclaraties in de Fspc. Klopt dat, Stef?
+ShowHS verder uitgewerkt. Begin gemaakt om ook inferentie bomen te kunnen showHSsen. 
 -}
