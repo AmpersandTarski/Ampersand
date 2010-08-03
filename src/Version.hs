db4209e9d191db4656d1d8cf499bf33ab7e6690c
Rieks Joosten <Rieks.Joosten@tno.nl>
2010-08-03 14:22:48 +0000

    Fout van Han gecorrigeerd
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@695 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -6,7 +6,7 @@ versionbanner :: String
 versionbanner = "ADL vs. "++versionNumber 
 
 versionNumber :: String
-versionNumber = "1.1.0.689" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
+versionNumber = "1.1.0.690" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
 {-
 SVN Version text:
 Some minor changes in document generation. 
