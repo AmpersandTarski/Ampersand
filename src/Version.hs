b02a1fd5b40f401f69af8083e6cc29c210619b05
Sebastiaan Joosten <sjcjoosten@gmail.com>
2010-08-12 19:31:01 +0000

    Version.hs
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@705 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -6,8 +6,8 @@ versionbanner :: String
 versionbanner = "ADL vs. "++versionNumber 
 
 versionNumber :: String
-versionNumber = "1.1.0.701" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
+versionNumber = "1.1.0.705" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
 {-
 SVN Version text:
-Class 'Explainable' geintroduceerd. Dit geeft vrijheid om eventueel semantisch verschillende vormen van explain-achtige functies te introduceren. 
+Code.hs en derivaten in de code gehangen (er wordt nu daadwerkelijk gebruik van gemaakt, al valt dit niet direct op)
 -}
