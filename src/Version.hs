52115588bb315c0b8e31853e9bb93a71abe86308
Sebastiaan Joosten <sjcjoosten@gmail.com>
2010-08-22 20:49:22 +0000

    Plug datatypes zijn opgesplitst in PlugPHP en PlugSQL, als je beide wilt, kun je ze combineren met makePlug, en [Plug] kun je omzetten naar [PlugPHP] of [PlugSQL] via pickTypedPlug
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@716 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -6,7 +6,7 @@ versionbanner :: String
 versionbanner = "ADL vs. "++versionNumber 
 
 versionNumber :: String
-versionNumber = "1.1.0.714" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
+versionNumber = "1.1.0.716" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
 {-
 SVN Version text:
 Plug datatypes zijn opgesplitst in PlugPHP en PlugSQL, als je beide wilt, kun je ze combineren met makePlug, en [Plug] kun je omzetten naar [PlugPHP] of [PlugSQL] via pickTypedPlug
