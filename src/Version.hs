7fc5f49a91977fee7e1ac62c43ccc224309b1e63
Han Joosten <han.joosten.han@gmail.com>
2010-09-04 11:17:33 +0000

    20100826_fspec_CONCEPT.issue opgelost. (Rieks, als je het er mee eens bent, graag de issue deleten.)
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@739 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -6,10 +6,8 @@ versionbanner :: String
 versionbanner = "ADL vs. "++versionNumber 
 
 versionNumber :: String
-versionNumber = "1.1.0.737" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
+versionNumber = "1.1.0.739" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
 {-
 SVN Version text:
-* Foutje verholpen in ampersand.cabl , met betrekking tot gebruik van UTF8
-*AGPL- license toegevoed.
-* class-diagram in hoofdstuk 4 beter passend gemaakt. 
+20100826_fspec_CONCEPT.issue opgelost. (Rieks, als je het er mee eens bent, graag de issue deleten.)
 -}
