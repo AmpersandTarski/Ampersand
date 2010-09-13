8f583d83d52d5e29b35c813c2e8b108713e8a7cf
Han Joosten <han.joosten.han@gmail.com>
2010-09-13 20:19:35 +0000

    RST (gebruikt voor explainations) wordt nu beter geparsed.
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@748 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Data/Explain.hs
+++ src/Data/Explain.hs
@@ -11,36 +11,30 @@ where
 import Text.Pandoc
 import Languages
 import Options 
-
 data AutoExplain = Because Lang ExplainContent deriving (Eq,Show)
  
-type ExplainContent = PandocADL --deriving (Eq,Show) --TODO: Show moet nog worden weggewerkt. 
-data PandocADL = PAdl [Block] deriving (Eq,Show)
+type ExplainContent = [Block]
 
-string2AutoExplain :: Options -> String -> AutoExplain  --TODO alle aanroepen hiervan vervangen door mooie PANDOC [Inline].
-string2AutoExplain flags x = Because (language flags) (string2ExplainContent flags x)
+string2AutoExplain :: Options -> String -> AutoExplain  
+string2AutoExplain flags x = Because (language flags) (rst2Blocks x)
 explainParagraph :: Options -> [Inline] -> AutoExplain
-explainParagraph flags x = Because (language flags) (PAdl [Para x])
+explainParagraph flags x = Because (language flags) [Para x]
 
 string2ExplainContent :: Options -> String -> ExplainContent
--- TODO: An attempt should be undertaken to parse the string to a [Inline]. 
--- If this succeeds, that [Inline] should be used. If it fails, the String itself should be used. 
--- The parsing should be done using Pandoc parsers. (http://hackage.haskell.org/packages/archive/pandoc/1.6/doc/html/src/Text-Pandoc-Readers-Markdown.html#readMarkdown
+string2ExplainContent _ text = rst2Blocks text  --TODO: Met de flags kunnen we wellicht verschillende pandoc parsers organiseren....
 
---string2ExplainContent text = inlines2ExplainContent  (map line2inline (lines text))
---   where line2inline str = Str str
+rst2Blocks :: String -> [Block]
+rst2Blocks str = blocks
+   where 
+     Pandoc _ blocks = readRST defaultParserState (removeLineFeeds str)
+     removeLineFeeds :: String -> String
+     removeLineFeeds [] = []
+     removeLineFeeds ('\r' :'\n' : xs) = '\n' : (removeLineFeeds xs)
+     removeLineFeeds (c:xs) = c:removeLineFeeds xs
 
-string2ExplainContent flags text = parseToPAdl text  --TODO: Met de flags kunnen we wellicht verschillende pandoc parsers organiseren....
-parseToPAdl :: String -> PandocADL
-parseToPAdl s = case readRST defaultParserState{stateStandalone=False} s of
-                     Pandoc _ xs  -> PAdl xs
-                     _            -> PAdl [Para [Str s]] 
-                        
 explainContent2String :: ExplainContent -> String
-explainContent2String ec = writeRST defaultWriterOptions {writerStandalone = False} dummyPandoc
-  where dummyPandoc = Pandoc (Meta [][][]) theBlock  --TODO Resultaat bekijken, eventueel moet er nog wat van de string worden gesloopt. 
-        PAdl theBlock = ec
+explainContent2String ec = writeRST defaultWriterOptions (Pandoc (Meta [][][]) ec)
 
 explainContent2Blocks :: ExplainContent -> [Block]
-explainContent2Blocks (PAdl bs) = bs
+explainContent2Blocks ( bs) = bs
 
--- src/Version.hs
+++ src/Version.hs
@@ -6,8 +6,8 @@ versionbanner :: String
 versionbanner = "ADL vs. "++versionNumber 
 
 versionNumber :: String
-versionNumber = "1.1.0.740" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
+versionNumber = "1.1.0.748" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
 {-
 SVN Version text:
-Kleine opschoonactie ter voorbereiding op betere (gesplitste) classDiagrams.
+RST (gebruikt voor explainations) wordt nu beter geparsed. 
 -}
