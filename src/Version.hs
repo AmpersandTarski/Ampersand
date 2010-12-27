2a96db987164ea6ceb02fbe045e22a2867d0537a
Stef Joosten <stef.joosten@ou.nl>
2010-12-27 11:43:23 +0000

    Roles is now used in the functional specification. A beginning has been made with a new chapter in the functional spec...
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@801 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Version.hs
+++ src/Version.hs
@@ -6,8 +6,8 @@ versionbanner :: String
 versionbanner = "ADL vs. "++versionNumber 
 
 versionNumber :: String
-versionNumber = "1.1.0.799" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
+versionNumber = "1.1.0.801" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
 {-
 SVN Version text:
-Ongoing work on Ampersand.Definition. 
+Roles is now used in the functional specification. A beginning has been made with a new chapter in the functional spec... 
 -}
