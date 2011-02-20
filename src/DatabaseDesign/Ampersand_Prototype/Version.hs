13c4c14c4918a388a363328d635b723190bbc7bf
Stef Joosten <stef.joosten@ou.nl>
2011-02-20 07:25:25 +0000

    mistake in .cabal file fixed (no ticket) This version is compilable with Ampersand vs. 2.0.0.14
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@880 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/DatabaseDesign/Ampersand_Prototype/Version.hs
+++ src/DatabaseDesign/Ampersand_Prototype/Version.hs
@@ -7,7 +7,7 @@ ampersandPrototypeVersionBanner :: String
 ampersandPrototypeVersionBanner = "Prototype vs. "++versionNumberPrototype++ "(core vs. "++versionNumber++")"
 
 versionNumberPrototype :: String
-versionNumberPrototype = "1.1.0.879" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
+versionNumberPrototype = "1.1.0.880" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
 {-
 SVN Version text:
 mistake in .cabal file fixed (no ticket)
