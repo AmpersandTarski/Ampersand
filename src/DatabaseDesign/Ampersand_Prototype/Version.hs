112f0e262b848fc3b3ea61e43610fd50dac2246b
Stef Joosten <stef.joosten@ou.nl>
2011-02-20 07:24:17 +0000

    mistake in .cabal file fixed (no ticket) This version is compilable with Ampersand vs. 2.0.0.14
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@879 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/DatabaseDesign/Ampersand_Prototype/Version.hs
+++ src/DatabaseDesign/Ampersand_Prototype/Version.hs
@@ -7,9 +7,9 @@ ampersandPrototypeVersionBanner :: String
 ampersandPrototypeVersionBanner = "Prototype vs. "++versionNumberPrototype++ "(core vs. "++versionNumber++")"
 
 versionNumberPrototype :: String
-versionNumberPrototype = "1.1.0.874" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
+versionNumberPrototype = "1.1.0.879" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
 {-
 SVN Version text:
-Slight restructuring of class Explainable caused minor changes in the prototype generator.
-Ticket 11 set to 'fixed'.
+mistake in .cabal file fixed (no ticket)
+This version is compilable with Ampersand vs. 2.0.0.14
 -}
