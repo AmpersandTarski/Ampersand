5c49950cf78e63b4a80a3e30e19c04268310a97b
Rieks Joosten <Rieks.Joosten@tno.nl>
2011-02-21 15:56:20 +0000

    Saving ongoing work
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@889 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- miscellaneous/update_highlighterfiles(RJ).bat
+++ miscellaneous/update_highlighterfiles(RJ).bat
@@ -1,2 +1,2 @@
-@xcopy "c:\Documents and Settings\joostenhjm\Application Data\JGsoft\EditPad Pro 6\Ampersand*.*" "D:\CC_Demos\ADL_Onderzoek\OU - ADL\trunk\miscellaneous\"
+@xcopy "c:\Documents and Settings\joostenhjm\Application Data\JGsoft\EditPad Pro 6\Ampersand*.*" "D:\CC_Demos\ADL_Onderzoek\OU - ADL\trunk\miscellaneous\" /y
 rem @pause
