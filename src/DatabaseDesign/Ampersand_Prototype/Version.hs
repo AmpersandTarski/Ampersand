da857630d2bc1ea76d57d9a3794e58fbc15f9d20
Stef Joosten <stef.joosten@ou.nl>
2011-02-25 16:27:28 +0000

    Oops... keeping Prototype compatible with core Ampersand. This version is compilable with Ampersand vs. 2.0.0.14
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@893 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/DatabaseDesign/Ampersand_Prototype/Version.hs
+++ src/DatabaseDesign/Ampersand_Prototype/Version.hs
@@ -7,9 +7,9 @@ ampersandPrototypeVersionBanner :: String
 ampersandPrototypeVersionBanner = "Prototype vs. "++versionNumberPrototype++ "(core vs. "++versionNumber++")"
 
 versionNumberPrototype :: String
-versionNumberPrototype = "1.1.0.880" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
+versionNumberPrototype = "1.1.0.893" -- #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 ); #4 SVN revision number.
 {-
 SVN Version text:
-mistake in .cabal file fixed (no ticket)
+keeping Prototype compatible with core Ampersand.
 This version is compilable with Ampersand vs. 2.0.0.14
 -}
