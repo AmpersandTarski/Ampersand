9bf676dcbb41e2d1722023379af077bc57e9a8cc
Martijn Schrage <martijn@oblomov.com>
2011-11-17 10:07:31 +0000

    use jquery from static to allow off-line use
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@1132 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- static/Interface.php
+++ static/Interface.php
@@ -248,9 +248,9 @@ function editDelete($rel, $isFlipped, $parentAtom, $childAtom) {
 <head>
 <link href="css/Ampersand.css" rel="stylesheet" type="text/css"/>
 <link href="css/Custom.css" rel="stylesheet" type="text/css"/>
-<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+<link href="css/jquery-ui-1.8.css" rel="stylesheet" type="text/css"/>
+<script src="js/jquery-1.5.min.js"></script>
+<script src="js/jquery-ui-1.8.min.js"></script>
 <script src="js/Ampersand.js"></script>
 <script type="text/javascript">
 
