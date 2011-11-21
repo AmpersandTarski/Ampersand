d871c827695b456c13838279c626cca70dbdef5f
Oblosys <martijn@oblomov.com>
2011-11-21 14:59:59 +0000

    export defaultFlags, as it is used in test-ampersand.hs
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@330 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/DatabaseDesign/Ampersand/Misc.hs
+++ src/DatabaseDesign/Ampersand/Misc.hs
@@ -3,7 +3,7 @@ module DatabaseDesign.Ampersand.Misc (module X) where
 import DatabaseDesign.Ampersand.Misc.Languages as X
        (Lang(..), plural)
 import DatabaseDesign.Ampersand.Misc.Options as X
-       (getOptions, Options(..), ParserVersion(..),
+       (getOptions, Options(..), defaultFlags, ParserVersion(..),
         verboseLn, verbose, DocTheme(..), FspecFormat(..),
         ImportFormat(..), helpNVersionTexts)
 import DatabaseDesign.Ampersand.Misc.Explain as X
