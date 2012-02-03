007b5a769fb5aa3f7f963b7d720314c3673d01a2
Martijn Schrage <martijn@oblomov.com>
2012-02-03 17:43:43 +0000

    First create all labels, then just reference them
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@434 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/DatabaseDesign/Ampersand/Fspec/UML.hs
+++ src/DatabaseDesign/Ampersand/Fspec/UML.hs
@@ -1,4 +1,4 @@
-module DatabaseDesign.Ampersand.Fspec.UML where
+module DatabaseDesign.Ampersand.Fspec.UML (generateUML) where
 
 import DatabaseDesign.Ampersand.Basics
 import DatabaseDesign.Ampersand.Misc
@@ -9,38 +9,31 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Control.Monad.State
 
+fatal :: Int -> String -> a
+fatal = fatalMsg "UML"
+
 -- TODO: escape
 -- TODO: names of model, package, assoc (empty?), etc.
 
 generateUML :: Fspc -> Options -> String
 generateUML fSpec opts = showUML (genUMLClassDiag $ cdAnalysis fSpec opts)
 
-data UMLState = UMLState { idCounter :: Int
-                         , labelIdMap :: Map String String
-                         , diagramEltIds :: [String]
-                         }
-                         
-type StateUML a = State UMLState a
-                           
-
-type UML = StateUML [String]
-
 showUML :: UML -> String
 showUML uml = unlines $ evalState uml $ UMLState 0 Map.empty []
-{-OOclassdiagram {classes     :: [Class]            --
-                                   ,assocs      :: [Association]      --
-                                   ,aggrs       :: [Aggregation]      --
-                                   ,geners      :: [Generalization]   --
-                                   ,nameandcpts :: (String,[A_Concept])}-}
+
 genUMLClassDiag :: ClassDiag -> UML
 genUMLClassDiag (OOclassdiagram classes assocs _ _ (contextName, allConcepts)) =
- do { packageId <- getUnlabeledId "Package" 
-    ; diagramId <- getUnlabeledId "Diagram" 
-    ; datatypesUML <- mapM genUMLDatatype $ map name allConcepts >- [ name | OOClass name _ _ <-  classes ] 
-    ; classesUML <- mapM genUMLClass $ classes 
-    ; assocsUML <- mapM genUMLAssociation $ {- [OOAssoc "Document" "" "" "Dossier" "" ""] -- -} assocs 
+ do { packageId <- mkUnlabeledId "Package" 
+    ; diagramId <- mkUnlabeledId "Diagram"
+    
+    ; sequence $ map (mkLabeledId "Datatype") datatypeNames
+    ; sequence $ map (mkLabeledId "Class") classNames
+
+    ; datatypesUML <- mapM genUMLDatatype datatypeNames
+    ; classesUML <- mapM genUMLClass classes 
+    ; assocsUML <- mapM genUMLAssociation assocs 
     ; diagramElements <- genDiagramElements
-    ; return $ [ "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+    ; return $ [ "<?xxxml version=\"1.0\" encoding=\"UTF-8\"?>"
                , "<xmi:XMI xmi:version=\"2.1\" xmlns:uml=\"http://schema.omg.org/spec/UML/2.1\" xmlns:xmi=\"http://schema.omg.org/spec/XMI/2.1\">"
                , " <xmi:Documentation exporter=\"Enterprise Architect\" exporterVersion=\"6.5\"/>"
                , " <uml:Model xmi:type=\"uml:Model\" name=\""++contextName++"\" visibility=\"public\">"
@@ -63,16 +56,19 @@ genUMLClassDiag (OOclassdiagram classes assocs _ _ (contextName, allConcepts)) =
                , " </xmi:Extension>"
                , "</xmi:XMI>" ]
     }
+ where classNames    = [ name | OOClass name _ _ <-  classes ]
+       datatypeNames = map name allConcepts >- classNames
+       
 genUMLDatatype :: String -> UML
 genUMLDatatype name =
- do { classId <- getLabeledId "Class" name
-    ; addToDiagram classId
-    ; return $ [ "   <packagedElement xmi:type=\"uml:DataType\" xmi:id=\""++classId++"\" name=\""++name++"\" visibility=\"public\"/> " ]
+ do { datatypeId <- refLabeledId name
+    ; addToDiagram datatypeId
+    ; return $ [ "   <packagedElement xmi:type=\"uml:DataType\" xmi:id=\""++datatypeId++"\" name=\""++name++"\" visibility=\"public\"/> " ]
     }
 
 genUMLClass :: Class -> UML
 genUMLClass (OOClass name attrs methods) =
- do { classId <- getLabeledId "Class" name
+ do { classId <- refLabeledId name
     ; addToDiagram classId
     ; attributesUML <- mapM genUMAttribute attrs
     ; return $ [ "   <packagedElement xmi:type=\"uml:Class\" xmi:id=\""++classId++"\" name=\""++name++"\" visibility=\"public\">"] ++
@@ -82,23 +78,25 @@ genUMLClass (OOClass name attrs methods) =
 
 genUMAttribute :: Attribute -> UML
 genUMAttribute  (OOAttr name attrType leftOpen) =
- do { attrId <- getUnlabeledId "Attr"
-    ; classId <- getLabeledId "Class" attrType
+ do { attrId <- mkUnlabeledId "Attr"
+    ; lIntId <- mkUnlabeledId "Int"
+    ; uIntId <- mkUnlabeledId "Int"
+    ; classId <- refLabeledId attrType
     ; return [ "      <ownedAttribute xmi:type=\"uml:Property\" xmi:id=\""++attrId++"\" name=\""++name++"\" visibility=\"public\" isStatic=\"false\""++
                                     " isReadOnly=\"false\" isDerived=\"false\" isOrdered=\"false\" isUnique=\"true\" isDerivedUnion=\"false\">"
-             , "       <lowerValue xmi:type=\"uml:LiteralInteger\" xmi:id=\"INTID1\" value=\"1\"/>"
-             , "       <upperValue xmi:type=\"uml:LiteralInteger\" xmi:id=\"INTID2\" value=\"1\"/>"
+             , "       <lowerValue xmi:type=\"uml:LiteralInteger\" xmi:id=\""++lIntId++"\" value=\"1\"/>"
+             , "       <upperValue xmi:type=\"uml:LiteralInteger\" xmi:id=\""++uIntId++"\" value=\"1\"/>"
              , "       <type xmi:idref=\""++classId++"\"/>"
              , "      </ownedAttribute>"]
     }
 
 genUMLAssociation :: Association -> UML
 genUMLAssociation (OOAssoc lClass lMults lRole rClass rMults rRole) =
- do { assocId <- getUnlabeledId "Assoc"
-    ; lClassId <- getLabeledId "Class" lClass
-    ; rClassId <- getLabeledId "Class" rClass
-    ; lEndId <- getUnlabeledId "MemberEnd"
-    ; rEndId <- getUnlabeledId "MemberEnd"
+ do { assocId <- mkUnlabeledId "Assoc"
+    ; lClassId <- refLabeledId lClass
+    ; rClassId <- refLabeledId rClass
+    ; lEndId <- mkUnlabeledId "MemberEnd"
+    ; rEndId <- mkUnlabeledId "MemberEnd"
     ; return $
         [ "   <packagedElement xmi:type=\"uml:Association\" xmi:id=\""++assocId++"\" name=\""++rRole++"\" visibility=\"public\">"
         , "    <memberEnd xmi:idref=\""++lEndId++"\"/>"
@@ -110,38 +108,56 @@ genUMLAssociation (OOAssoc lClass lMults lRole rClass rMults rRole) =
         ]
     }
  where genOwnedEnd assocId endId classId =
-         [ "    <ownedEnd xmi:type=\"uml:Property\" xmi:id=\""++endId++"\" visibility=\"public\" association=\""++assocId++"\" isStatic=\"false\" isReadOnly=\"false\" isDerived=\"false\" isOrdered=\"false\" isUnique=\"true\" isDerivedUnion=\"false\" aggregation=\"none\">"
+         [ "    <ownedEnd xmi:type=\"uml:Property\" xmi:id=\""++endId++"\" visibility=\"public\" association=\""++assocId++"\" isStatic=\"false\""++
+                        " isReadOnly=\"false\" isDerived=\"false\" isOrdered=\"false\" isUnique=\"true\" isDerivedUnion=\"false\" aggregation=\"none\">"
          , "     <type xmi:idref=\""++classId++"\"/>"
          , "    </ownedEnd>"
          ]
                      
 genDiagramElements :: UML
 genDiagramElements =
- do { state <- get
-    ; return [ "     <element subject=\""++elementId++"\"/>" | elementId <- diagramEltIds state ]
+ do { elementIds <- gets diagramEltIds
+    ; return [ "     <element subject=\""++elementId++"\"/>" | elementId <- elementIds ]
     }
+
+
+-- State and Monad
+
+data UMLState = UMLState { idCounter :: Int
+                         , labelIdMap :: Map String String
+                         , diagramEltIds :: [String]
+                         }
+                         
+type StateUML a = State UMLState a
+                           
+
+type UML = StateUML [String]
+
 
 addToDiagram :: String -> StateUML ()
 addToDiagram elementId =
  do { modify $ \state -> state { diagramEltIds = elementId : diagramEltIds state} 
     }
     
-getUnlabeledId :: String -> StateUML String
-getUnlabeledId tag =
+mkUnlabeledId :: String -> StateUML String
+mkUnlabeledId tag =
  do { idC <- gets idCounter 
     ; modify $ \state -> state { idCounter = idCounter state + 1}
-    ; let classId = tag++"ID_"++show idC 
-    ; return classId
+    ; let unlabeledId = tag++"ID_"++show idC 
+    ; return unlabeledId
     }
 
-getLabeledId :: String -> String -> StateUML String
-getLabeledId tag label =
+refLabeledId :: String -> StateUML String
+refLabeledId label =
  do { lidMap <- gets labelIdMap
-    ; classId <- case Map.lookup label lidMap of
+    ; labeledId <- case Map.lookup label lidMap of
                    Just lid -> return lid
-                   Nothing  -> do { let classId = tag++"ID_"++label 
-                                  ; modify $ \state -> state { labelIdMap =  Map.insert label classId (labelIdMap state) }
-                                  ; return classId
-                                  }
-    ; return classId
+                   Nothing  -> fatal 147 $ "Requesting non-existent label "++label
+    ; return labeledId
+    }
+    
+mkLabeledId :: String -> String -> StateUML ()
+mkLabeledId tag label =
+ do { let classId = tag++"ID_"++label 
+    ; modify $ \state -> state { labelIdMap =  Map.insert label classId (labelIdMap state) }
     }
