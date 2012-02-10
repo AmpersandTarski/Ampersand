2a62619168e9043d5a0de8541ce2254ec88d50e2
Martijn Schrage <martijn@oblomov.com>
2012-02-10 16:59:07 +0000

    removed old Monastir interface
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@1678 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- php/monastir/Aanvraag/Application.php
+++
@@ -1,8 +0,0 @@
-<?php // generated with ADL vs. 0.8.10
-  
-  require "localsettings.inc.php";
-  require "Application.inc.php";
-  
-  $view = new view(parseRequest(getObject_Application()),getObject_Application());
-  $view->display();
-  ?>
--- php/monastir/Aanvraag/Decision.php
+++
@@ -1,8 +0,0 @@
-<?php // generated with ADL vs. 0.8.10
-  
-  require "localsettings.inc.php";
-  require "Decision.inc.php";
-  
-  $view = new view(parseRequest(getObject_Decision()),getObject_Decision());
-  $view->display();
-  ?>
