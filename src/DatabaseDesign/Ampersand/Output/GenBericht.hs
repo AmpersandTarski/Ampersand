40e8293e691407d098f93805a0fd6f4cf05cfe1f
Martijn Schrage <martijn@oblomov.com>
2012-02-16 20:19:54 +0000

    also generate cardinality & definition, and produce actual CSV
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@455 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/DatabaseDesign/Ampersand/Output/GenBericht.hs
+++ src/DatabaseDesign/Ampersand/Output/GenBericht.hs
@@ -1,30 +1,45 @@
 module DatabaseDesign.Ampersand.Output.GenBericht where
 
 import Data.List
+import Text.CSV
 
+import DatabaseDesign.Ampersand.ADL1
 import DatabaseDesign.Ampersand.Basics
 import DatabaseDesign.Ampersand.Misc
 import DatabaseDesign.Ampersand.Fspec
 import DatabaseDesign.Ampersand.Core.AbstractSyntaxTree
 import DatabaseDesign.Ampersand.Fspec
 
-generateBericht :: Fspc -> Options -> IO [(String, String)]
-generateBericht fSpec opts = return [("Bericht.csv", layout . genBerichtInterfaces $ interfaceS fSpec)]
+-- TODO: only show Rel and Flp Rel? give error otherwise?
+--       what about Typ, Brk etc.?
 
-genBerichtInterfaces :: [Interface] -> [[String]]
-genBerichtInterfaces interfaces = [["Naam", "Type"]] ++
+generateBericht :: Fspc -> Options -> IO [(String, String)]
+generateBericht fSpec opts = return [("Bericht.csv", printCSV {-layout-} . genBerichtInterfaces $ interfaceS fSpec)]
+
+genBerichtInterfaces :: [Interface] -> CSV
+genBerichtInterfaces interfaces = [["Naam", "Card.", "Definitie", "Type"]] ++
                                   concatMap genBerichtInterface interfaces
 
-genBerichtInterface :: Interface -> [[String]]
-genBerichtInterface interface = genBerichtObjDef 0 (ifcObj interface) ++ [["",""]]
+genBerichtInterface :: Interface -> CSV
+genBerichtInterface interface = genBerichtObjDef (ifcObj interface) ++ [["","","",""]]
 
-genBerichtObjDef :: Int -> ObjectDef -> [[String]]
-genBerichtObjDef nesting objDef = indentHead nesting $ 
-   [[name objDef, name (target $ objctx objDef)]] ++
-   concatMap (genBerichtObjDef (nesting+1)) (objats objDef)           
+genBerichtObjDef :: ObjectDef -> CSV
+genBerichtObjDef objDef = indentHead 1 $ 
+   [[ name objDef
+    , card $ objctx objDef
+    , def $ objctx objDef 
+    , name (target $ objctx objDef)
+    ]] ++
+   concatMap genBerichtObjDef (objats objDef)           
+ where card e = (if isTot e then "1" else "0")++".."++(if isUni e then "1" else "*")
+       
+       def (ERel (Rel{reldcl=Sgn{decMean=meaning}}))        = showMeaning meaning
+       def (EFlp (ERel (Rel{reldcl=Sgn{decMean=meaning}}))) = "~ "++showMeaning meaning
+       def _                                         = "meaningless"
+       
+       showMeaning meaning = concat [ aMarkup2String m | m@A_Markup{amLang=Dutch} <- ameaMrk meaning ]
 
-
-indentHead i lines = [[replicate i ' '++c1]++line | (c1:line) <- lines]
+indentHead i lines = [[replicate i '+'++c1]++line | (c1:line) <- lines]
 
 layout :: [[String]] -> String
 layout lines = 
@@ -34,3 +49,4 @@ layout lines =
                          in  map (fill width) col
   in  unlines . map (intercalate " ") . transpose . map formatColumn $ columns
  where fill i str = str ++ take (i - length str) (replicate i ' ') 
+ 
