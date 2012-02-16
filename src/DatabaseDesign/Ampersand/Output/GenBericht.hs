60f6453ef62d2c01e3ed480409ea41511c0632d1
Martijn Schrage <martijn@oblomov.com>
2012-02-16 15:41:48 +0000

    Added some basic generation in textual format
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@454 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/DatabaseDesign/Ampersand/Output/GenBericht.hs
+++ src/DatabaseDesign/Ampersand/Output/GenBericht.hs
@@ -1,7 +1,36 @@
 module DatabaseDesign.Ampersand.Output.GenBericht where
 
+import Data.List
+
+import DatabaseDesign.Ampersand.Basics
 import DatabaseDesign.Ampersand.Misc
+import DatabaseDesign.Ampersand.Fspec
+import DatabaseDesign.Ampersand.Core.AbstractSyntaxTree
 import DatabaseDesign.Ampersand.Fspec
 
 generateBericht :: Fspc -> Options -> IO [(String, String)]
-generateBericht fSpec opts = return [("Bericht1.csv","bericht1"),("Bericht2.csv","bericht2")]
+generateBericht fSpec opts = return [("Bericht.csv", layout . genBerichtInterfaces $ interfaceS fSpec)]
+
+genBerichtInterfaces :: [Interface] -> [[String]]
+genBerichtInterfaces interfaces = [["Naam", "Type"]] ++
+                                  concatMap genBerichtInterface interfaces
+
+genBerichtInterface :: Interface -> [[String]]
+genBerichtInterface interface = genBerichtObjDef 0 (ifcObj interface) ++ [["",""]]
+
+genBerichtObjDef :: Int -> ObjectDef -> [[String]]
+genBerichtObjDef nesting objDef = indentHead nesting $ 
+   [[name objDef, name (target $ objctx objDef)]] ++
+   concatMap (genBerichtObjDef (nesting+1)) (objats objDef)           
+
+
+indentHead i lines = [[replicate i ' '++c1]++line | (c1:line) <- lines]
+
+layout :: [[String]] -> String
+layout lines = 
+  let columns = transpose lines
+      widths = map (sum . map length) $ columns
+      formatColumn col = let width = maximum . map length $ col
+                         in  map (fill width) col
+  in  unlines . map (intercalate " ") . transpose . map formatColumn $ columns
+ where fill i str = str ++ take (i - length str) (replicate i ' ') 
