960165f5c13286a1f7ce6779ebfe3fcd4ac26105
Martijn Schrage <martijn@oblomov.com>
2012-03-19 18:29:17 +0000

    added function executePHP that takes a string with php code and returns a string with the execution results
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@1871 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/DatabaseDesign/Ampersand_Prototype/ValidateSQL.hs
+++ src/DatabaseDesign/Ampersand_Prototype/ValidateSQL.hs
@@ -2,7 +2,11 @@ module DatabaseDesign.Ampersand_Prototype.ValidateSQL where
 
 import DatabaseDesign.Ampersand.Misc
 import DatabaseDesign.Ampersand.Fspec
+import System.Process
+import System.IO
 
+
+-- TODO: fail with error code if validation fails or something goes wrong along the way
 {-
 Validate the generated SQL for all rules in the fSpec, by comparing the evaluation results
 with the results from Haskell-based Ampersand rule evaluator. The latter is much simpler and
@@ -10,4 +14,38 @@ therefore most likely to be correct in case of discrepancies.
 -}
 
 validateRuleSQL :: Fspc -> Options -> IO ()
-validateRuleSQL fSpec opts = putStrLn "Validation of generated SQL for rules has not been implemented yet."
+validateRuleSQL fSpec opts =
+ do { res <- executePHP "<?php echo 'PHP says hi'; ?>"
+    ; putStrLn $ "php results: "++res
+    ; return ()
+    }
+    
+executePHP :: String -> IO String
+executePHP phpStr =
+ do { (mStdIn, mStdOut, mStdErr, procHandle) <- createProcess cp 
+    ; case (mStdIn, mStdOut, mStdErr) of
+        (Nothing, _, _) -> error "no input handle"
+        (_, Nothing, _) -> error "no output handle"
+        (_, _, Nothing) -> error "no error handle"
+        (Just stdInH, Just stdOutH, Just stdErrH) ->
+         do { putStrLn "done"
+            ; hPutStr stdInH phpStr -- feed php script into the input pipe
+            ; hClose stdInH
+            ; outputStr <- hGetContents stdOutH --and fetch the results from the output pipe
+            ; errStr <- hGetContents stdErrH
+            ; seq (length outputStr) $ return ()
+            ; seq (length errStr) $ return ()
+            ; hClose stdOutH
+            ; hClose stdErrH -- TODO: read stdErr
+            ; return outputStr
+            }
+    }
+ where cp = CreateProcess
+              { cmdspec      = RawCommand "php" []
+              , cwd          = Nothing -- path
+              , env          = Nothing -- environment
+              , std_in       = CreatePipe 
+              , std_out      = CreatePipe
+              , std_err      = CreatePipe
+              , close_fds    = False -- don't close all other file descr.
+              }
