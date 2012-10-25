d14334b2a07766ae3f479cbdfa325a93e7893233
Stef Joosten <stef.joosten@ou.nl>
2012-10-25 06:14:09 +0000

    Created fatals in order to catch all "cannot find in Map" errors.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@762 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/lib/DatabaseDesign/Ampersand/Core/AbstractSyntaxTree.hs
+++ src/lib/DatabaseDesign/Ampersand/Core/AbstractSyntaxTree.hs
@@ -342,19 +342,16 @@ data Population
          , popps :: Pairs
          }
 
-{- To avoid a compile time loop, names were used in the old data structure. Since we have a distinction between P-structure and A-structure,
-the reason for doing this has evaporated. So we should be able to fix this without trouble. TODO -}
 data ExplObj = ExplConceptDef ConceptDef
              | ExplDeclaration Declaration
              | ExplRule String
              | ExplKeyDef String
-             | ExplPattern String   -- SJ: (now obsolete...) To avoid a compile time loop, the name of the pattern is used rather than the entire pattern. Hence, for patterns the PRef2Pattern is identical to the ExplPattern
-             | ExplProcess String   -- SJ: (now obsolete...) To avoid a compile time loop, the name of the process is used rather than the entire process. Hence, for patterns the PRef2Process is identical to the ExplProcess
-             | ExplInterface String -- SJ: (now obsolete...) To avoid a compile time loop, the name of the interface is used rather than the entire interface. Hence, for interfaces the PRef2Interface is identical to the ExplInterface
-             | ExplContext String   -- SJ: (now obsolete...) To avoid a compile time loop, the name of the context is used rather than the entire context. Hence, for contexts the PRef2Context is identical to the ExplContext
-             | ExplFspc String      -- SJ: (now obsolete...) To avoid a compile time loop, the name of the fSpec is used rather than the entire fSpec. Hence, for contexts the PRef2Fspc is identical to the ExplFspc
+             | ExplPattern String
+             | ExplProcess String
+             | ExplInterface String
+             | ExplContext String
+             | ExplFspc String
           deriving (Show ,Eq)
-                  
 
 data Expression
       = EEqu (Expression,Expression)       -- ^ equivalence             =
