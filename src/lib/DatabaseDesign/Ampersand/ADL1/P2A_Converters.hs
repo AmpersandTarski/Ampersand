9c208cf3acd0bc116f2b1895ce713794ffbd4f48
Stef Joosten <stef.joosten@ou.nl>
2012-11-21 18:34:06 +0000

    corrected a mistake in genE. The classes were generated redundantly. Now, classes are generated with precisely one concept per class. Each class is now sorted.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@802 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/lib/DatabaseDesign/Ampersand/ADL1/P2A_Converters.hs
+++ src/lib/DatabaseDesign/Ampersand/ADL1/P2A_Converters.hs
@@ -1001,7 +1001,15 @@ pCtx2aCtx p_context
     specializationTuples :: [(A_Concept,A_Concept)]
     specializationTuples = [(pCpt2aCpt specCpt,pCpt2aCpt genCpt) | (specCpt, genCpt)<-isas]
     gEandClasses :: (A_Concept->A_Concept->DatabaseDesign.Ampersand.Core.Poset.Ordering, [[A_Concept]])
-    gEandClasses = DatabaseDesign.Ampersand.Core.Poset.makePartialOrder specializationTuples   -- The base hierarchy for the partial order of concepts (see makePartialOrder)
+    gEandClasses = error ("Diagnosis : "++show (nub (map (sortWith name) classes)))   -- The base hierarchy for the partial order of concepts (see makePartialOrder)
+     where (_, cls) = DatabaseDesign.Ampersand.Core.Poset.makePartialOrder specializationTuples
+           classes  = rd [] (map (DatabaseDesign.Ampersand.Core.Poset.sortBy DatabaseDesign.Ampersand.Core.Poset.compare) cls)
+           rd seen (cs:css)
+            = case cs>-seen of
+               [] -> rd seen css
+               _  -> cs: rd (seen++cs) css
+           rd _ [] = []
+               
     typeErrors :: [CtxError]
     typeErrors
      | (not.null) derivedEquals = derivedEquals
