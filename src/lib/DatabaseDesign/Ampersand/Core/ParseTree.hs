0d1f0576be68deaaaca1059fd84cca9b61d5ff86
Stef Joosten <stef.joosten@ou.nl>
2013-04-10 07:38:08 +0000

    Implemented instance Prelude.Ord P_Declaration on the basis of origin, for efficiency reasons.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@940 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/lib/DatabaseDesign/Ampersand/Core/ParseTree.hs
+++ src/lib/DatabaseDesign/Ampersand/Core/ParseTree.hs
@@ -169,7 +169,7 @@ where
    instance Identified ConceptDef where
     name = cdcpt
    
-   data RelConceptDef = RelConceptDef SrcOrTgt String deriving (Eq, Prelude.Ord, Show)
+   data RelConceptDef = RelConceptDef SrcOrTgt String deriving (Eq, Show)
    
    data P_Declaration = 
          P_Sgn { dec_nm :: String    -- ^ the name of the declaration
@@ -183,9 +183,11 @@ where
                , dec_popu :: Pairs     -- ^ the list of tuples, of which the relation consists.
                , dec_fpos :: Origin    -- ^ the position in the Ampersand source file where this declaration is declared. Not all decalartions come from the ampersand souce file. 
                , dec_plug :: Bool      -- ^ if true, this relation may not be stored in or retrieved from the standard database (it should be gotten from a Plug of some sort instead)
-               } deriving (Prelude.Ord, Show) -- for debugging and testing only
+               } deriving Show -- for debugging and testing only
    instance Eq P_Declaration where
     decl==decl' = origin decl==origin decl'
+   instance Prelude.Ord P_Declaration where
+    decl `compare` decl' = origin decl `compare` origin decl'
    instance Identified P_Declaration where
     name = dec_nm
    instance Traced P_Declaration where
@@ -249,7 +251,7 @@ where
       PCpl orig _    -> orig
       PBrk orig _    -> orig
 
-   data SrcOrTgt = Src | Tgt deriving (Eq, Prelude.Ord, Show)
+   data SrcOrTgt = Src | Tgt deriving (Show, Eq)
 
    isSrc :: SrcOrTgt -> Bool
    isSrc Src = True
@@ -276,13 +278,13 @@ where
     name = rr_nm
     
    data PMeaning = PMeaning P_Markup 
-            deriving (Eq, Prelude.Ord, Show)
+            deriving Show
             
    data P_Markup = 
        P_Markup  { mLang ::   Maybe Lang
                  , mFormat :: Maybe PandocFormat
                  , mString :: String
-                 } deriving (Eq, Prelude.Ord, Show) -- Show is for debugging only     
+                 } deriving Show -- for debugging only     
                
    data P_Population
      = P_RelPopu { p_rnme ::  String  -- the name of a relation
@@ -441,9 +443,9 @@ where
    instance Traced P_Gen where
     origin = gen_fp
 
-   data Lang = Dutch | English deriving (Eq, Prelude.Ord, Show)
+   data Lang = Dutch | English deriving (Show, Eq)
 
-   data PandocFormat = HTML | ReST | LaTeX | Markdown deriving (Eq, Prelude.Ord, Show)
+   data PandocFormat = HTML | ReST | LaTeX | Markdown deriving (Eq, Show)
 
    type Props = [Prop]
 
