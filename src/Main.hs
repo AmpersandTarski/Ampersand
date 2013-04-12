4e1517715a62e4567cfc307ef090e3c7ecfadc6d
Han Joosten <han.joosten.han@gmail.com>
2013-04-12 13:13:39 +0000

    Cleanup structure of Main. Now all ampersand command line flags are supported.
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@2369 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/Main.hs
+++ src/Main.hs
@@ -4,13 +4,11 @@ module Main where
 import Control.Monad
 import Data.List
 import Data.Function (on)
-import System.FilePath        (replaceExtension,combine)
+import System.FilePath        (combine)
 import System.Exit
 import Prelude hiding (putStr,readFile,writeFile)
-import Data.GraphViz hiding (addExtension, C)
 import DatabaseDesign.Ampersand_Prototype.ObjBinGen    (phpObjInterfaces)
 import DatabaseDesign.Ampersand_Prototype.Apps.RAP   (atlas2context, atlas2populations)
-import DatabaseDesign.Ampersand_Prototype.Apps.RAPImport
 import DatabaseDesign.Ampersand_Prototype.CoreImporter
 import DatabaseDesign.Ampersand_Prototype.Version
 import DatabaseDesign.Ampersand_Prototype.GenBericht
@@ -22,7 +20,6 @@ import qualified DatabaseDesign.Ampersand.Basics as Basics
 fatal :: Int -> String -> a
 fatal = fatalMsg "Main"
 
--- TODO: should be cleaned up
 main :: IO ()
 main
  = do flags <- getOptions
@@ -30,59 +27,17 @@ main
        then mapM_ putStr (helpNVersionTexts prototypeVersionStr flags)
        else do gFspec <- createFspec flags
                case gFspec of
-                Errors err -> do Prelude.putStrLn $ "Error(s) found:"
+                Errors err -> do Prelude.putStrLn "Error(s) found:"
                                  mapM_ Basics.putStrLn (intersperse  (replicate 30 '=') (map showErr err))
                                  exitWith $ ExitFailure 10
-                Checked fspc -> generateProtoStuff flags fspc
-  where
-  parseAndTypeCheck :: Options -> IO A_Context 
-  parseAndTypeCheck flags 
-   = do { verboseLn flags "Start parsing...."
-        ; pCtxOrErr <- parseContext flags (fileName flags)
-        ; case pCtxOrErr of
-           Left pErr ->
-            do { Prelude.putStrLn $ "Parse error:"
-               ; Prelude.putStrLn $ show pErr
-               ; exitWith $ ExitFailure 10 
-               }
-           Right p_context ->  
-            do { let importFilename = importfile flags
-               ; pPops <- if null importFilename then return [] 
-                          else
-                           do popsText <- readFile importFilename
-                              case fileformat flags of
-                                Adl1PopFormat -> parsePopulations popsText flags importFilename
-                                Adl1Format -> do verbose flags ("Importing "++importFilename++" in RAP... ")
-                                                 imppCtxOrErr <- parseContext flags (importfile flags)
-                                                 case imppCtxOrErr of
-                                                   (Right imppcx) -> let (aCtxOrErr,_,_) = typeCheck imppcx [] in
-                                                                     case aCtxOrErr of
-                                                                      Checked a_context  -> importfspec  (makeFspec flags a_context) flags
-                                                                      Errors _           -> importfailed imppCtxOrErr flags 
-                                                   (Left _)       -> importfailed imppCtxOrErr flags 
-               ; verboseLn flags "Type checking..."
-               ; let (actxOrErrs,stTypeGraph,condensedGraph) = typeCheck p_context pPops
-               ; if typeGraphs flags
-                 then do { condensedGraphPath<-runGraphvizCommand Dot condensedGraph Png (replaceExtension ("Condensed_Graph_of_"++baseName flags) ".png")
-                         ; putStr ("\n"++condensedGraphPath++" written.")
-                         ; stDotGraphPath<-runGraphvizCommand Dot stTypeGraph Png (replaceExtension ("stGraph_of_"++baseName flags) ".png")
-                         ; putStr ("\n"++stDotGraphPath++" written.")
-                         }
-                 else do { putStr "" }
-               ; case actxOrErrs of
-                  Errors type_errors-> do { Prelude.putStrLn $ "The following type errors were found:\n"
-                                          ; Prelude.putStrLn $ intercalate "\n\n" (map show type_errors)
-                                          ; exitWith $ ExitFailure 20
-                                          }
-                  Checked actx      -> return actx
-               }
-        }
+                Checked fspc -> generateAmpersandOutput flags fspc
+                             >> generateProtoStuff flags fspc
 
 generateProtoStuff :: Options -> Fspc -> IO ()
 generateProtoStuff flags fSpec | validateSQL flags =
  do { verboseLn flags "Validating SQL expressions..."
     ; isValid <- validateRuleSQL fSpec flags
-    ; when (not isValid) $
+    ; unless isValid $
         exitWith $ ExitFailure 30
     }
 generateProtoStuff flags fSpec | export2adl flags && fileformat flags==Adl1Format =
