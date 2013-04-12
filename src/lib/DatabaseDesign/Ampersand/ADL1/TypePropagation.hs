116bfe7e98509097dbf2835f8b8bcc617f770c27
Stef Joosten <stef.joosten@ou.nl>
2013-04-12 07:26:10 +0000

    mrgUnion checking switched off
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@960 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/lib/DatabaseDesign/Ampersand/ADL1/TypePropagation.hs
+++ src/lib/DatabaseDesign/Ampersand/ADL1/TypePropagation.hs
@@ -419,7 +419,6 @@ composeMaps m1 m2 = Map.map (\x -> foldr mrgUnion [] [Map.findWithDefault [] s m
 symClosure :: (Ord a, Show a) => Map a [a] -> Map a [a]
 symClosure m = Map.unionWith mrgUnion m (reverseMap m)
 
-{-
 -- The following mrgUnion and mrgIntersect are more efficient, but lack checking...
 mrgUnion :: (Show a,Ord a) => [a] -> [a] -> [a]
 mrgUnion (a:as) (b:bs) | a<b       = a:mrgUnion as (b:bs)
@@ -438,7 +437,7 @@ nub' (x:y:xs) | x==y      = nub' (y:xs)
               | otherwise = x:(nub' (y:xs))
 nub' [x] = [x]
 nub' [] = []
--}
+{-
 
 -- The following mrgUnion and mrgIntersect are for debug purposes
 mrgUnion :: (Show a,Ord a) => [a] -> [a] -> [a]
@@ -475,6 +474,7 @@ nub' xs' = if isSortedAndDistinct res then res else fatal 428 ("nub' expects a s
                             | otherwise = x:(nub' (y:xs))
               nub'' [x] = [x]
               nub'' [] = []
+-}
 
 distinctCons :: (Ord a, Eq a, Show a) => a -> a -> [a] -> [a]
 distinctCons a b' (b:bs) = if a<b then b':(b:bs)
