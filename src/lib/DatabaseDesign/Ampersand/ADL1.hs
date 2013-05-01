ed1a12ec24f23835b7677d4aeef96e27d7b1d8df
Han Joosten <han.joosten.han@gmail.com>
2013-05-01 12:37:04 +0000

    some work on plugs
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@1004 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/lib/DatabaseDesign/Ampersand/ADL1.hs
+++ src/lib/DatabaseDesign/Ampersand/ADL1.hs
@@ -1,78 +1,68 @@
 {-# OPTIONS_GHC -Wall #-}
-module DatabaseDesign.Ampersand.ADL1
-           ( P_Context(..), A_Context(..)
-           , P_Meta(..), Meta(..)
-           , MetaObj(..)
-           , Process(..), P_Process(..), P_RoleRelation(..),RoleRelation(..), RoleRule(..)
-           , P_Pattern(..), Pattern(..)
-           , P_PairView(..), P_PairViewSegment(..), PairView(..), PairViewSegment(..) 
-           , SrcOrTgt(..)
-           , P_Rule(..), Rule(..), consequent, rulefromProp, ruleviolations, violationsexpr
-           , P_IdentDef(..), P_IdentSegment(..), IdentityDef(..), IdentitySegment(..)
-           , P_ViewDef(..), P_ViewSegment(..), ViewDef(..), ViewSegment(..)
-           , P_Population(..), UserDefPop(..)
-           , P_ObjectDef(..)
-           , ObjectDef(..), P_Interface(..)
-           , objAts, objatsLegacy
-           , P_SubInterface(..), SubInterface(..)
-           , Interface(..)
-           , Term(..),Expression(..) 
-           , A_Gen(..),P_Gen(..)
-           , Declaration(..),decusr
-           , P_Declaration(..)
-           , ConceptDef(..)
-           , P_Concept(..), A_Concept(..), (<==>),meet,join, P_Sign(..), Sign(..), showSign, GenR
-           , RuleType(..)
-           , Prop(..)
-           , isaRule
-           , FilePos(..), Origin(..), Pos(..), Traced(..)
-           , iExpr, vExpr
-           , antecedent,hasantecedent,notCpl,isCpl,deMorgan
-           , Signaling(..)
-           , Association(..)
-           , Label(..)
-           , Paire, Pairs, mkPair , srcPaire, trgPaire
-           , isAll, isCHC, isBlk, isNop, isDo, dos
-           , PPurpose(..), PRef2Obj(..), ExplObj(..)
-           , Purpose(..)
-           , (.==.), (.|-.), (./\.), (.\/.), (.-.), (./.), (.\.), (.:.), (.!.), (.*.)
-           )
+module DatabaseDesign.Ampersand.ADL1 (module X) 
 where
-   import DatabaseDesign.Ampersand.Core.ParseTree
-   import DatabaseDesign.Ampersand.Core.AbstractSyntaxTree         (
-                                          A_Concept(..)
-                                         ,Meta(..)
-                                         ,Sign(..),showSign,GenR()
-                                         , (<==>),meet,join
-                                         ,Signaling(..)
-                                         ,A_Context(..),Process(..)
-                                         ,Association(..)
-                                         ,Expression(..)
-                                         ,A_Gen(..)
-                                         ,IdentityDef(..)
-                                         ,IdentitySegment(..)
-                                         ,ViewDef(..)
-                                         ,ViewSegment(..)
-                                         ,ObjectDef(..)
-                                         ,objAts
-                                         ,objatsLegacy
-                                         ,SubInterface(..)
-                                         ,Declaration(..),decusr
-                                         ,Interface(..)
-                                         ,Pattern(..)
-                                         ,PairView(..)
-                                         ,PairViewSegment(..) 
-                                         ,Rule(..)
-                                         ,RuleType(..)
-                                         ,RoleRelation(..)
-                                         ,UserDefPop(..)
-                                         ,Purpose(..), ExplObj(..)
-                                         , iExpr, vExpr
-                                         , (.==.), (.|-.), (./\.), (.\/.), (.-.), (./.), (.\.), (.:.), (.!.), (.*.)
-                                         )
-   import DatabaseDesign.Ampersand.ADL1.Expression                 (
-                                          notCpl, isCpl, deMorgan)
-   import DatabaseDesign.Ampersand.ADL1.ECArule                    (isAll, isCHC, isBlk, isNop, isDo, dos)
-   import DatabaseDesign.Ampersand.ADL1.Rule                       (
-                                          rulefromProp, isaRule, ruleviolations, violationsexpr
-                                         ,consequent,antecedent,hasantecedent)
+import DatabaseDesign.Ampersand.Core.ParseTree as X (
+           PPurpose(..), PRef2Obj(..)
+         , Paire, Pairs, mkPair , srcPaire, trgPaire
+         , Label(..)
+         , FilePos(..), Origin(..), Pos(..), Traced(..)
+         , Prop(..)
+         , P_Concept(..)
+         , P_Sign(..)
+         , P_Context(..)
+         , P_Meta(..)
+         , MetaObj(..)
+         , P_Process(..), P_RoleRelation(..), RoleRule(..)
+         , P_Pattern(..)
+         , P_PairView(..), P_PairViewSegment(..)
+         , SrcOrTgt(..)
+         , P_Rule(..)
+         , P_IdentDef(..), P_IdentSegment(..)
+         , P_ViewDef(..), P_ViewSegment(..)
+         , P_Population(..)
+         , P_ObjectDef(..)
+         , P_Interface(..)
+         , P_SubInterface(..)
+         , Term(..)
+         , P_Gen(..)
+         , P_Declaration(..)
+         , ConceptDef(..)
+         )
+import DatabaseDesign.Ampersand.Core.AbstractSyntaxTree as X  (
+          A_Concept(..)
+         ,Meta(..)
+         ,Sign(..),showSign,GenR()
+         , (<==>),meet,join
+         ,Signaling(..)
+         ,A_Context(..),Process(..)
+         ,Association(..)
+         ,Expression(..)
+         ,A_Gen(..)
+         ,IdentityDef(..)
+         ,IdentitySegment(..)
+         ,ViewDef(..)
+         ,ViewSegment(..)
+         ,ObjectDef(..)
+         ,objAts
+         ,objatsLegacy
+         ,SubInterface(..)
+         ,Declaration(..),decusr
+         ,Interface(..)
+         ,Pattern(..)
+         ,PairView(..)
+         ,PairViewSegment(..) 
+         ,Rule(..)
+         ,RuleType(..)
+         ,RoleRelation(..)
+         ,UserDefPop(..)
+         ,Purpose(..), ExplObj(..)
+         , iExpr, vExpr
+         , (.==.), (.|-.), (./\.), (.\/.), (.-.), (./.), (.\.), (.:.), (.!.), (.*.)
+         )
+import DatabaseDesign.Ampersand.ADL1.Expression as X (
+         notCpl, isCpl, deMorgan)
+import DatabaseDesign.Ampersand.ADL1.ECArule as X (
+         isAll, isCHC, isBlk, isNop, isDo, dos)
+import DatabaseDesign.Ampersand.ADL1.Rule as X (
+          rulefromProp, isaRule, ruleviolations, violationsexpr
+         ,consequent,antecedent,hasantecedent)
