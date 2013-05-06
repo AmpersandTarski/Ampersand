4333a5788cfce3f3665a2914a72c8c7e21bddcc4
hanjoosten <han.joosten.han@gmail.com>
2013-05-06 07:18:03 +0000

    Another representation of the error message (no kernelpath)
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@1014 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/lib/DatabaseDesign/Ampersand/Fspec/Plug.hs
+++ src/lib/DatabaseDesign/Ampersand/Fspec/Plug.hs
@@ -17,7 +17,7 @@ where
 import DatabaseDesign.Ampersand.ADL1 
 import DatabaseDesign.Ampersand.Classes (Object(..),Populated(..),atomsOf,ConceptStructure(..),Relational(..))
 import DatabaseDesign.Ampersand.Basics
-import Data.List(elemIndex,nub,intercalate)
+import Data.List(elemIndex,nub,intercalate,sort)
 import GHC.Exts (sortWith)
 import DatabaseDesign.Ampersand.Fspec.Fspec
 import DatabaseDesign.Ampersand.Fspec.FPA (FPAble(fpa))
@@ -342,13 +342,13 @@ plugpath p@TblSQL{} srcfld trgfld
   --connect two paths over I[X] (I[X];srce)~;(I[X];trge) => filter I[X] => srcpath~;trgpath
   | (not.null) (pathsoverIs srcfld trgfld) =      foldr1 (.:.) (head (pathsoverIs srcfld trgfld))
   | (not.null) (pathsoverIs trgfld srcfld) = flp (foldr1 (.:.) (head (pathsoverIs trgfld srcfld)))
-  | otherwise = let showRow (es, s, t) = fldname s ++" => "++fldname t++":\n  "++intercalate "\n     " (map show es)
+  | otherwise = let showRow (es, s, t) = fldname s ++" => "++fldname t++":\n  " -- ++intercalate "\n     " (map show es)
                 in fatal 406 $ "no kernelpath:"
                 ++"\nplugname: "++(show.name) p
                 ++"\nsrcfld: "++(show.fldname) srcfld
                 ++"\ntrgfld: "++(show.fldname) trgfld
                 ++"\neLkpTbl ("++(show.length.eLkpTbl) p++" rows):\n"
-                ++intercalate "\n***\n" (map showRow [(es,s,t) | (es,s,t) <- eLkpTbl p, (s==srcfld && t==trgfld)||(t==srcfld && s==trgfld)])
+                ++intercalate "\n***\n" (sort (map showRow [(es,s,t) | (es,s,t) <- eLkpTbl p]))
   --paths from s to t by connecting r from mLkpTbl
   --the (r,srcfld,trgfld) from mLkpTbl form paths longer paths if connected: (trgfld m1==srcfld m2) => (m1;m2,srcfld m1,trgfld m2)
   where
