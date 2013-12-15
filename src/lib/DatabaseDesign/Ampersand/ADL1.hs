678da839e11fcde8f2f10ed2fd5538cf1f4b190c
hanjoosten <han.joosten.han@gmail.com>
2013-12-15 21:51:44 +0000

    Minor cleanup: removed violationsexpr
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@1248 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/lib/DatabaseDesign/Ampersand/ADL1.hs
+++ src/lib/DatabaseDesign/Ampersand/ADL1.hs
@@ -62,5 +62,5 @@ import DatabaseDesign.Ampersand.ADL1.Expression as X (
 import DatabaseDesign.Ampersand.ADL1.ECArule as X (
          isAll, isCHC, isBlk, isNop, isDo, dos)
 import DatabaseDesign.Ampersand.ADL1.Rule as X (
-          rulefromProp, ruleviolations, violationsexpr
+          rulefromProp, ruleviolations
          ,consequent,antecedent,hasantecedent)
