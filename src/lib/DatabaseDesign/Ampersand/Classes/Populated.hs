b1a001cacb5945b38eaad7c0e5216c4471e56f97
Stef Joosten <stef.joosten@ou.nl>
2013-12-20 12:59:06 +0000

    Users are not allowed to name SESSION atoms.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@1254 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/lib/DatabaseDesign/Ampersand/Classes/Populated.hs
+++ src/lib/DatabaseDesign/Ampersand/Classes/Populated.hs
@@ -105,7 +105,7 @@ where
             EDcI c     -> [mkPair a a | a <- atomsOf gens pt c]
             EEps i _   -> [mkPair a a | a <- atomsOf gens pt i]
             EDcV sgn   -> [mkPair s t | s <- atomsOf gens pt (source sgn), t <- atomsOf gens pt (target sgn) ]
-            EMp1 a c   -> if name c=="SESSION" then [fatal 111 ("cannot produce the SESSION atom "++a++"["++show c++"]")] else [mkPair a a] -- prevent populating SESSION
+            EMp1 a c   -> if name c=="SESSION" then [fatal 108 ("(Known problem) You have an atom of type SESSION called \""++a++"\". You should not give names to session atoms, because they are named by the computer.")] else [mkPair a a] -- prevent populating SESSION
 
 {- Derivation of contents (ERrs (l,r)):
 Let cL = contents l
