b3fc199bf6a11e9b56bb95c78cae2771e19c3c44
Han Joosten <han.joosten.han@gmail.com>
2014-03-02 17:13:31 +0000

    Oops, iets te veel van het goede
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@2935 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- src/DatabaseDesign/Ampersand_Prototype/RelBinGenSQL.hs
+++ src/DatabaseDesign/Ampersand_Prototype/RelBinGenSQL.hs
@@ -370,7 +370,7 @@ selectExprInFROM :: Fspc
                -> Expression  -- ^ Whatever expression to generate an SQL query for
                -> Maybe String
 selectExprInFROM fSpec i src trg expr
-   | src == trg = fatal 373 $ "selectExprInFrom must not be called with identical src and trg. ("++show src++") "++showADL expr
+   | src == trg && (not.isIdent) expr = fatal 373 $ "selectExprInFrom must not be called with identical src and trg. ("++show src++") "++showADL expr
    | unquoted src = selectExprInFROM fSpec i (quote src) trg         expr
    | unquoted trg = selectExprInFROM fSpec i src         (quote trg) expr
    | otherwise = 
