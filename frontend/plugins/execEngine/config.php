617b3133a4da7ec2925382642621c6f9a56cb619
Michiel Stornebrink <Michiel.Stornebrink@tno.nl>
2014-03-11 17:09:35 +0000

    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@2956 80d59baa-9aef-c140-ba76-e18f3b6eea4d

--- frontend/plugins/execEngine/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Making the ExecEngine silent (or not):
-$execEngineWhispers = false; // When 'true', the ExecEngine provides details when logging.
-$execEngineSays = true; // When 'true' the ExecEngine provides high level logging
-// This file contains all global variables that installations may want to keep private
-
-
-?>
