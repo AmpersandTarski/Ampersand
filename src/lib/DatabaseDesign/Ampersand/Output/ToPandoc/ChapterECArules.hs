7cc027663296ee84f2421e83333399f6000c47fe
stefjoosten <stef.joosten@ou.nl>
2014-03-19 07:32:23 +0000

    Reactivated ECA-rules in --haskell. SQL code generation for ECA-rules will have to wait till we are conficent that everything is correct.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@1307 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/lib/DatabaseDesign/Ampersand/Output/ToPandoc/ChapterECArules.hs
+++ src/lib/DatabaseDesign/Ampersand/Output/ToPandoc/ChapterECArules.hs
@@ -3,6 +3,7 @@
 module DatabaseDesign.Ampersand.Output.ToPandoc.ChapterECArules
 where
 import DatabaseDesign.Ampersand.Output.ToPandoc.SharedAmongChapters 
+import DatabaseDesign.Ampersand.Fspec.ToFspec.NormalForms (proofPA)
 import DatabaseDesign.Ampersand.ADL1
 
 chpECArules :: Fspc -> Options ->  Blocks
@@ -23,15 +24,16 @@ chpECArules fSpec _
    = fromList $
      case fsLang fSpec of
       Dutch   -> Para [ Str "ECA rules:",LineBreak, Str "   ",Str "tijdelijk ongedocumenteerd" ] : 
-                 [ BlockQuote (toList (codeBlock
-                      ( showECA fSpec "\n     " eca
--- Dit inschakelen          ++[LineBreak, Str "------ Derivation ----->"]
---  voor het bewijs         ++showProof (showECA fSpec [LineBreak, Str ">     "]) (proofPA (ecaAction eca))
---                          ++[LineBreak, Str "<------End Derivation --"]
-                      ) ) )
-                 | eca<-vEcas fSpec, not (isNop (ecaAction eca))]
+                 concat [ [ BlockQuote (toList (codeBlock ( showECA fSpec "\n     " eca )))
+                          , Para ( [ LineBreak, Str "------ Afleiding ----->"]                      ++   -- Dit in- en uitschakelbaar maken
+                                   showProof (showECA fSpec "\n>     ") (proofPA (ecaAction eca))    ++   --  voor het bewijs
+                                   [ LineBreak, Str "<------Einde afleiding --"] )                         --
+                          ]
+                        | eca<-vEcas fSpec, not (isNop (ecaAction eca))]
       English -> Para [ Str "ECA rules:",LineBreak, Str "   ",Str "temporarily not documented" ] :
-                 [ BlockQuote (toList (codeBlock
-                    ( showECA fSpec "\n" eca )))
-                 | eca<-vEcas fSpec, not (isNop (ecaAction eca))]
-
+                 concat [ [ BlockQuote (toList (codeBlock ( showECA fSpec "\n     " eca )))
+                          , Para ( [ LineBreak, Str "------ Derivation ----->"]                      ++   -- Dit in- en uitschakelbaar maken
+                                   showProof (showECA fSpec "\n>     ") (proofPA (ecaAction eca))    ++   --  voor het bewijs
+                                   [ LineBreak, Str "<------End Derivation --"] )                         --
+                          ]
+                        | eca<-vEcas fSpec, not (isNop (ecaAction eca))]
