b720b78bbe266264716fffc5dcf1804302e4d173
hanjoosten <han.joosten.han@gmail.com>
2014-04-13 14:36:39 +0000

    More cleanup of Options.hs
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@1328 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/lib/DatabaseDesign/Ampersand/Misc.hs
+++ src/lib/DatabaseDesign/Ampersand/Misc.hs
@@ -3,7 +3,7 @@ module DatabaseDesign.Ampersand.Misc (module X) where
 import DatabaseDesign.Ampersand.Misc.Languages as X
        (Lang(..), plural)
 import DatabaseDesign.Ampersand.Misc.Options as X
-       (getOptions, Options(..), defaultFlags, ParserVersion(..),
+       (getOptions, Options(..), ParserVersion(..),
         verboseLn, verbose, DocTheme(..), FspecFormat(..),
         FileFormat(..), helpNVersionTexts)
 import DatabaseDesign.Ampersand.Misc.Explain as X
