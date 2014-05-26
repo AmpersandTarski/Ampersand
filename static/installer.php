ec3b3915bd9643a7a30e0335cd2cdf629805c6b9
Rieks Joosten <Rieks.Joosten@tno.nl>
2014-05-26 06:14:55 +0000

    Hiermee is stap 3 van ticket 399 gezet. Han, nu kun jij vast stappen 1 en 2 van dat ticket afmaken.
    git-svn-id: http://svnint.ou.nl:8080/svn/ADL/trunk@3103 80d59baa-9aef-c140-ba76-e18f3b6eea4d

---
+++ static/installer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="no-cache">
+<meta http-equiv="Expires" content="-1">
+<meta http-equiv="cache-Control" content="no-cache">
+
+</html>
+<body>
+<?php
+  require 'InstallerDBstruct.php';
+  require 'InstallerDefPop.php';
+?>
+</body>
+</html>
