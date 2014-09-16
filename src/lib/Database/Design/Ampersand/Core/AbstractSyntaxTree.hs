0a4b73e4c373a5630fab2d785bf8391aa73e7d80
stefjoosten <stef.joosten@ou.nl>
2014-09-16 15:24:17 +0000

    I have introduced ECA rules in the interfaces. Compile shouldsucceed/InterfaceTest2.adl with option --haskell to see the effect in a small example.
    git-svn-id: svn+ssh://svn.code.sf.net/p/ampersand/code/trunk@1437 8b35ee93-31c1-4acc-b283-6c2059e6c15c

--- src/lib/Database/Design/Ampersand/Core/AbstractSyntaxTree.hs
+++ src/lib/Database/Design/Ampersand/Core/AbstractSyntaxTree.hs
@@ -33,6 +33,7 @@ module Database.Design.Ampersand.Core.AbstractSyntaxTree (
  , Population(..)
  , GenR
  , Association(..)
+ , PAclause(..), Event(..), ECArule(..), InsDel(..)
   -- (Poset.<=) is not exported because it requires hiding/qualifying the Prelude.<= or Poset.<= too much
   -- import directly from Database.Design.Ampersand.Core.Poset when needed
  , (<==>),join,meet,greatest,least,maxima,minima,sortWith
@@ -307,9 +308,11 @@ data Interface = Ifc { ifcParams :: [Expression] -- Only primitive expressions a
                      , ifcArgs ::   [[String]]
                      , ifcRoles ::  [String]
                      , ifcObj ::    ObjectDef -- NOTE: this top-level ObjectDef is contains the interface itself (ie. name and expression)
+                     , ifcEcas ::   [ECArule]
                      , ifcPos ::    Origin
                      , ifcPrp ::    String
                      } deriving Show
+
 instance Eq Interface where
   s==s' = name s==name s'
 instance Identified Interface where
@@ -346,6 +349,68 @@ instance Traced ObjectDef where
   origin = objpos
 
 data SubInterface = Box A_Concept [ObjectDef] | InterfaceRef String deriving (Eq, Show)
+
+data InsDel   = Ins | Del
+                 deriving (Show,Eq)
+data ECArule= ECA { ecaTriggr :: Event       -- The event on which this rule is activated
+                  , ecaDelta ::  Declaration -- The delta to be inserted or deleted from this rule. It actually serves very much like a formal parameter.
+                  , ecaAction :: PAclause    -- The action to be taken when triggered.
+                  , ecaNum ::    Int         -- A unique number that identifies the ECArule within its scope.
+                  }
+                  
+instance Show ECArule where
+  showsPrec _ r = showString ("ON "++show (ecaTriggr r)++" "++show (ecaDelta r)++" do something.")
+
+instance Eq (ECArule) where
+   e==e' = ecaNum e==ecaNum e'
+
+data Event = On { eSrt :: InsDel
+                , eDcl :: Declaration
+                } deriving (Show,Eq)
+
+data PAclause
+              = CHC { paCls :: [PAclause]                 -- precisely one clause is executed.
+                    , paMotiv :: [(Expression,[Rule] )]   -- tells which conjunct from which rule is being maintained
+                    }
+              | GCH { paGCls :: [(InsDel,Expression,PAclause)]    -- guarded choice; The rule is maintained if one of the clauses of which the expression is populated is executed.
+                    , paMotiv :: [(Expression,[Rule] )]   -- tells which conjunct from which rule is being maintained
+                    }
+              | ALL { paCls :: [PAclause]                 -- all clauses are executed.
+                    , paMotiv :: [(Expression,[Rule] )]
+                    }
+              | Do  { paSrt :: InsDel                     -- do Insert or Delete
+                    , paTo :: Declaration                 -- into toExpr    or from toExpr
+                    , paDelta :: Expression               -- delta
+                    , paMotiv :: [(Expression,[Rule] )]
+                    }
+              | New { paCpt :: A_Concept                  -- make a new instance of type c
+                    , paCl :: String->PAclause            -- to be done after creating the concept
+                    , paMotiv :: [(Expression,[Rule] )]
+                    }
+              | Rmv { paCpt :: A_Concept                  -- Remove an instance of type c
+                    , paCl :: String->PAclause            -- to be done afteremoving the concept
+                    , paMotiv :: [(Expression,[Rule] )]
+                    }
+              | Nop { paMotiv :: [(Expression,[Rule] )]   -- tells which conjunct from whichule is being maintained
+                    }
+              | Blk { paMotiv :: [(Expression,[Rule] )]   -- tells which expression from whichule has caused the blockage
+                    }
+              | Let { paExpr :: PAclause                  -- the expression that represents a condition to be tested.
+                    , paBody :: PAclause -> PAclause
+                    , paMotiv :: [(Expression,[Rule] )]
+                    }
+              | Ref { paVar :: String
+                    }
+
+instance Eq PAclause where
+   CHC ds _ == CHC ds' _ = ds==ds'
+   GCH ds _ == GCH ds' _ = ds==ds'
+   ALL ds _ == ALL ds' _ = ds==ds'
+   p@Do{}   ==   p'@Do{} = paSrt p==paSrt p' && paTo p==paTo p' && paDelta p==paDelta p'
+   Nop _    ==     Nop _ = True
+   p@New{}  ==  p'@New{} = paCpt p==paCpt p'
+   p@Rmv{}  ==  p'@Rmv{} = paCpt p==paCpt p'
+   _ == _ = False
 
 -- | Explanation is the intended constructor. It explains the purpose of the object it references.
 --   The enrichment process of the parser must map the names (from PPurpose) to the actual objects
