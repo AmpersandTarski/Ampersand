57d974dd3442ffa2146ac191cebf46e51d88897e
Oblosys <martijn@oblomov.com>
2014-09-23 16:36:01 +0200

    Show git commit info with --version + Cleanup

--- src/Database/Design/Ampersand_Prototype/Version.hs
+++ src/Database/Design/Ampersand_Prototype/Version.hs
@@ -17,11 +17,8 @@ prototypeVersionWithoutBuildtimeStr :: String
 prototypeVersionWithoutBuildtimeStr = prototypeOnlyVersionStr ++ " (lib: "++ampersandVersionWithoutBuildTimeStr++")"
 
 prototypeOnlyVersionStr :: String
-prototypeOnlyVersionStr = "Prototype v"++cabalVersionStr++"."++svnRevisionStr
+prototypeOnlyVersionStr = "Prototype v"++cabalVersionStr++"["++gitInfoStr++"]"
 {-
-   #1.#2.#3.#4 : #1 major version; #2 student release version; #3 production fix version (normally 0 );
-   #4 SVN revision number:
-      - may be a range separated by ':' if the working copy contains mixed revisions (e.g. "163:165")
-      - ends with an 'M' if the working copy was modified (e.g. "163M")
-      - for other (rare) values, see the output of the command 'svnversion --help'
+   #1.#2.#3[$gitInfo] : #1 major version; #2 student release version; #3 production fix version (normally 0 );
+   $gitInfo: "branch:SHA", followed by a '*' if the working copy was dirty: e.g. "master:0eea5e3*" 
 -}
