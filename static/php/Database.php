49a53ad8df22a91a46040aa7c07a3d318d42bd21
Martijn Schrage <martijn@oblomov.com>
2014-10-02 18:18:54 +0200

    For update commands, pass parent and child concepts to php
    Only for update, since we don't seem to need it for the other commands yet.

--- static/php/Database.php
+++ static/php/Database.php
@@ -85,10 +85,11 @@ function processCommand($command)
       break;
     case 'update':
       if (array_key_exists('relation', $command) && array_key_exists('isFlipped', $command) &&
-          array_key_exists('parentAtom', $command) && array_key_exists('childAtom', $command) &&
+          array_key_exists('parentAtom', $command) && array_key_exists('parentConcept', $command) &&
+          array_key_exists('childAtom', $command) && array_key_exists('childConcept', $command) &&
           array_key_exists('parentOrChild', $command) && array_key_exists('originalAtom', $command))
-        editUpdate($command->relation, $command->isFlipped, $command->parentAtom, $command->childAtom
-                  ,$command->parentOrChild, $command->originalAtom);
+        editUpdate($command->relation, $command->isFlipped, $command->parentAtom, $command->parentConcept,
+                   $command->childAtom, $command->childConcept, $command->parentOrChild, $command->originalAtom);
       else 
         error("Command $command->dbCmd is missing parameters");
       break;
@@ -110,7 +111,7 @@ function editAddToConcept($atom, $concept)
 }
 
 // NOTE: if $originalAtom == '', editUpdate means insert
-function editUpdate($rel, $isFlipped, $parentAtom, $childAtom, $parentOrChild, $originalAtom)
+function editUpdate($rel, $isFlipped, $parentAtom, $parentConcept, $childAtom, $childConcept, $parentOrChild, $originalAtom)
 { global $relationTableInfo;
   global $tableColumnInfo;
   
