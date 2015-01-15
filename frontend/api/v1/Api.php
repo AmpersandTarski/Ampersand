d61dcc744d04ada50257c1aeb871bf83c9ccb254
Michiel-s <michielstornebrink@gmail.com>
2015-01-15 20:26:19 +0100

    Cleanup  API

--- frontend/api/v1/Api.php
+++ frontend/api/v1/Api.php
@@ -21,7 +21,7 @@ class Api
 	}
 	
 	/**
-	 * @url GET session/
+	 * @url GET session
 	 */
 	public function getSession(){
 	
@@ -31,14 +31,14 @@ class Api
 	}
 	
 	/**
-	 * @url DELETE session/
+	 * @url DELETE session
 	 * @url DELETE session/{session_id}
 	 */
 	public function destroySession($session_id){
 
 		$session = Session::singleton();
 		$session->destroySession($session_id);
-	
+		
 	}
 
 /**************************** NOTIFICATIONS ****************************/	
@@ -64,11 +64,66 @@ class Api
 	{
 		return (array) $GLOBALS['apps'];
 	}
-		
+
+/**************************** OBJECTINTERFACES ****************************/
+
+	/**
+	 * @url GET interface/{interfaceName}/atom
+	 * @url GET interface/{interfaceName}/atom/{atomid}
+	 * @param string $interfaceName
+	 * @param string $sessionId
+	 * @param string $atomid
+	 * @param int $roleId
+	 */
+	public function getAtom($interfaceName, $sessionId, $atomid = null, $roleId = null)
+	{
+		$session = Session::singleton();
+	
+		if(is_null($atomid)) $atomid = $sessionId;
+	
+		try{
+			$session->setRole($roleId);
+			$session->setInterface($interfaceName);
+		}catch(Exception $e){
+			throw new RestException(404, $e->getMessage());
+		}
+	
+		if(!$session->role->isInterfaceForRole($interfaceName)) throw new RestException(403, 'Interface is not accessible for specified role: '.$session->role->name.' (roleId:' . $roleId .')' );
+	
+		$atom = new Atom($atomid);
+		$interface = new ObjectInterface($interfaceName);
+		return current($atom->getContent($interface));
+	}
+	
+	/**
+	 * @url PATCH interface/{interfaceName}/atom/{atomid}
+	 * @param string $interfaceName
+	 * @param string $atomid
+	 * @param int $roleId
+	 */
+	public function patchAtom($interfaceName, $atomid, $roleId = null, $request_data = null)
+	{
+		$session = Session::singleton();
+		try{
+			$session->setRole($roleId);
+			$session->setInterface($interfaceName);
+		}catch(Exception $e){
+			throw new RestException(404, $e->getMessage());
+		}
+	
+		if(!$session->role->isInterfaceForRole($interfaceName)) throw new RestException(403, 'Interface is not accessible for specified role: '.$session->role->name.' (roleId:' . $roleId .')' );
+	
+		$atom = new Atom($atomid);
+		$interface = new ObjectInterface($interfaceName);
+		 
+		return $atom->patch($interface, $request_data);
+	
+	}
+	
 
 /**************************** CONCEPTS AND ATOMS ****************************/
     /**
-     * @url GET concepts/
+     * @url GET concepts
      */
     public function getConcepts()
     {
@@ -76,7 +131,7 @@ class Api
     }
 	
 	/**
-     * @url GET concept/{concept}/
+     * @url GET concept/{concept}
 	 * @param string $direction the direction of the relations: "from", "to", "both".
      */
     public function getConcept($concept, $direction = "both")
@@ -85,68 +140,15 @@ class Api
     }
 	
 	/**
-     * @url GET concept/{concept}/atoms/
+     * @url GET concept/{concept}/atoms
      */
     public function getAtoms($concept)
     {
         return Concept::getAllAtoms($concept); // "Return list of all atoms for $concept"
     }
     
-    /**
-     * @url GET interface/{interfaceName}/atoms
-     * @url GET interface/{interfaceName}/atom/{atomid}/
-     * @param string $interfaceName
-     * @param string $atomid
-     * @param int $roleId
-     */
-    public function getAtom($interfaceName, $atomid = null, $roleId = null)
-    {
-    	$session = Session::singleton();
-    	 
-    	if(is_null($atomid)) $atomid = session_id(); // TODO: wordt al gedaan door Session->setAtom(), hieronder met session->atom werken?
-    	 
-    	try{
-    		$session->setRole($roleId);
-    		$session->setInterface($interfaceName);
-    	}catch(Exception $e){
-    		throw new RestException(404, $e->getMessage());
-    	}
-    	 
-    	if(!$session->role->isInterfaceForRole($interfaceName)) throw new RestException(403, 'Interface is not accessible for specified role: '.$session->role->name.' (roleId:' . $roleId .')' );
-    	 
-    	$atom = new Atom($atomid);
-    	$interface = new ObjectInterface($interfaceName);
-    	return current($atom->getContent($interface));
-    }
-    
-    /**
-     * @url PATCH interface/{interfaceName}/atom/{atomid}
-     * @param string $interfaceName
-     * @param string $atomid
-     * @param int $roleId
-     */
-    public function patchAtom($interfaceName, $atomid, $roleId = null, $request_data = null)
-    {
-    	$session = Session::singleton();
-    	try{
-    		$session->setRole($roleId);
-    		$session->setInterface($interfaceName);
-    	}catch(Exception $e){
-    		throw new RestException(404, $e->getMessage());
-    	}
-    	 
-    	if(!$session->role->isInterfaceForRole($interfaceName)) throw new RestException(403, 'Interface is not accessible for specified role: '.$session->role->name.' (roleId:' . $roleId .')' );
-    	 
-    	$atom = new Atom($atomid);
-    	$interface = new ObjectInterface($interfaceName);
-    	
-    	return $atom->patch($interface, $request_data);
-    
-    }
-    
-	
 	/**
-     * @url POST concept/{concept}/atom/
+     * @url POST concept/{concept}/atom
 	 * @status 201
      */
 	function postAtom($concept){ 
@@ -156,7 +158,7 @@ class Api
 	}
 
 	/**
-	 * @url DELETE concept/{concept}/atom/{atom}/
+	 * @url DELETE concept/{concept}/atom/{atom}
 	 * @status 204
 	 */
 	function deleteAtom($concept, $atom){ 
@@ -165,69 +167,11 @@ class Api
 		$database->deleteAtom($atom, $concept); // delete atom + all relations with other atoms
 	}
 	
-/**************************** RELATIONS AND LINKS ****************************/
-	
-	/**
-	 * @url POST relation/{relationName}/link
-	 * @status 201
-	 */
-	public function postLink($relation, $srcConcept, $tgtConcept, $srcAtom = null, $tgtAtom = null){
-		$database = Database::singleton();
-	
-		// Check if combination of $relation, $srcConcept, $tgtConcept exists. Otherwise this causes database issues.
-		if (!Relation::isCombination($relation, $srcConcept, $tgtConcept)){
-			// Tip: If you have defined this relation in Ampersand, then you must be sure to also have defined an INTERFACE that uses this relation (or else it does not show up in the PHP relation administration. TODO: create ticket for Prototype generator to fix this.
-			throw new Exception('Cannot find ' . $relation . '[' . $srcConcept . '*' . $tgtConcept.']');
-		}
-		
-		// If srcAtom is not specified, a new atom of srcConcept is created
-		if($srcAtom == null){
-			$srcAtom = $database->addAtomToConcept(Concept::createNewAtom($srcConcept), $srcConcept);
-		}elseif(!Concept::isAtomInConcept($srcAtom, $srcConcept)){
-			$database->addAtomToConcept($srcAtom, $srcConcept);
-		}
-		
-		// If tgtAtom is not specified, a new atom of tgtConcept is created
-		if($tgtAtom == null){
-			$tgtAtom = $database->addAtomToConcept(Concept::createNewAtom($tgtConcept), $tgtConcept);
-		}elseif(!Concept::isAtomInConcept($tgtAtom, $tgtConcept)){
-			$database->addAtomToConcept($tgtAtom, $tgtConcept);
-		}
-		
-		$database->editUpdate($relation, false, $srcAtom, $srcConcept, $tgtAtom, $tgtConcept, 'child', '');
-		
-		ErrorHandling::addLog('Tupple ('.$srcAtom.' - '.$tgtAtom.') inserted into '.$relation.'['.$srcConcept.'*'.$tgtConcept.']');
-		
-		return array('relation' => $relation
-					, 'srcConcept' => $srcConcept
-					, 'tgtConcept' => $tgtConcept
-					, 'srcAtom' => $srcAtom
-					, 'tgtAtom' => $tgtAtom);
-	}
-	
-	/**
-	 * @url DELETE relation/{relationName}/link
-	 * @status 204
-	 */
-	public function deleteLink($relation, $srcConcept, $srcAtom, $tgtConcept, $tgtAtom){
-		$database = Database::singleton();
-		
-		// Check if combination of $relation, $srcConcept, $tgtConcept exists. Otherwise this causes database issues.
-		if (!Relation::isCombination($relation, $srcConcept, $tgtConcept)){
-			// Tip: If you have defined this relation in Ampersand, then you must be sure to also have defined an INTERFACE that uses this relation (or else it does not show up in the PHP relation administration. TODO: create ticket for Prototype generator to fix this.
-			throw new Exception('Cannot find ' . $relation . '[' . $srcConcept . '*' . $tgtConcept.']');
-		}
-		
-		$database->editDelete($relation, false, $srcAtom, $srcConcept, $tgtAtom, $tgtConcept);
-		
-		ErrorHandling::addLog('Tupple ('.$srcAtom.' - '.$tgtAtom.') deleted from '.$relation.'['.$srcConcept.'*'.$tgtConcept.']');
-	}
-	
 /**************************** RULES AND VIOLATIONS ****************************/
 	
 	/**
-     * @url GET rule/
-	 * @url GET rule/{ruleName}/
+     * @url GET rule
+	 * @url GET rule/{ruleName}
      */
     public function getRules($ruleName = NULL)
     {
@@ -240,13 +184,14 @@ class Api
     }
 	
 	/**
-     * @url GET rule/{ruleName}/violations/
+     * @url GET rule/{ruleName}/violations
      */
     public function getViolations($ruleName)
     {
 		$rule = RuleEngine::getRule($ruleName);
         return RuleEngine::checkProcessRule($rule); // "Return list of violations (tuples of src, tgt atom) for rule $rule"
     }
+    
 	
 /**************************** ROLES ****************************/
 	
@@ -261,7 +206,7 @@ class Api
     
     /**
      * @url GET role
-     * @url GET role/{roleNr}/
+     * @url GET role/{roleNr}
      */
     public function getRole($roleNr = NULL)
     {
@@ -273,7 +218,6 @@ class Api
     	}
     }
     
-	
 	
 /**************************** INTERFACES ****************************/
     
@@ -294,8 +238,8 @@ class Api
     }
     
 	/**
-     * @url GET interfaces/
-	 * @url GET interface/{interfaceName}/
+     * @url GET interfaces
+	 * @url GET interface/{interfaceName}
 	 * @param string $interfaceName
 	 * @param int $roleId
      */
@@ -319,22 +263,7 @@ class Api
         }else{
         	return $session->role->getInterfaces();  // "Return list of all interfaces"
 		}
-    }    
-
-
-/**************************** OLD TRANSACTION ****************************/
-	/**
-     * @url POST transaction/
-     */
-    public function processCommands($commands, $role)
-    {
-		$session = Session::singleton();
-		$session->database->transaction(json_decode($commands), $role);
-		
-		return ErrorHandling::getAll();
-
-    }
-	
+    }	
 	
 }
 
