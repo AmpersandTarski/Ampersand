554299278e3dbb732bdc13187184c66691b34aa8
Michiel-s <michielstornebrink@gmail.com>
2015-01-20 16:51:23 +0100

    Changed default Role to the first role specified in Generics.

--- frontend/localSettings.php
+++ frontend/localSettings.php
@@ -5,7 +5,7 @@ require_once (__DIR__.'/ampersand/Generics.php');
 // define ('IP', 'http://localhost/[APP]');
 
 /************ CONFIG ********************/
-define ('DEFAULT_ROLEID', '1');
+define ('DEFAULT_ROLEID', '0');
 
 /************ DB CONFIG ****************/
 $DB_host = 'localhost';
