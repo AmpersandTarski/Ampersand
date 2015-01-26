97e51f6a938ae2439ba45494f02e9d63b587e7e5
Michiel-s <michielstornebrink@gmail.com>
2015-01-26 18:09:48 +0100

    Frontend: improved installer UI

--- static/newFrontend/app/controllers/static_installerController.js
+++ static/newFrontend/app/controllers/static_installerController.js
@@ -1,14 +1,17 @@
 AmpersandApp.controller('static_installerController', ['$scope', '$rootScope', '$routeParams', 'Restangular', function ($scope, $rootScope, $routeParams, Restangular) {
 	
-	$scope.installer = Restangular.one('installer').get().then(function(data) {
-		$rootScope.notifications = data;
-		
-		// refresh session
-		$rootScope.session = Restangular.one('session').get().$object;
-		
-		// refresh interfaces list
-		$rootScope.interfaces = Restangular.all('interfaces/top').getList().$object;
-	});
-
+	$scope.install = function(){
+		if(confirm('Are you sure?')){
+			Restangular.one('installer').get().then(function(data) {
+				$rootScope.notifications = data;
+				
+				// refresh session
+				$rootScope.session = Restangular.one('session').get().$object;
+				
+				// refresh interfaces list
+				$rootScope.interfaces = Restangular.all('interfaces/top').getList().$object;
+			});
+		}
+	}
 	
 }]);
