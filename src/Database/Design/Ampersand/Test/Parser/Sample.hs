250b5ef3fb9b3bf7d3d245dca2f8659003089a2e
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-01-27 22:33:38 +0100

    Checking the parser by reading the given files

--- src/Database/Design/Ampersand/Test/Parser/Sample.hs
+++ src/Database/Design/Ampersand/Test/Parser/Sample.hs
@@ -8,6 +8,7 @@ import Test.QuickCheck
 
 import Database.Design.Ampersand.ADL1.P2A_Converters(Guarded(..),pCtx2aCtx)
 import Database.Design.Ampersand.Core.AbstractSyntaxTree (A_Context(..))
+import Database.Design.Ampersand.Core.ParseTree (P_Context(..))
 import Database.Design.Ampersand.FSpec.ShowADL
 import Database.Design.Ampersand.Input.ADL1.Parser
 import Database.Design.Ampersand.Input.ADL1.UU_Scanner
@@ -21,10 +22,20 @@ options = Options {}
 noCtx :: A_Context
 noCtx = ACtx{ctxnm="NO_CONTEXT"}
 
+unguard :: String -> Guarded (P_Context, [String]) -> Bool
+unguard str guard = case guard of
+   Errors  parseErr -> trace ("Cannot parse: " ++ (show parseErr) ++ ": " ++ str) False
+   Checked (_,_)    -> trace (str ++ " parsed succesfully") True
+
+parseFile :: FilePath -> IO Bool
+parseFile name = do contents <- readFile name
+                    let pResult = runParser pContext name contents
+                    return $ unguard name pResult
+
 parse :: String -> Guarded A_Context
 parse str = let pResult = runParser pContext "Sample.hs" str
             in case pResult of
-               Errors  parseErr -> trace ("Cannot parse: " ++ str) $ Errors parseErr
+               Errors  parseErr -> trace ("Cannot parse " ++ (show parseErr) ++ ": " ++ str) $ Errors parseErr
                Checked (pctx,_) -> pCtx2aCtx options pctx
 
 checkResult :: ShowADL a => Guarded a -> (a -> Bool) -> Bool
@@ -36,7 +47,37 @@ checkResult guard check =
 prop_pretty :: A_Context -> Bool
 prop_pretty xs = checkResult (parse $ showADL xs) (\p -> xs == p)
 
+testScripts :: [String] -> IO Bool
+testScripts [] = return True
+testScripts fs = do
+    parsed <- parseFile (head fs)
+    if parsed then testScripts (tail fs)
+    else return False
+
+scripts :: [FilePath]
+scripts = [ "../ampersand-models/Misc/Arbeidsduur.adl"
+          , "../ampersand-models/Misc/ArchiTest1.adl"
+          , "../ampersand-models/Misc/ArchiTest3.adl"
+          , "../ampersand-models/Misc/ArchiTest4.adl"
+          , "../ampersand-models/Misc/ArchiTest5.adl"
+          , "../ampersand-models/Misc/ARM-Test1.adl"
+          , "../ampersand-models/Misc/ARM-Test2.adl"
+          , "../ampersand-models/Misc/ARM20-Test1.adl"
+          , "../ampersand-models/Misc/ARM20-Test2.adl"
+          , "../ampersand-models/Misc/ARM20-Test3.adl"
+          , "../ampersand-models/Misc/ARM20-Test4.adl"
+          , "../ampersand-models/Misc/ARM20-Test5.adl"
+          , "../ampersand-models/Misc/ARM20-Test6.adl"
+          , "../ampersand-models/Misc/ARM20-Test7.adl"
+          , "../ampersand-models/Misc/ARM20-Test8.adl"
+          , "../ampersand-models/Misc/FraakTest1.adl"
+          , "../ampersand-models/Misc/FraakTest2.adl"
+          , "../ampersand-models/Misc/Hello.adl"
+          , "../ampersand-models/Misc/Kernmodel.adl"]
+
 runTests :: IO ()
-runTests = do quickCheck prop_pretty
+runTests = do success <- testScripts scripts
+              if success then quickCheck prop_pretty
+              else return ()
               --- quickCheck prop_2
 -- main = $quickCheckAll >>= print
