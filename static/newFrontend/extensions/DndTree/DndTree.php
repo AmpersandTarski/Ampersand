284ded35eaa4ece0395e8bb830760877dd0b4928
Michiel-s <michielstornebrink@gmail.com>
2015-01-27 15:13:07 +0100

    Minor bug fix for dndTree extension

--- static/newFrontend/extensions/DndTree/DndTree.php
+++ static/newFrontend/extensions/DndTree/DndTree.php
@@ -4,3 +4,4 @@ $GLOBALS['hooks']['after_Viewer_load_cssFiles'][] = 'extensions/DndTree/ui/css/s
 $GLOBALS['hooks']['after_Viewer_load_angularScripts'][] = 'extensions/DndTree/ui/js/DndTreeController.js';
 $GLOBALS['hooks']['after_Viewer_load_angularScripts'][] = 'http://d3js.org/d3.v3.min.js';
 $GLOBALS['hooks']['after_Viewer_load_angularScripts'][] = 'extensions/DndTree/lib/dndTree.js';
+?>
