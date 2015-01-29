2140183bc43a991b3fddd66d627f8770141bc6d5
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-01-30 00:28:58 +0100

    Pretty printing processes, terms and rules

--- src/Database/Design/Ampersand/ADL1/PrettyPrinters.hs
+++ src/Database/Design/Ampersand/ADL1/PrettyPrinters.hs
@@ -2,9 +2,12 @@ module Database.Design.Ampersand.ADL1.PrettyPrinters
 where
 
 import Database.Design.Ampersand.Core.ParseTree
+import Data.Char
 
 (<+>) :: String -> String -> String
-(<+>) a b = a ++ " " ++ b
+(<+>) a b = a ++ space ++ b
+          where space = if null (a) || isSpace(last a) then ""
+                        else " "
 
 (<~>) :: Pretty b => String -> b -> String
 (<~>) a b = a <+> pretty b
@@ -34,11 +37,11 @@ instance Pretty a => Pretty (Maybe a) where
 instance Pretty P_Context where
     pretty p = "CONTEXT" <+> ctx_nm p <~> ctx_lang p <+\>
                perline (ctx_metas p) <+>
-               perline (ctx_ps p)
+               perline (ctx_ps p) <+\>
+               perline (ctx_PPrcs p)
          --, ctx_markup :: Maybe PandocFormat  -- ^ The default markup format for free text in this context
          --, ctx_thms ::   [String]         -- ^ Names of patterns/processes to be printed in the functional specification. (For partial documents.)
          --, ctx_pats ::   [P_Pattern]      -- ^ The patterns defined in this context
-         --, ctx_PPrcs ::  [P_Process]      -- ^ The processes as defined by the parser
          --, ctx_rs ::     [(P_Rule TermPrim)]         -- ^ All user defined rules in this context, but outside patterns and outside processes
          --, ctx_ds ::     [P_Declaration]  -- ^ The relations defined in this context, outside the scope of patterns
          --, ctx_cs ::     [ConceptDef]     -- ^ The concept definitions defined in this context, outside the scope of patterns
@@ -46,11 +49,9 @@ instance Pretty P_Context where
          --, ctx_vs ::     [P_ViewDef]      -- ^ The view definitions defined in this context, outside the scope of patterns
          --, ctx_gs ::     [P_Gen]          -- ^ The gen definitions defined in this context, outside the scope of patterns
          --, ctx_ifcs ::   [P_Interface]    -- ^ The interfaces defined in this context
-         --,        -- ^ The purposes defined in this context, outside the scope of patterns and processes
          --, ctx_pops ::   [P_Population]   -- ^ The populations defined in this context
          --, ctx_sql ::    [P_ObjectDef]    -- ^ user defined sqlplugs, taken from the Ampersand script
          --, ctx_php ::    [P_ObjectDef]    -- ^ user defined phpplugs, taken from the Ampersand script
-         --, 
 
 instance Pretty Meta where
     pretty p = "META" <~> mtObj p <+> quote mtName p <+> quote mtVal p
@@ -59,7 +60,18 @@ instance Pretty MetaObj where
     pretty _ = ""
 
 instance Pretty P_Process where
-    pretty p = show p
+    pretty p = "PROCESS" <+> procNm p <+\>
+               perline (procRules p) <+\>
+               perline (procGens p) <+\>
+               perline (procDcls p) <+\>
+               perline (procRRuls p) <+\>
+               perline (procRRels p) <+\>
+               perline (procCds p) <+\>
+               perline (procIds p) <+\>
+               perline (procVds p) <+\>
+               perline (procXps p) <+\>
+               perline (procPop p) <+\>
+               "ENDPROCESS"
 
 instance Pretty P_RoleRelation where
     pretty p = show p
@@ -73,8 +85,28 @@ instance Pretty P_Pattern where
 instance Pretty P_Declaration where
     pretty p = show p
 
-instance Pretty (Term a) where
-    pretty _ = ""
+instance Pretty a => Pretty (Term a) where
+   pretty p = case p of
+       Prim a -> pretty a
+       PEqu _ t1 t2 -> two t1 t2 "="
+       PImp _ t1 t2 -> two t1 t2 "|-"
+       PIsc _ t1 t2 -> two t1 t2 "/\\"
+       PUni _ t1 t2 -> two t1 t2 "\\/"
+       PDif _ t1 t2 -> two t1 t2 "-"
+       PLrs _ t1 t2 -> two t1 t2 "/"
+       PRrs _ t1 t2 -> two t1 t2 "\\"
+       PDia _ t1 t2 -> two t1 t2 "<>"
+       PCps _ t1 t2 -> two t1 t2 ";"
+       PRad _ t1 t2 -> two t1 t2 "!"
+       PPrd _ t1 t2 -> two t1 t2 "*"
+       PKl0 _ t -> pos t "*"
+       PKl1 _ t -> pos t "+"
+       PFlp _ t -> pos t "~"
+       PCpl _ t -> pre t "-"
+       PBrk _ t -> "( " ++ pretty t ++ " )"
+       where pos t op     = "" ++ pretty t ++ op ++ ""
+             pre t op     = "" ++ op ++ pretty t ++ ""
+             two t1 t2 op = "(" ++ pretty t1 ++ op ++ pretty t2 ++ ")"
 
 instance Pretty TermPrim where
     pretty (PI _) = "I"
@@ -86,23 +118,27 @@ instance Pretty TermPrim where
     pretty (Prel _ str) = str
     pretty (PTrel _ str sign) = str <~> sign
 
-instance Pretty (PairView a) where
+instance Pretty a => Pretty (PairView a) where
     pretty _ = ""
 
-instance Pretty (PairViewSegment a) where
+instance Pretty a => Pretty (PairViewSegment a) where
     pretty _ = ""
 
-instance Pretty (PairViewTerm a) where
+instance Pretty a => Pretty (PairViewTerm a) where
     pretty _ = ""
 
-instance Pretty (PairViewSegmentTerm a) where
+instance Pretty a => Pretty (PairViewSegmentTerm a) where
     pretty _ = ""
 
 instance Pretty SrcOrTgt where
     pretty p = show p
 
-instance Pretty (P_Rule a) where
-    pretty _ = ""
+instance Pretty a => Pretty (P_Rule a) where
+    pretty p = "RULE" <+> (if null (rr_nm p) then "" else (rr_nm p) ++ ":") <~\>
+               rr_exp p <+\>
+               perline (rr_mean p) <+\>
+               perline (rr_msg p) <~\>
+               rr_viol p
 
 instance Pretty ConceptDef where
     pretty p = show p
