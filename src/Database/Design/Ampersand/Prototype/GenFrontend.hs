676713a96482c0d9c2fb2c9ed15fbd145e1884c2
Martijn Schrage <martijn@oblomov.com>
2015-02-04 14:06:52 +0100

    Simplify intermediate datastructure

--- src/Database/Design/Ampersand/Prototype/GenFrontend.hs
+++ src/Database/Design/Ampersand/Prototype/GenFrontend.hs
@@ -1,6 +1,7 @@
 module Database.Design.Ampersand.Prototype.GenFrontend (doGenFrontend) where
 
 import Prelude hiding (putStrLn,readFile)
+import Data.List
 import Data.Maybe
 import System.FilePath
 import Text.StringTemplate
@@ -24,45 +25,38 @@ doGenFrontend fSpec =
 -- TODO: interface ref: Editable relations from interface def of ref, or origin?
 --       both may result in unexpected editability. Maybe take intersection?
 
-data FEInterface = FEInterface String FEObject deriving Show
-
-data FEObject = FEObject String FESubinterface deriving Show
-
--- subinterface/subobject?
-data FESubinterface = FEBox (Maybe String) [FEObject] | FEAtoms deriving Show
+data FEInterface = FEBox    { ifcName :: String, ifcMClass :: Maybe String, ifcSubIfcs :: [FEInterface] }
+                 | FEAtomic { ifcName :: String } deriving Show
 
 buildInterfaces :: FSpec -> [FEInterface]
 buildInterfaces fSpec = map (buildInterface fSpec) $ interfaceS fSpec
 
 buildInterface :: FSpec -> Interface -> FEInterface
-buildInterface fSpec interface = FEInterface (name interface) $ buildObject fSpec {- (ifcParams interface) -} (ifcObj interface)
-    
+buildInterface fSpec interface = buildObject fSpec {- (ifcParams interface) -} (ifcObj interface)
+-- TODO: handle editable rels here, and maybe interface class
 
-buildObject :: FSpec -> ObjectDef -> FEObject
-buildObject fSpec object = FEObject (name object) $ buildSubinterface fSpec (objmsub object)
-
-buildSubinterface :: FSpec -> Maybe SubInterface -> FESubinterface
-buildSubinterface fSpec mSubIfc =
-  case mSubIfc of
+buildObject :: FSpec -> ObjectDef -> FEInterface
+buildObject fSpec object =
+  case objmsub object of
     Just (InterfaceRef nm) -> case filter (\ifc -> name ifc == nm) $ interfaceS fSpec of -- Follow interface refs
-                                [i] -> buildSubinterface fSpec (objmsub $ ifcObj i) -- TODO: skip interface root object, what about exp and editable rels?
+                                [i] -> buildInterface fSpec i -- TODO: skip interface root object, what about exp and editable rels?
                                 []  -> fatal 44 $ "Referenced interface " ++ nm ++ " missing"
                                 _   -> fatal 45 $ "Multiple declarations of referenced interface " ++ nm
-    Just (Box _ cl objects) -> FEBox cl $ map (buildObject fSpec) objects
-    Nothing                 -> FEAtoms
-
-
-
+    Just (Box _ cl objects) -> FEBox    (name object) cl $ map (buildObject fSpec) objects
+    Nothing                 -> FEAtomic (name object)
+  
+  
 traverseInterfaces :: FSpec -> [FEInterface] -> IO ()
 traverseInterfaces fSpec ifcs =
  do { putStrLn $ show $ map name (interfaceS fSpec)
-    ; mapM_ (traverseInterface fSpec) $ ifcs
+    ; mapM_ (traverseTopLevelInterface fSpec) $ ifcs
     }
 
-traverseInterface :: FSpec -> FEInterface -> IO ()
-traverseInterface fSpec (FEInterface interfaceName obj) =
+traverseTopLevelInterface :: FSpec -> FEInterface -> IO ()
+traverseTopLevelInterface _     (FEAtomic _)                  =  fatal 57 $ "Unexpected atomic top-level interface"
+traverseTopLevelInterface fSpec ifc@(FEBox interfaceName _ _) =
  do { verboseLn (getOpts fSpec) $ "\nTop-level interface: " ++ interfaceName
-    ; lns <- traverseObject fSpec 0 Nothing obj
+    ; lns <- traverseInterface fSpec 0 ifc
     ; template <- readTemplate fSpec "views/TopLevelInterface.html"
     ; let contents = render $ setAttribute "ampersandVersionStr" ampersandVersionStr
                             $ setAttribute "interfaceName"       interfaceName
@@ -73,48 +67,41 @@ traverseInterface fSpec (FEInterface interfaceName obj) =
     ; writePrototypeFile fSpec ("views" </> filename) $ contents 
     }
 
-traverseObject :: FSpec -> Int -> Maybe String -> FEObject -> IO [String]
-traverseObject fSpec depth mParentClass (FEObject nm subifc) =
- do { verboseLn (getOpts fSpec) $ replicate depth ' ' ++ nm
-
-    ; lns <- traverseSubinterface fSpec depth subifc
-
-    ; let clss = fromMaybe "BOX" mParentClass
-    ; template <- readTemplate fSpec $ "views/Box-" ++ clss ++ "-child.html"
-    ; let contents = render $ setAttribute "contents" (unlines $ indent 2 lns)
-                            $ template
-    
-    ; return $ lines contents
-    }
-
-traverseSubinterface :: FSpec -> Int -> FESubinterface -> IO [String]
-traverseSubinterface fSpec depth subIntf =
+traverseInterface :: FSpec -> Int -> FEInterface -> IO [String]
+traverseInterface fSpec depth subIntf =
   case subIntf of
-    FEAtoms ->
-     do { template <- readTemplate fSpec $ "views/Atoms-DEFAULT-parent.html"
+    FEAtomic _ ->
+     do { template <- readTemplate fSpec $ "views/Atomic.html"
         ; let contents = render $ template
+        ; putStrLn $ "ATOMIC contents" ++ show contents
+        ; return $ lines contents
+        }
+    FEBox _ mClass ifcs ->
+     do { verboseLn (getOpts fSpec) $ replicate depth ' ' ++ "BOX" ++ maybe "" (\c -> "<"++c++">") mClass
+
+        ; childLnss <- mapM (traverseInterface fSpec (depth + 1)) ifcs
+
+        ; let clss = maybe "" (\cl -> "-" ++ cl) mClass
+        ; childTemplate <- readTemplate fSpec $ "views/Box" ++ clss ++ "-child.html"
+        
+        ; let wrappedChildLnss = map (wrapChild childTemplate) childLnss 
+        ; putStrLn $ "wrapped child contents" ++ show wrappedChildLnss
+        
+        ; parentTemplate <- readTemplate fSpec $ "views/Box" ++ clss ++ "-parent.html"
+        ; let contents = render $ setAttribute "class"    clss
+                                $ setAttribute "contents" (intercalate "\n" $ indent 2 $ concat wrappedChildLnss)
+                                $ parentTemplate
+
+        ; putStrLn $ "BOX contents" ++ show contents
 
         ; return $ lines contents
         }
-    FEBox mCl objects ->
-     do { verboseLn (getOpts fSpec) $ replicate depth ' ' ++ "BOX" ++ maybe "" (\c -> "<"++c++">") mCl
-        ; let clss = fromMaybe "BOX" mCl
-
-        ; lnss <- mapM (traverseObject fSpec (depth + 1) mCl) objects
-
-        ; template <- readTemplate fSpec $ "views/Box-" ++ clss ++ "-parent.html"
-        ; let contents = render $ setAttribute "class"    clss
-                                $ setAttribute "contents" (unlines $ indent 2 $ concat lnss)
-                                $ template
-
-        ; return $ lines contents
-        } {-
-  where wrapChild :: StringTemplate String -> [String] -> [String]
-        wrapChild childTemplate childLns = lines $
-          render $ setAttribute "contents" (unlines $ indent 2 childLns)
-                 $ childTemplate
-  -}
-
+      where wrapChild :: StringTemplate String -> [String] -> [String]
+            wrapChild childTemplate childLns = trace ("childLns"++show childLns++"\nunlines indent "++show (unlines $ indent 2 childLns)) $ lines $
+              render $ setAttribute "contents" (intercalate "\n" $ indent 2 childLns)
+                     $ childTemplate
+      
+        
 readTemplate :: FSpec -> String -> IO (StringTemplate String)
 readTemplate fSpec templatePath =
  do { res <- readUTF8File $ (getTemplateDir fSpec) </> templatePath
