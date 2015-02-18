589660a8b3d4efeac03f2d9e5e92b43a16f3b167
Maarten Baertsoen <maarten.baertsoen@telenet.be>
2015-02-18 15:04:05 +0100

    Addition of 2 Toker types for conversion purposes

--- SO_UU_scanner/LexerToken.hs
+++ SO_UU_scanner/LexerToken.hs
@@ -6,27 +6,75 @@ import Text.Parsec.Pos(SourcePos)
 
 --type Token = (SourcePos,Tok,String)
 
-data Token = Tokv { tokenT  :: Tok
-                 , sp      :: SourcePos
-                 , val     :: String
-                 }
+-- Original Token structure that will be replaced with the new ParsecT structure 
+-- 
 
-data Tok = TkSymbol String
-         | TkOp String
-         | TkKeyword String
-         | TkString String
-         | TkExpl String
-         | TkAtom String
-         | TkChar Char
-         | TkInteger Int
-         | TkUpperId String
-         | TkLowerId String
-         | TkTextName String
-         | TkTextLine String
-         | TkSpace
-    deriving (Show, Eq)
-	
+type Line = Int
+type Column = Int
+
+data Pos = Pos{line:: !Line, column:: !Column} deriving (Eq, Ord, Show)
+type Filename   = String
+
+data TokenType
+  = TkSymbol
+  | TkVarid
+  | TkConid
+  | TkKeyword
+  | TkOp
+  | TkString
+  | TkExpl
+  | TkAtom
+  | TkChar
+  | TkInteger8
+  | TkInteger10
+  | TkInteger16
+  | TkTextnm
+  | TkTextln
+  | TkSpace
+  | TkError
+  deriving (Eq, Ord)
+
+data Token = Tok { tp' :: TokenType
+                 , val1 :: String
+                 , val2 :: String
+                 , pos :: !Pos
+                 , file :: !Filename
+                 }
+				 
 instance Show Token where
+  showsPrec _ token'
+    = showString
+       (case token' of
+        (Tok TkSymbol    _  s2 i fn)  -> "symbol "                ++ s2         ++ maybeshow i fn
+        (Tok TkOp        _  s2 i fn)  -> "operator "              ++ s2         ++ maybeshow i fn
+        (Tok TkKeyword   _  s2 i fn)  ->                        show s2         ++ maybeshow i fn
+        (Tok TkString    _  s2 i fn)  -> "string \""              ++ s2 ++ "\"" ++ maybeshow i fn
+        (Tok TkExpl      _  s2 i fn)  -> "explanation {+"         ++ s2 ++ "-}" ++ maybeshow i fn
+        (Tok TkAtom      _  s2 i fn)  -> "atom '"                 ++ s2 ++ "'"  ++ maybeshow i fn
+        (Tok TkChar      _  s2 i fn)  -> "character '"            ++ s2 ++ "'"  ++ maybeshow i fn
+        (Tok TkInteger8  _  s2 i fn)  -> "octal integer "         ++ s2         ++ maybeshow i fn
+        (Tok TkInteger10 _  s2 i fn)  -> "decimal Integer "       ++ s2         ++ maybeshow i fn
+        (Tok TkInteger16 _  s2 i fn)  -> "hexadecimal integer "   ++ s2         ++ maybeshow i fn
+        (Tok TkVarid     _  s2 i fn)  -> "lower case identifier " ++ s2         ++ maybeshow i fn
+        (Tok TkConid     _  s2 i fn)  -> "upper case identifier " ++ s2         ++ maybeshow i fn
+        (Tok TkTextnm    _  s2 i fn)  -> "text name "             ++ s2         ++ maybeshow i fn
+        (Tok TkTextln    _  s2 i fn)  -> "text line "             ++ s2         ++ maybeshow i fn
+        (Tok TkSpace     _  _  i fn)  -> "spaces "                              ++ maybeshow i fn
+        (Tok TkError     _  s2 i fn)  -> "error in scanner: "     ++ s2         ++ maybeshow i fn
+       )
+
+	   
+
+-- Parsec Token structure is introduced as a replacement of the original Token structure
+-- 
+
+data TokenP = Tokv { lexeme  :: Lexeme
+                   , sp      :: SourcePos
+                   , val     :: String
+                   }
+
+	
+instance Show TokenP where
   showsPrec _ token'
     = showString
        (case token' of
@@ -44,3 +92,43 @@ instance Show Token where
         (Tokv (TkTextLine val)  sp tval)  -> "text line "             ++ val         ++ show sp ++ tval
         (Tokv (TkSpace)         sp tval)  -> "spaces "                               ++ show sp ++ tval
        )
+
+data Lexeme  = TkSymbol String
+             | TkOp String
+             | TkKeyword String
+             | TkString String
+             | TkExpl String
+             | TkAtom String
+             | TkChar Char
+             | TkInteger Int
+             | TkUpperId String
+             | TkLowerId String
+             | TkTextName String
+             | TkTextLine String
+             | TkSpace
+        deriving (Show, Eq)
+	
+	
+instance Show lexeme where 
+    show x = case x of 
+         LexChar    c        -> Texts.parserCharacterLiteral      ++ " '" ++ c      ++ "'" 
+         LexString  s        -> Texts.parserStringLiteral         ++ " \""++ s      ++ "\"" 
+         LexInt     i        -> Texts.parserIntegerLiteral        ++ " '" ++ i      ++ "'" 
+         LexFloat   f        -> Texts.parserFloatLiteral          ++ " '" ++ f      ++ "'" 
+ 
+ 
+         LexVar     n        -> Texts.parserVariable              ++ " '" ++ n      ++ "'" 
+         LexVarSym  o        -> Texts.parserOperator              ++ " '" ++ o      ++ "'" 
+         LexCon     c        -> Texts.parserConstructor           ++ " '" ++ c      ++ "'" 
+         LexConSym  o        -> Texts.parserConstructorOperator   ++ " '" ++ o      ++ "'" 
+          
+         LexKeyword kwd      -> Texts.parserKeyword ++ " '" ++ kwd ++ "'" 
+         LexResVarSym s      -> "'" ++ s ++ "'" 
+         LexResConSym s      -> "'" ++ s ++ "'" 
+         LexSpecial c        -> "'" ++ [c] ++ "'" 
+          
+         LexInsertedOpenBrace  -> Texts.parserInsertedLBrace  
+         LexInsertedCloseBrace -> Texts.parserEndOfBlock 
+         LexInsertedSemicolon  -> Texts.parserNextInBlock 
+                          
+         LexEOF              -> Texts.parserEndOfFile 
