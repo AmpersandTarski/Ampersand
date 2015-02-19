7b961dafd8078cadba4d41ec018ea0552f4048e5
Maarten Baertsoen <maarten.baertsoen@telenet.be>
2015-02-19 11:12:14 +0100

    Introduction of generic token to abstract the Lexer engine from the output token structure

--- src/Database/Design/Ampersand/Input/ADL1/LexerToken.hs
+++ src/Database/Design/Ampersand/Input/ADL1/LexerToken.hs
@@ -4,14 +4,70 @@ where
 
 import Text.Parsec.Pos(SourcePos)
 
--- Original Token structure that will be replaced with the new ParsecT structure 
--- Functions based on the old Token structure are beneath the data and type declarations
-
 type Line = Int
 type Column = Int
 
 data Pos = Pos{line:: !Line, column:: !Column} deriving (Eq, Ord, Show)
 type Filename   = String
+
+-- GenToken out of which different token formats can be generated
+-- The generic token abtracts from the main lexer logic and the output format to make the scanner easy maintainable when another TokenType is needed
+data GTokenTypeTokenType
+  = GtkSymbol
+  | GtkVarid
+  | GtkConid
+  | GtkKeyword
+  | GtkOp
+  | GtkString
+  | GtkExpl
+  | GtkAtom
+  | GtkChar
+  | GtkInteger8
+  | GtkInteger10
+  | GtkInteger16
+  | GtkTextnm
+  | GtkTextln
+  | GtkSpace
+  | GtkError
+  deriving (Eq, Ord)
+
+data GenToken = GenTok { tp'    :: TokenType
+                       , val    :: String
+                       , pos    :: !Pos
+                       , file   :: !Filename
+                       }
+
+instance Show GenToken where
+  showsPrec _ gentoken'
+    = showString
+       (case gentoken' of
+        (GenTok GtkSymbol    s2 i fn)  -> "symbol "                ++ s2         ++ maybeshow i fn
+        (GenTok GtkOp        s2 i fn)  -> "operator "              ++ s2         ++ maybeshow i fn
+        (GenTok GtkKeyword   s2 i fn)  ->                        show s2         ++ maybeshow i fn
+        (GenTok GtkString    s2 i fn)  -> "string \""              ++ s2 ++ "\"" ++ maybeshow i fn
+        (GenTok GtkExpl      s2 i fn)  -> "explanation {+"         ++ s2 ++ "-}" ++ maybeshow i fn
+        (GenTok GtkAtom      s2 i fn)  -> "atom '"                 ++ s2 ++ "'"  ++ maybeshow i fn
+        (GenTok GtkChar      s2 i fn)  -> "character '"            ++ s2 ++ "'"  ++ maybeshow i fn
+        (GenTok GtkInteger8  s2 i fn)  -> "octal integer "         ++ s2         ++ maybeshow i fn
+        (GenTok GtkInteger10 s2 i fn)  -> "decimal Integer "       ++ s2         ++ maybeshow i fn
+        (GenTok GtkInteger16 s2 i fn)  -> "hexadecimal integer "   ++ s2         ++ maybeshow i fn
+        (GenTok GtkVarid     s2 i fn)  -> "lower case identifier " ++ s2         ++ maybeshow i fn
+        (GenTok GtkConid     s2 i fn)  -> "upper case identifier " ++ s2         ++ maybeshow i fn
+        (GenTok GtkTextnm    s2 i fn)  -> "text name "             ++ s2         ++ maybeshow i fn
+        (GenTok GtkTextln    s2 i fn)  -> "text line "             ++ s2         ++ maybeshow i fn
+        (GenTok GtkSpace     _  i fn)  -> "spaces "                              ++ maybeshow i fn
+        (GenTok GtkError     s2 i fn)  -> "error in scanner: "     ++ s2         ++ maybeshow i fn
+       )
+
+maybeshow :: Pos -> Filename -> String
+maybeshow (Pos 0 0) _  =  ""
+maybeshow (Pos l c) fn =  " at line " ++ show l
+                       ++ ", column " ++ show c
+                       ++ " of file " ++ show fn	
+					   
+-- Original Token structure that will be replaced with the new ParsecT structure 
+-- Functions based on the old Token structure are beneath the data and type declarations
+
 
 data TokenType
   = TkSymbol
@@ -60,17 +116,12 @@ instance Show Token where
         (Tok TkSpace     _  _  i fn)  -> "spaces "                              ++ maybeshow i fn
         (Tok TkError     _  s2 i fn)  -> "error in scanner: "     ++ s2         ++ maybeshow i fn
        )
-
-maybeshow :: Pos -> Filename -> String
-maybeshow (Pos 0 0) _  =  ""
-maybeshow (Pos l c) fn =  " at line " ++ show l
-                       ++ ", column " ++ show c
-                       ++ " of file " ++ show fn	   
+   
 
 -- Parsec Token structure is introduced as a replacement of the original Token structure
 -- 
 
-data TokenP = Tokv { lexeme  :: Lexeme
+data TokenL = TokL { lexeme  :: Lexeme
                    , sp      :: SourcePos
                    , val     :: String
                    }
@@ -80,19 +131,19 @@ instance Show TokenP where
   showsPrec _ token'
     = showString
        (case token' of
-        (Tokv (LexSymbol val)    sp tval)  -> "symbol "                ++      val             ++ show sp ++ tval
-        (Tokv (LexOp val)        sp tval)  -> "operator "              ++      val             ++ show sp ++ tval
-        (Tokv (LexKeyword val)   sp tval)  ->                             show val             ++ show sp ++ tval
-        (Tokv (LexString val)    sp tval)  -> "string \""              ++      val     ++ "\"" ++ show sp ++ tval
-        (Tokv (LexExpl val)      sp tval)  -> "explanation {+"         ++      val     ++ "-}" ++ show sp ++ tval
-        (Tokv (LexAtom val)      sp tval)  -> "atom '"                 ++      val     ++ "'"  ++ show sp ++ tval
-        (Tokv (LexChar val)      sp tval)  -> "character '"            ++ show val     ++ "'"  ++ show sp ++ tval
-        (Tokv (LexInteger val)   sp tval)  -> "decimal Integer "       ++ show val             ++ show sp ++ tval
-        (Tokv (LexLowerId val)   sp tval)  -> "lower case identifier " ++      val             ++ show sp ++ tval
-        (Tokv (LexUpperId val)   sp tval)  -> "upper case identifier " ++      val             ++ show sp ++ tval
-        (Tokv (LexTextName val)  sp tval)  -> "text name "             ++      val             ++ show sp ++ tval
-        (Tokv (LexTextLine val)  sp tval)  -> "text line "             ++      val             ++ show sp ++ tval
-        (Tokv (LexSpace)         sp tval)  -> "spaces "                                        ++ show sp ++ tval
+        (TokL (LexSymbol val)    sp tval)  -> "symbol "                ++      val             ++ show sp ++ tval
+        (TokL (LexOp val)        sp tval)  -> "operator "              ++      val             ++ show sp ++ tval
+        (TokL (LexKeyword val)   sp tval)  ->                             show val             ++ show sp ++ tval
+        (TokL (LexString val)    sp tval)  -> "string \""              ++      val     ++ "\"" ++ show sp ++ tval
+        (TokL (LexExpl val)      sp tval)  -> "explanation {+"         ++      val     ++ "-}" ++ show sp ++ tval
+        (TokL (LexAtom val)      sp tval)  -> "atom '"                 ++      val     ++ "'"  ++ show sp ++ tval
+        (TokL (LexChar val)      sp tval)  -> "character '"            ++ show val     ++ "'"  ++ show sp ++ tval
+        (TokL (LexInteger val)   sp tval)  -> "decimal Integer "       ++ show val             ++ show sp ++ tval
+        (TokL (LexLowerId val)   sp tval)  -> "lower case identifier " ++      val             ++ show sp ++ tval
+        (TokL (LexUpperId val)   sp tval)  -> "upper case identifier " ++      val             ++ show sp ++ tval
+        (TokL (LexTextName val)  sp tval)  -> "text name "             ++      val             ++ show sp ++ tval
+        (TokL (LexTextLine val)  sp tval)  -> "text line "             ++      val             ++ show sp ++ tval
+        (TokL (LexSpace)         sp tval)  -> "spaces "                                        ++ show sp ++ tval
        )
 
 data Lexeme  = LexSymbol      String
