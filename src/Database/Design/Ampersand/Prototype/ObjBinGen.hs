20a97d7888324abbea3328a62b9ccad39e6076d5
Martijn Schrage <martijn@oblomov.com>
2015-02-19 20:09:27 +0100

    Remove timestamp-dependent static file updates
    It leads to problems when switching between the old and new front-ends (index.html will get stuck on the new front-end version), and I seem to be the only one who was using this anyway. The functionality that only non-existent statics were written was already removed in the past (not sure why, but I'll leave it like that)

--- src/Database/Design/Ampersand/Prototype/ObjBinGen.hs
+++ src/Database/Design/Ampersand/Prototype/ObjBinGen.hs
@@ -13,13 +13,6 @@ import Database.Design.Ampersand.Prototype.CoreImporter
 import Prelude hiding (writeFile,readFile,getContents)
 import Database.Design.Ampersand.Prototype.StaticFiles_Generated
 
-#ifdef MIN_VERSION_unix
-import System.Posix.Files (setFileTimes) -- If unix is not available, we are on windows and cannot set file timestamps 
-
-import Data.Time.Format -- not unix specific, but only needed if we set file timestamps
-import System.Locale    -- 
-#endif
-
 phpObjInterfaces :: FSpec -> IO()
 phpObjInterfaces fSpec =
  do { writeStaticFiles (getOpts fSpec)
@@ -69,58 +62,15 @@ doGenAtlas fSpec =
 writeStaticFiles :: Options -> IO()
 writeStaticFiles opts =
   if genStaticFiles opts
-  then
- do {
-#ifdef MIN_VERSION_unix
-      verboseLn opts "Updating static files"
-#else
-      verboseLn opts "Writing static files"
-#endif
-    ; sequence_ [ writeWhenMissingOrOutdated opts sf (writeStaticFile opts sf) 
-                | sf@SF{isNewFrontend=isNew} <- allStaticFiles, isNew == newFrontend opts
-                ]
-    }
-  else
-      verboseLn opts "Skipping static files (because of command line argument)"
-
-writeWhenMissingOrOutdated :: Options -> StaticFile -> IO () -> IO ()
-writeWhenMissingOrOutdated opts staticFile writeFileAction =
-#ifdef MIN_VERSION_unix
--- On Mac/Linux we set the modification time for generated static files to the modification time of the compiled versions
--- in StaticFiles_Generated.hs. This allows us to only replace those static files that are outdated (or missing.) 
- do { exists <- doesFileExist $ absFilePath opts staticFile
-    --; verboseLn opts $ "  Processing static file "++ filePath staticFile
-    ; if exists then
-       do { oldTimeStampUTC <- getModificationTime $ absFilePath opts staticFile
-          ; let oldTimeStamp = read $ formatTime defaultTimeLocale "%s" oldTimeStampUTC -- convert to epoch seconds
-          ; if oldTimeStamp < timeStamp staticFile then
-              do { verboseLn opts $ "  Replacing static file "++ filePath staticFile ++" with current version."
-                 ; writeFileAction
-                 }
-            else
-              return () -- skip is not really worth logging
-          }
-      else
-       do { verboseLn opts $ "  Writing static file "++ filePath staticFile
-          ; writeFileAction
-          }
-    }
-#else
--- On windows we cannot set the file modification time without requiring a cygwin or mingw build environment,
--- so we simply replace all static files on each generation.
- do { verboseLn opts $ "  Writing static file "++ filePath staticFile
-    ; writeFileAction
-    }
-#endif
+  then sequence_ [ writeStaticFile opts sf 
+                 | sf@SF{isNewFrontend=isNew} <- allStaticFiles, isNew == newFrontend opts
+                 ]
+  else verboseLn opts "Skipping static files (because of command line argument)"
 
 writeStaticFile :: Options -> StaticFile -> IO()
 writeStaticFile opts sf =
   do { createDirectoryIfMissing True (takeDirectory (absFilePath opts sf))
      ; write (absFilePath opts sf) (contentString sf)
-#ifdef MIN_VERSION_unix
-     ; let t = (fromIntegral $ timeStamp sf)
-     ; setFileTimes (absFilePath opts sf) t t
-#endif
      }
  where write a b = BS.writeFile a (BS.pack b)
 
