01dd5d4b941d435925b5b798f44a0a0d437757f6
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-02-25 18:57:57 +0100

    Showing the quickcheck results with the stderr as well (through trace)

--- src/Database/Design/Ampersand/Test/Parser/QuickChecks.hs
+++ src/Database/Design/Ampersand/Test/Parser/QuickChecks.hs
@@ -7,7 +7,8 @@ import Database.Design.Ampersand.Test.Parser.ArbitraryTree()
 import Database.Design.Ampersand.ADL1.PrettyPrinters(pretty_print)
 import Database.Design.Ampersand.Core.ParseTree (P_Context)
 
-import Test.QuickCheck
+import Test.QuickCheck(Args(..), quickCheckWithResult, Testable, Result(..))
+import Debug.Trace
 
 -- Tries to parse a string, and if successful, tests the result with the given function
 testParse :: String -> (P_Context -> Bool) -> Bool
@@ -18,10 +19,23 @@ testParse text check = if success then check ctx else False
 prop_pretty :: P_Context -> Bool
 prop_pretty ctx = testParse (pretty_print ctx) (\p -> pretty_print ctx == pretty_print p)
 
-parserQuickChecks :: IO Bool
-parserQuickChecks =
-         -- do res <- quickCheckResult prop_pretty
-         do res <- return $ Success 0 [] ""
-            case res of
+checkArgs :: Args
+checkArgs = Args
+  { replay          = Nothing
+  , maxSuccess      = 100
+  , maxDiscardRatio = 10
+  , maxSize         = 100
+  , chatty          = False
+  }
+
+-- TODO: Improve the messages given here, remove all trace's
+test :: Testable prop => prop -> IO Bool
+test p = do res <- quickCheckWithResult checkArgs p
+            case trace (show res) res of
                 Success _ _ _ -> return True
                 _             -> return False
+
+parserQuickChecks :: IO Bool
+parserQuickChecks =
+         do res <- test prop_pretty
+            return res
--- test.bat
+++ test.bat
@@ -2,4 +2,4 @@
 ((ghc -o runtest.exe -Wall ^
 	-isrc/exec:src/lib:dist/build/autogen:src:src/Database/Design/Ampersand/Test:/MinGW/bin ^
 	src/Database/Design/Ampersand/Test/Main )^
-&& runtest.exe && echo Success!) || echo Failure!
+&& runtest.exe && echo Returned 0) || echo Failure!
