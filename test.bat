f7b7dca703ab5f4af6d4776e65e3894de5ec16f0
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-03-02 15:24:29 +0100

    Limiting memory usage

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @echo off
-((ghc -o runtest.exe -Wall ^
+((ghc -with-rtsopts="-M1024m" -o runtest.exe -Wall ^
 	-isrc/exec:src/lib:dist/build/autogen:src:src/Database/Design/Ampersand/Test:/MinGW/bin ^
 	src/Database/Design/Ampersand/Test/Main )^
 && runtest.exe && echo Returned 0) || echo Failure!
