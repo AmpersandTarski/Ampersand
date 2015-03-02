92d6fd32a4a05044db68acbbaa1066ce216f268b
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-03-02 20:52:14 +0100

    Adding more memory restrictions

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @echo off
-((ghc -with-rtsopts="-M1024m" -o runtest.exe -Wall ^
+((ghc -with-rtsopts="-M256m -H128m -K64m" -o runtest.exe -Wall ^
 	-isrc/exec:src/lib:dist/build/autogen:src:src/Database/Design/Ampersand/Test:/MinGW/bin ^
 	src/Database/Design/Ampersand/Test/Main )^
 && runtest.exe && echo Returned 0) || echo Failure!
