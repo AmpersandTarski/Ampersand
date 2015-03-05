568c9ed407c80b9b048c2bc99ea9d8a83250075c
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-03-05 22:04:21 +0100

    Working out parsing lib operators

--- src/Database/Design/Ampersand/Input/ADL1/ParsingLib.hs
+++ src/Database/Design/Ampersand/Input/ADL1/ParsingLib.hs
@@ -1,59 +1,41 @@
 {-# LANGUAGE FlexibleContexts, MultiParamTypeClasses, MagicHash, FlexibleInstances #-}
 module Database.Design.Ampersand.Input.ADL1.ParsingLib(
-    UU.getMsgs,UU.parse,UU.evalSteps,UU.Pair(..),UU.Message(..),UU.Action(..),
+--    UU.getMsgs,UU.parse,UU.evalSteps,UU.Pair(..),UU.Message(..),UU.Action(..),
     pSym, pSucceed, AmpParser,pAtom,
-    (<$>), (<*>), (<|>), (<$), (<*), (*>), (<??>),
+    (DF.<$>), (P.<|>), (<$), (CA.<*>), (CA.<*), (CA.*>), (<??>),
     pList, pList1, opt, pListSep, pList1Sep,pKey,pConid,pString,pSpec,pExpl,pVarid,pComma,pSemi,
     SourcePos, sourceName, sourceLine, sourceColumn
 ) where
 
-import qualified UU.Parsing as UU
-import Text.Parsec.Pos (SourcePos, sourceName, sourceLine, sourceColumn)
-import Database.Design.Ampersand.Input.ADL1.LexerToken (Token(..), TokenType(..))
+import Database.Design.Ampersand.Input.ADL1.LexerToken (Token)
 import Database.Design.Ampersand.Input.ADL1.Lexer
 import Control.Monad.Identity (Identity)
 import Data.Char (isUpper)
 import Text.Parsec.Char
 import Text.Parsec.Combinator
 import Text.Parsec.Pos
-import Text.Parsec.Prim as P
+import qualified Text.Parsec.Prim as P
+import Text.Parsec as P
 import Text.Parsec.Token
 import qualified Data.Functor as DF
 import qualified Control.Applicative as CA
 
-infixl 4 <*>, <$> 
-infixl 4 <$, <*, *>
 
 --TODO: TokenMonad?
-type AmpParser a = ParsecT [Token] SourcePos Identity a
-type AmpT a      = ParsecT String  [Token] Identity a
+type AmpParser a = P.ParsecT [Token] SourcePos Identity a
+type AmpT a      = P.ParsecT String  [Token] Identity a
 
---Operators from UU.Parsing
-(<$>) :: (a -> b) -> AmpParser a -> AmpParser b
-(<$>) =  (DF.<$>)
-
-(<|>) :: AmpParser a -> AmpParser a -> AmpParser a
-(<|>) =  (P.<|>)
-
-(<*>) :: AmpParser (a -> b) -> AmpParser a -> AmpParser b
-(<*>)  = (CA.<*>)
-
-(<*) :: AmpParser a -> AmpParser b -> AmpParser a
-(<*) = (CA.<*)
-
-(<$) :: AmpParser (a -> b) -> AmpParser a -> AmpParser b
-(<$) = (CA.<$)
-
-(*>) :: AmpParser a -> AmpParser b -> AmpParser b
-(*>) =  (CA.*>)
+--Operators
+infixl 4 <$
+(<$) :: a -> AmpParser b -> AmpParser a
+a <$ p = do { _ <- p; return a }
 
 (<**>) :: AmpParser a -> AmpParser (a -> b) -> AmpParser b
-p <**> q = (\ x f -> f x) CA.<$> p <*> q
+p <**> q = (\ x f -> f x) CA.<$> p CA.<*> q
 
 (<??>) :: AmpParser a -> AmpParser (a -> a) -> AmpParser a
 p <??> q = p <**> (q `opt` id)
 
---Functions from UU.Parsing
 ----------------------------------------------------------------------------------
 -- Functions copied from Lexer after decision to split lexer and parser
 ----------------------------------------------------------------------------------
@@ -80,29 +62,23 @@ pSym :: Token -> AmpParser Token
 pSym = pSym
 
 pSucceed :: a -> AmpParser a
-pSucceed = UU.pSucceed
+pSucceed = P.parserReturn
 
 pList :: AmpParser a -> AmpParser [a]
-pList = UU.pList
+pList = P.many
 
 pList1 ::  AmpParser a -> AmpParser [a]
-pList1 = UU.pList1
+pList1 = P.many1
 
-pListSep :: AmpParser a -> AmpParser a1 -> AmpParser [a1]
-pListSep = UU.pListSep
+pListSep :: AmpParser a -> AmpParser sep -> AmpParser [a]
+pListSep = P.sepBy
 
-pList1Sep ::  AmpParser a -> AmpParser a1 -> AmpParser [a1]
-pList1Sep = UU.pList1Sep
+pList1Sep ::  AmpParser a -> AmpParser sep -> AmpParser [a]
+pList1Sep = P.sepBy1
 
 opt ::  AmpParser a -> a -> AmpParser a
-opt = UU.opt
+a `opt` b = P.option b a
 
-instance UU.Symbol Token where
-    deleteCost t = 0#
-    symBefore t = t
-    symAfter t = t
-
-	
 pKey :: String -> AmpT ()
 pKey = reserved lexer
 
@@ -125,10 +101,10 @@ pSpec x = do { y <- char x; return [y] }
 
 --- Expl ::= '{+' Any* '-}'
 pExpl :: AmpT String
-pExpl = do try (string "{+")
+pExpl = do _ <- try (string "{+")
            inExpl
-        where inExpl =  do{ try (string "+}")            ; return "explanation" }
-                    P.<|> do{ skipMany1 (noneOf "+}")      ; inExpl } -- TODO: We shouldn't skip them of course
+        where inExpl =  do { _ <- try (string "+}"); return "explanation" }
+                    P.<|> do{ skipMany1 (noneOf "+}"); inExpl } -- TODO: We shouldn't skip them of course
                     P.<?> "end of comment"
 
 --- Varid ::= (LowerChar | '_') (Char | '_')*
