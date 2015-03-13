455c2db091fc77695463fddddbce62b73841652e
Michiel-s <michielstornebrink@gmail.com>
2015-03-13 13:08:23 +0100

    Bugfix in ExecEngine.js

--- static/newFrontend/extensions/ExecEngine/ui/js/ExecEngine.js
+++ static/newFrontend/extensions/ExecEngine/ui/js/ExecEngine.js
@@ -6,22 +6,10 @@ AmpersandApp.config(function($routeProvider) {
 				controller: 'ExecEngineController',
 				templateUrl: 'extensions/ExecEngine/ui/views/ExecEngine.html'
 			});
-		.when('/ext/CycleExecEngine',
-			{
-				controller: 'CycleExecEngineController',
-				templateUrl: 'extensions/ExecEngine/ui/views/ExecEngine.html'
-			});
 });
 
 AmpersandApp.controller('ExecEngineController', function ($scope, $rootScope, Restangular) {
 	
 	$scope.role = Restangular.one('role/name/ExecEngine').get().$object;
-    
-});
-
-AmpersandApp.controller('CycleExecEngineController', function ($scope, $rootScope, Restangular) {
-	
-        console.info('true', 'RieksJ was here');
-    };
     
 });
