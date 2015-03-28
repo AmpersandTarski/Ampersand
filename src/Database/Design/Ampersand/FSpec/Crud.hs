c2994dbde60485b2a7a8e91b6c9d8ad87961cde5
Martijn Schrage <martijn@oblomov.com>
2015-03-28 13:55:20 +0100

    Implement CRUD Create & Delete objects
    These seem to be equal, according to the description in issue #45.

--- src/Database/Design/Ampersand/FSpec/Crud.hs
+++ src/Database/Design/Ampersand/FSpec/Crud.hs
@@ -1,13 +1,17 @@
-module Database.Design.Ampersand.FSpec.Crud where
+module Database.Design.Ampersand.FSpec.Crud (CrudInfo(..), showCrudInfo, mkCrudInfo) where
 
 import Data.List
-import Database.Design.Ampersand.Basics.Unique
+import Database.Design.Ampersand.Basics
 import Database.Design.Ampersand.Classes.ConceptStructure
 import Database.Design.Ampersand.Classes.Relational
 import Database.Design.Ampersand.Core.AbstractSyntaxTree
 
-data CrudInfo = CrudInfo { getCrudObjects :: [A_Concept]
-                         , getIfcCrudMatrices :: [ (Interface, [(A_Concept,Bool,Bool,Bool,Bool)]) ] 
+fatal :: Int -> String -> a
+fatal = fatalMsg "Crud"
+
+
+data CrudInfo = CrudInfo { getCrudObjects     :: [A_Concept]
+                         , getIfcCrudMatrices :: [ (Interface, [(A_Concept,Bool,Bool,Bool,Bool)]) ]
                          -- TODO: think about representation of these matrices
                          } deriving Show
 
@@ -17,16 +21,16 @@ showCrudInfo (CrudInfo crudObjects ifcCrudMatrices) =
   "\nMatrices\n" ++ concat
     [ "Interface " ++ name ifc ++
       "\nC R U D Object\n" ++
-      (unlines $ map showCrud cObjs) 
+      (unlines $ map showCrud cObjs)
     | (ifc, cObjs) <- ifcCrudMatrices
     ] ++ "\n"
   where showNames xs = intercalate ", " $ map name xs
-        showCrud (cncpt, isC, isR, isU, isD) = concat [showX isX ++ " " | isX <- [isC, isR, isU, isD] ] ++ show (name cncpt)
+        showCrud (cncpt, isC, isR, isU, isD) = concat [ showX isX ++ " " | isX <- [isC, isR, isU, isD] ] ++ show (name cncpt)
         showX isX = if isX then "X" else " "
-  
+
 mkCrudInfo :: [A_Concept] -> [Declaration] -> [Interface] -> CrudInfo
-mkCrudInfo  allConceptsPrim allDecls allInterfaces =
-  CrudInfo allCrudObjects [ (ifc, getCrudMatrix ifc) | ifc <- allInterfaces ]
+mkCrudInfo  allConceptsPrim allDecls allIfcs =
+  CrudInfo allCrudObjects [ (ifc, getCrudMatrix ifc) | ifc <- allIfcs ]
   where allConcepts = [ c | c <- allConceptsPrim, not $ c == ONE || name c == "SESSION" ]
         nonCrudObjects = [ source d | d <- allDecls, isUni d && isSur d ] ++
                          [ target d | d <- allDecls, isInj d && isTot d ]
@@ -37,8 +41,31 @@ mkCrudInfo  allConceptsPrim allDecls allInterfaces =
         getCrudMatrix ifc = [ (cObj, cObj `elem` crudCreateObjs, cObj `elem` crudReadObjs, cObj `elem` crudUpdateObjs, cObj `elem` crudDeleteObjs)
                             | cObj <- allCrudObjects
                             ]
-        
-          where crudCreateObjs = []
-                crudReadObjs   = concs (relsUsedIn ifc)
-                crudDeleteObjs = []
+
+          where crudCreateObjs = getEditableTargets allIfcs ifc
+                crudReadObjs   = concs (relsUsedIn ifc) -- NOTE: this includes interface params, even if they do not appear in any of the field expressions
+                crudDeleteObjs = crudCreateObjs -- TODO: check if these are the same in Ampersand
                 crudUpdateObjs = []
+
+getEditableTargets :: [Interface] -> Interface -> [A_Concept]
+getEditableTargets allIfcs ifc = concatMap editableTarget $ getAllInterfaceExprs allIfcs ifc
+  where editableTarget expr = 
+          case getExpressionRelation expr of
+            Nothing                                                              -> []
+            Just (declSrc, decl, declTgt, isFlipped) | decl `elem` ifcParams ifc -> [if isFlipped then declSrc else declTgt]
+                                                     | otherwise                 -> []
+-- TODO: Handle specs? And do we use source/target expr or declSrc/Tgt?
+
+getAllInterfaceExprs :: [Interface] -> Interface -> [Expression]
+getAllInterfaceExprs allIfcs ifc = getExprs $ ifcObj ifc
+  where getExprs Obj{objctx=expr, objmsub=subObj} = 
+          expr : case subObj of Nothing                -> []
+                                Just (InterfaceRef nm) ->
+                                  case filter (\rIfc -> name rIfc == nm) $ allIfcs of -- Follow interface ref
+                                    []      -> fatal 65 $ "Referenced interface " ++ nm ++ " missing"
+                                    (_:_:_) -> fatal 66 $ "Multiple declarations of referenced interface " ++ nm
+                                    [i]     -> getAllInterfaceExprs allIfcs i
+                                Just (Box _ _ objs)    -> concatMap getExprs objs
+
+                                
+                                
