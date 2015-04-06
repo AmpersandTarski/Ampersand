dd02e781bad9eeef751562c8b6a6ab7d534dd65c
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-04-06 18:11:10 +0200

    Handling exit codes properly in the test suite

--- src/Database/Design/Ampersand/Test/Main.hs
+++ src/Database/Design/Ampersand/Test/Main.hs
@@ -2,7 +2,7 @@ module Main (main) where
 
 import Database.Design.Ampersand.Test.Parser.ParseScripts
 import Database.Design.Ampersand.Test.Parser.QuickChecks
-import System.Exit (ExitCode(..))
+import System.Exit (ExitCode, exitFailure, exitSuccess)
 
 testFunctions :: IO [(String, IO Bool)]
 testFunctions = do scr <- scripts
@@ -10,18 +10,13 @@ testFunctions = do scr <- scripts
                      ("Parsing " ++ show (length scr) ++ " scripts.", testScripts scr),
                      ("Running automatic quick checks", parserQuickChecks)]
 
-runTests :: IO Bool
-runTests = do funcs <- testFunctions
-              tests funcs
-    where tests :: [(String, IO Bool)] -> IO Bool
-          tests [] = return True
+main :: IO ExitCode
+main = do funcs <- testFunctions
+          tests funcs
+    where tests :: [(String, IO Bool)] -> IO ExitCode
+          tests [] = exitSuccess
           tests ((msg,fun):xs) =
             do putStrLn msg
                success <- fun
                if success then tests xs
-               else return False
-
-main :: IO ExitCode
-main = do res <- runTests
-          if res then return ExitSuccess
-          else return $ ExitFailure 1
+               else exitFailure
