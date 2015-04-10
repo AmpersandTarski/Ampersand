6823f0d096f41d1cdfc5947da5bdca78365e0c82
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-04-10 13:42:41 +0200

    Writing error messages to test.adl file

--- test.bat
+++ test.bat
@@ -1,6 +1,7 @@
 @echo off
+del runtest.exe.*
 ((ghc -fhpc -with-rtsopts="-M1536m -H256m -K128m" -o runtest.exe -Wall ^
 	-isrc/exec:src/lib:dist/build/autogen:src:src/Database/Design/Ampersand/Test:/MinGW/bin ^
 	src/Database/Design/Ampersand/Test/Main )^
-&& runtest.exe && echo Returned 0 && hpc markup runtest.exe.tix --destdir=hpc && del runtest.exe.*) ^
-|| echo Failure!
+&& runtest.exe && echo Success!  See test.adl for warnings. && hpc markup runtest.exe.tix --destdir=hpc) 2> test.adl ^
+|| echo Failure! See test.adl for details.
