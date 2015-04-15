fd92b52f6e5057220c4c8e7a4b33c04376e2ac03
Daniel Schiavini <Daniel.Schiavini@gmail.com>
2015-04-15 21:29:59 +0200

    Trying to enable "cabal test" command

--- test.bat
+++ test.bat
@@ -1,7 +1,7 @@
 @echo off
 del runtest.exe.*
 ((ghc -fhpc -with-rtsopts="-M1536m -H256m -K128m" -o runtest.exe -Wall ^
-	-isrc/exec:src/lib:dist/build/autogen:src:src/Database/Design/Ampersand/Test:/MinGW/bin ^
-	src/Database/Design/Ampersand/Test/Main )^
+	-isrc/exec:src/lib:dist/build/autogen:src:/MinGW/bin ^
+	src/Test.hs )^
 && runtest.exe && echo Success!  See test.adl for warnings. && hpc markup runtest.exe.tix --destdir=hpc) 2> test.adl ^
 || echo Failure! See test.adl for details.
