b84897b4162faf732243ecb4618b1484f12be738
Michiel-s <michielstornebrink@gmail.com>
2015-05-11 11:20:05 +0200

    ExecEngine default on (again:)

--- static/newFrontend/localSettings.php
+++ static/newFrontend/localSettings.php
@@ -26,7 +26,7 @@ $DB_name = $dbName; // from Generics.php
  */
 
 // Enable ExecEngine
-// require_once(__DIR__ . '/extensions/ExecEngine/ExecEngine.php');
+require_once(__DIR__ . '/extensions/ExecEngine/ExecEngine.php');
 $ext['ExecEngine']['ExecEngineRoleName'] = 'ExecEngine';
 	
 	// Config params for SendEmail function of ExecEngine (now using Gmail) 
