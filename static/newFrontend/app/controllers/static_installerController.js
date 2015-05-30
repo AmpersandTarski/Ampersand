3ec75661efb872d947f2207fc61c3556980e0fff
Rieks <rieks.joosten@tno.nl>
2015-05-30 16:00:29 +0200

    Installer now has a moving circle showing that the database is being installed.

--- static/newFrontend/app/controllers/static_installerController.js
+++ static/newFrontend/app/controllers/static_installerController.js
@@ -1,17 +1,17 @@
 AmpersandApp.controller('static_installerController', ['$scope', '$rootScope', '$routeParams', 'Restangular', function ($scope, $rootScope, $routeParams, Restangular) {
-	
+	$scope.installing = false;
 	$scope.install = function(){
-		if(confirm('Are you sure?')){
-			Restangular.one('installer').get().then(function(data) {
-				$rootScope.notifications = data;
-				
-				// refresh session
-				$rootScope.session = Restangular.one('session').get().$object;
-				
-				// refresh interfaces list
-				$rootScope.interfaces = Restangular.one('interfaces/all').get().$object;
-			});
-		}
+		$scope.installing = !$scope.installing;
+		Restangular.one('installer').get().then(function(data) {
+			$rootScope.notifications = data;
+			
+			// refresh session
+			$rootScope.session = Restangular.one('session').get().$object;
+			
+			// refresh interfaces list
+			$rootScope.interfaces = Restangular.one('interfaces/all').get().$object;
+		$scope.installing = !$scope.installing;
+		});
 	}
 	
 }]);
