14cf039ec620b8b7d2ec49a222e2abc278e8b8ef
Michiel-s <michielstornebrink@gmail.com>
2015-06-10 12:58:03 +0200

    Add filter module to transform objects to arrays

--- static/newFrontend/app/AmpersandApp.js
+++ static/newFrontend/app/AmpersandApp.js
@@ -111,4 +111,18 @@ AmpersandApp.directive('myShowonhoverRow', function (){
             }
         });
     };
+}).filter('toArray', function() {
+	// used from: https://github.com/petebacondarwin/angular-toArrayFilter
+	return function (obj, addKey) {
+	    if (!obj) return obj;
+	    if ( addKey === false ) {
+	      return Object.keys(obj).map(function(key) {
+	        return obj[key];
+	      });
+	    } else {
+	      return Object.keys(obj).map(function (key) {
+	        return Object.defineProperty(obj[key], '$key', { enumerable: false, value: key});
+	      });
+	    }
+	  };
 });
