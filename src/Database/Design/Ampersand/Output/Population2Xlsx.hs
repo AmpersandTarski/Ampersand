892c69e6504aef1581a63ec6df4634844d2a2c06
Han Joosten <han.joosten.han@gmail.com>
2015-06-16 14:43:08 +0200

    Added the population writer.

--- src/Database/Design/Ampersand/Output/Population2Xlsx.hs
+++ src/Database/Design/Ampersand/Output/Population2Xlsx.hs
@@ -2,24 +2,68 @@
 module Database.Design.Ampersand.Output.Population2Xlsx
   (fSpec2PopulationXlsx)
 where
-import Database.Design.Ampersand.Misc
 import Database.Design.Ampersand.FSpec
-import Database.Design.Ampersand.Core.AbstractSyntaxTree
-import Database.Design.Ampersand.FSpec.FPA
 import Database.Design.Ampersand.Basics
+import Database.Design.Ampersand.Core.AbstractSyntaxTree
 import System.Time
-
+import qualified Data.Map as M
 import Codec.Xlsx
-import Control.Lens
 import qualified Data.ByteString.Lazy as L
-import System.Time
+import Data.Text hiding (zip, transpose)
+import Data.Maybe
+import Data.List
 
 fSpec2PopulationXlsx :: ClockTime -> FSpec -> L.ByteString 
 fSpec2PopulationXlsx ct fSpec = 
   fromXlsx ct xlsx
     where
-      sheet = def & cellValueAt (1,2) ?~ CellDouble 42.0
-                  & cellValueAt (3,2) ?~ CellText "foo"
-      xlsx = def & atSheet "List1" ?~ sheet
+      xlsx =def { _xlSheets = plugs2Sheets fSpec}
+               
      
 
+plugs2Sheets :: FSpec -> M.Map Text Worksheet
+plugs2Sheets fSpec = M.fromList . catMaybes . Prelude.map plug2sheet $ plugInfos fSpec
+  where
+    plug2sheet :: PlugInfo -> Maybe (Text, Worksheet)
+    plug2sheet ExternalPlug{} = Nothing  -- Not supported at present
+    plug2sheet (InternalPlug plug) = fmap (\x -> (pack (name plug),x)) sheet
+      where 
+       sheet :: Maybe Worksheet
+       --sheet = def & cellValueAt (1,1) ?~ CellText (pack ("["++name plug++"'s]"))
+       sheet = case matrix of
+                 Nothing -> Nothing
+                 Just m -> Just def{_wsCells = fromRows . numberList . Prelude.map numberList $ m }
+            where 
+              numberList :: [c] -> [(Int, c)]
+              numberList = zip [1..] 
+       matrix :: Maybe  [[Cell]]
+       matrix = 
+         case plug of
+           TblSQL{} -> Just $ headers ++ content  
+           BinSQL{} -> Nothing
+           ScalarSQL{} -> Nothing
+--       field2Row :: SqlField -> [Cell]
+--       field2Row fld = headerRows ++ content
+         where
+           headers :: [[Cell]]
+           headers = transpose (Prelude.map f (fields plug)) 
+             where f :: SqlField -> [Cell]
+                   f fld = Prelude.map toCell 
+                             [ case flduse fld of -- In case of the main concept of the table, we put the fieldname inbetween brackets,
+                                                  -- to be able to find the population again by the reader of the .xlsx file
+                                 (TableKey True _) -> Just $ "["++name fld++"]" 
+                                 _ ->  Just $ name fld
+                             , Just $ name .target . fldexpr $ fld ]
+           content = Prelude.map record2Cells (tblcontents (vgens fSpec) (initialPops fSpec) plug)
+           record2Cells :: TblRecord -> [Cell]
+           record2Cells = fmap toCell
+       toCell :: Maybe String -> Cell
+       toCell mStr = Cell { _cellStyle = Nothing
+                         , _cellValue = fmap (\x -> CellText . pack $ x) mStr
+                         }
+       
+       -- case tblcontents (vgens fSpec) (initialPops fSpec) plug of
+type TblRecord = [Maybe String]
+
+  
+            
