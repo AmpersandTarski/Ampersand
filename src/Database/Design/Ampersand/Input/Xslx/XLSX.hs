0ef25e83ef1e8c8a54b22694235bdcbd8f6d58b8
Han Joosten <han.joosten.han@gmail.com>
2015-06-17 16:22:46 +0200

    cleaning up

--- src/Database/Design/Ampersand/Input/Xslx/XLSX.hs
+++ src/Database/Design/Ampersand/Input/Xslx/XLSX.hs
@@ -35,23 +35,21 @@ data SheetCellsForTable
                 , popRowNrs    :: [Int]
                 , colNrs       :: [Int]
                 }
-showMapping :: SheetCellsForTable -> String
-showMapping x 
- = unlines $
-    [ "Sheet       : "++theSheetName x
-    , "headerRowNrs: "++show (headerRowNrs x)
-    , "popRowNrs   : "++show (popRowNrs x)
-    , "colNrs      : "++show (colNrs x)
-    ]  
+instance Show SheetCellsForTable where  --for debugging only
+  show x 
+   = unlines $
+      [ "Sheet       : "++theSheetName x
+      , "headerRowNrs: "++show (headerRowNrs x)
+      , "popRowNrs   : "++show (popRowNrs x)
+      , "colNrs      : "++show (colNrs x)
+      ]  
 toPops :: FilePath -> SheetCellsForTable -> [P_Population]
-toPops file x = trace (showMapping x) $
-                map popForColumn (colNrs x)
+toPops file x = map popForColumn (colNrs x)
   where
     popForColumn :: Int -> P_Population
     popForColumn i 
       | i  == 1  
-        = trace ("Starting for colunm: "++show i) 
-           P_CptPopu { p_orig = popOrigin
+        =  P_CptPopu { p_orig = popOrigin
                      , p_cnme = sourceConceptName 
                      , p_popas = map T.unpack [fromMaybe (fatal 49 "this value should be present")
                                                          (case value(row,i) of
@@ -60,9 +58,7 @@ toPops file x = trace (showMapping x) $
                                               | row <- popRowNrs x] 
                      }
       | otherwise
-        = trace ("Starting for colunm: "++show i) $
-          trace (show(conceptNamesRow,relNamesRow,sourceCol,targetCol))$
-           (case mTargetConceptName of
+        =  (case mTargetConceptName of
              Just tCptName 
                 -> P_TRelPop { p_orig = popOrigin
                              , p_rnme = relName
@@ -120,27 +116,22 @@ toPops file x = trace (showMapping x) $
 
 theSheetCellsForTable :: (T.Text,Worksheet) -> [SheetCellsForTable]
 theSheetCellsForTable (sheetName,ws) 
-  = trace (T.unpack sheetName ++": "++show tableStarters) $ 
-     catMaybes [mapping i | i <- [0..(Prelude.length tableStarters) - 1]]
+  =  catMaybes [theMapping i | i <- [0..(Prelude.length tableStarters) - 1]]
   where
     tableStarters :: [(Int,Int)]
-    tableStarters = Prelude.filter isStartOfTable $ M.keys cm  
-      where cm = ws  ^. wsCells
-            ks = M.keys cm
-            isStartOfTable :: (Int,Int) -> Bool
-            isStartOfTable k@(r,c) 
-              = c == 1 && case  value k of
+    tableStarters = Prelude.filter isStartOfTable $ M.keys (ws  ^. wsCells)  
+      where isStartOfTable :: (Int,Int) -> Bool
+            isStartOfTable k 
+              = (snd k) == 1 && case  value k of
                              Just (CellText t) -> (not . T.null ) t && T.head t == '[' && T.last t == ']'
                              _  -> False
     value :: (Int,Int) -> Maybe CellValue
     value k = (ws  ^. wsCells) ^? ix k . cellValue . _Just
-    mapping :: Int -> Maybe SheetCellsForTable
-    mapping indexInTableStarters 
+    theMapping :: Int -> Maybe SheetCellsForTable
+    theMapping indexInTableStarters 
      | length headerRows /= 2 = Nothing  -- Because there are not enough header rows
      | otherwise
-     = --trace (show ((r1,c1),theRows,theCols)
-       --       ) $
-        Just Mapping { theSheetName = T.unpack sheetName
+     =  Just Mapping { theSheetName = T.unpack sheetName
                      , theCellMap   = ws  ^. wsCells
                      , headerRowNrs = headerRows
                      , popRowNrs    = populationRows
@@ -178,59 +169,4 @@ theSheetCellsForTable (sheetName,ws)
                
    
 
-
-type TableContent = [[Cell]]
-extractTableContents :: Xlsx -> [TableContent]
-extractTableContents xlsx =  Prelude.concatMap tablesOfSheet' theSheets
-  where 
-        theSheets :: [(T.Text,Worksheet)]
-        theSheets = xlsx ^. xlSheets . to M.toList   
-        cellmap :: (T.Text,Worksheet) -> CellMap
-        cellmap (t,ws) = ws  ^. wsCells
-        
-
-
-tablesOfSheet' :: (T.Text,Worksheet) -> [TableContent]
-tablesOfSheet' (t,ws) = trace aap []
-   where aap = T.unpack . T.intercalate ("\n  ") . Prelude.map T.pack $ 
-                  [ "Sheet: "++T.unpack t
-               --   , "columns: " ++ myshow (ws ^? wsColumns )
-               --   , "rowprops:"++ show (ws ^? wsRowPropertiesMap  )
-                    , "cells: " ++ showCells (ws ^? wsCells  )
-               --   , "merges: " ++ show (ws ^? wsMerges  )
-                  ]
-         myshow :: Maybe [ColumnsWidth] -> String
-         myshow Nothing = ""
-         myshow (Just cws) = Prelude.concatMap show1 cws 
-         show1 (ColumnsWidth min max width style) =  show (min,max,width,style)  
-         colums = ws ^? wsCells  
-         showCells :: Maybe CellMap -> String
-         showCells (Just cm)= "Range: "++show (firstRow ks,lastRow ks)++ " - "++show ( firstCol ks,lastCol ks)
-                                       ++show ("starters: "++show (tableStarters cm))
-                                       ++Prelude.concat ["\nKeys vanaf start "++show start++" t/m "++show eind++": \n  "
-                                                 -- ++show ks
-                                                |(start,eind) <-
-                                                     [(rij !! i, rij !! (i+1))| i <- [0..Prelude.length rij - 1]]]
-                                                
-           where ks = M.keys cm
-                 rij = tableStarters cm++ [(lastRow ks,1)]
-         firstRow ks= Prelude.minimum (Prelude.map fst ks)
-         lastRow  ks= Prelude.maximum (Prelude.map fst ks)
-         firstCol ks= Prelude.minimum (Prelude.map snd ks)
-         lastCol  ks= Prelude.maximum (Prelude.map snd ks)
-         tableStarters :: CellMap -> [(Int,Int)]
-         tableStarters cm = Prelude.filter isStartOfTable $ M.keys cm  
-           where ks = M.keys cm
-                 isStartOfTable :: (Int,Int) -> Bool
-                 isStartOfTable k@(r,c) 
-                   = c == 1 && case  value k of
-                                  Just (CellText t) -> (not . T.null ) t && T.head t == '[' && T.last t == ']'
-                                  _  -> False
-                 value :: (Int,Int) -> Maybe CellValue
-                 value k = cm ^? ix k . cellValue . _Just
-         tableWidth :: (Int,Int) -> Int
-         tableWidth (r,c) = 1 -- length noot
-           where -- noot = Prelude.takeWhile (isJust) ((fmap (lookup k          
-                keysOfRow = Prelude.filter (\k -> r == fst k) (M.keys cm)
-                cm = ws ^. wsCells  
          
