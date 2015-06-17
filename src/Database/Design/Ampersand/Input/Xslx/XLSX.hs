6bdf322bfcd24dc43e3504d1b277d631ba4259be
Han Joosten <han.joosten.han@gmail.com>
2015-06-17 16:12:35 +0200

    In works! I had a succesfull roundtrip.

--- src/Database/Design/Ampersand/Input/Xslx/XLSX.hs
+++ src/Database/Design/Ampersand/Input/Xslx/XLSX.hs
@@ -22,13 +22,12 @@ parseXlsxFile :: Options -> FilePath -> IO (Guarded [P_Population])
 parseXlsxFile _ filePath = 
   do bytestr <- L.readFile filePath
      return . xlsx2pContext . toXlsx $ bytestr
-
-xlsx2pContext :: Xlsx -> Guarded [P_Population]
-xlsx2pContext xlsx 
-  = case Prelude.concatMap theSheetCellsForTable (xlsx ^. xlSheets . to M.toList) of --
-                       [] -> fatal 28 "INCLUDE Excel files comming soon."
-                       xs -> fatal 29 "INCLUDE Excel files comming soon."
-
+ where
+  xlsx2pContext :: Xlsx -> Guarded [P_Population]
+  xlsx2pContext xlsx 
+    = Checked $ concatMap (toPops filePath) $
+         Prelude.concatMap theSheetCellsForTable (xlsx ^. xlSheets . to M.toList)
+      
 data SheetCellsForTable 
        = Mapping{ theSheetName :: String
                 , theCellMap   :: CellMap
@@ -36,29 +35,87 @@ data SheetCellsForTable
                 , popRowNrs    :: [Int]
                 , colNrs       :: [Int]
                 }
-
+showMapping :: SheetCellsForTable -> String
+showMapping x 
+ = unlines $
+    [ "Sheet       : "++theSheetName x
+    , "headerRowNrs: "++show (headerRowNrs x)
+    , "popRowNrs   : "++show (popRowNrs x)
+    , "colNrs      : "++show (colNrs x)
+    ]  
 toPops :: FilePath -> SheetCellsForTable -> [P_Population]
-toPops file x = map popForColumn (colNrs x)
+toPops file x = trace (showMapping x) $
+                map popForColumn (colNrs x)
   where
     popForColumn :: Int -> P_Population
     popForColumn i 
       | i  == 1  
-         = P_CptPopu { p_orig = originOfCell (headerRowNrs x !! (2-1), colNrs x !! (1-1))
-                     , p_cnme = case value (headerRowNrs x !! (2-1),i) of
-                         Just (CellText t) -> T.unpack t
-                         _ -> fatal 49 "No valid string fond. This should have been checked before" 
-                     , p_popas = map T.unpack [] 
+        = trace ("Starting for colunm: "++show i) 
+           P_CptPopu { p_orig = popOrigin
+                     , p_cnme = sourceConceptName 
+                     , p_popas = map T.unpack [fromMaybe (fatal 49 "this value should be present")
+                                                         (case value(row,i) of
+                                                           Just (CellText t) -> Just t
+                                                           _ -> Nothing)
+                                              | row <- popRowNrs x] 
                      }
-                             
-      | otherwise = fatal 47 "TODO"
+      | otherwise
+        = trace ("Starting for colunm: "++show i) $
+          trace (show(conceptNamesRow,relNamesRow,sourceCol,targetCol))$
+           (case mTargetConceptName of
+             Just tCptName 
+                -> P_TRelPop { p_orig = popOrigin
+                             , p_rnme = relName
+                             , p_type = P_Sign {pSrc = PCpt sourceConceptName
+                                               ,pTgt = PCpt tCptName
+                                               }
+                             , p_popps = thePairs}
+             Nothing
+                -> P_RelPopu { p_orig = popOrigin
+                             , p_rnme = relName
+                             , p_popps = thePairs}
+           )
+     where                             
+       popOrigin :: Origin
+       popOrigin = originOfCell (relNamesRow, targetCol)
+       conceptNamesRow = headerRowNrs x !! 1
+       relNamesRow     = headerRowNrs x !! 0
+       sourceCol       = colNrs x !! 0
+       targetCol       = i 
+       sourceConceptName :: String
+       sourceConceptName 
+          = case value (conceptNamesRow,sourceCol) of
+                Just (CellText t) -> T.unpack t
+                _ -> fatal 66 "No valid source conceptname found. This should have been checked before"
+       mTargetConceptName :: Maybe String
+       mTargetConceptName 
+          = case value (conceptNamesRow,targetCol) of
+                Just (CellText t) -> Just (T.unpack t)
+                _ -> Nothing
+       relName :: String
+       relName 
+          = case value (relNamesRow,targetCol) of
+                Just (CellText t) -> T.unpack t
+                _ -> fatal 87 $ "No valid relation name found. This should have been checked before" ++show (relNamesRow,targetCol)
+       thePairs = catMaybes (map pairAtRow (popRowNrs x))
+       pairAtRow :: Int -> Maybe Paire
+       pairAtRow r = case (value (r,sourceCol)
+                          ,value (r,targetCol)
+                          ) of
+                       (Just s,Just t) -> Just $ mkPair (cellToString s) (cellToString t)
+                       _ -> Nothing
+       cellToString :: CellValue -> String
+       cellToString cv = case cv of
+                          CellText t -> T.unpack t
+                          CellDouble d -> show d
+                          CellBool b -> show b 
     originOfCell :: (Int,Int) -- (row number,col number)
                  -> Origin
     originOfCell (r,c) 
-      = Origin $ file ++", sheet: "++theSheetName x++", Cell ("++show r++","++show c++")" --TODO: Make separate Origin constructor for this 
+      = Origin $ file ++",\n  Sheet: "++theSheetName x++", Cell ("++show r++","++show c++")" --TODO: Make separate Origin constructor for this 
 
     value :: (Int,Int) -> Maybe CellValue
     value k = (theCellMap x) ^? ix k . cellValue . _Just
-
 
 
 theSheetCellsForTable :: (T.Text,Worksheet) -> [SheetCellsForTable]
@@ -93,8 +150,8 @@ theSheetCellsForTable (sheetName,ws)
        (r1,_{-c1-}) = tableStarters !! indexInTableStarters 
        maxRowOfWorksheet = Prelude.maximum (Prelude.map fst (M.keys (ws  ^. wsCells)))
        maxColOfWorksheet = Prelude.maximum (Prelude.map snd (M.keys (ws  ^. wsCells)))
-       maxRows = (map fst tableStarters++[maxRowOfWorksheet])!!(indexInTableStarters+1)-r1
-       headerRows = map (+ r1) $ filter isProperRow [1,2]
+       maxRows = ((map fst tableStarters++[maxRowOfWorksheet])!!(indexInTableStarters+1))-r1+1
+       headerRows = map (+ (r1-1)) $ filter isProperRow [1,2]
        populationRows = filter isProperRow [length headerRows+1..maxRows]
        isProperRow :: Int -> Bool
        isProperRow i
--- src/Database/Design/Ampersand/Output/Population2Xlsx.hs
+++ src/Database/Design/Ampersand/Output/Population2Xlsx.hs
@@ -9,7 +9,7 @@ import System.Time
 import qualified Data.Map as M
 import Codec.Xlsx
 import qualified Data.ByteString.Lazy as L
-import Data.Text hiding (zip, transpose)
+import qualified Data.Text as T
 import Data.Maybe
 import Data.List
 
@@ -21,15 +21,14 @@ fSpec2PopulationXlsx ct fSpec =
                
      
 
-plugs2Sheets :: FSpec -> M.Map Text Worksheet
+plugs2Sheets :: FSpec -> M.Map T.Text Worksheet
 plugs2Sheets fSpec = M.fromList . catMaybes . Prelude.map plug2sheet $ plugInfos fSpec
   where
-    plug2sheet :: PlugInfo -> Maybe (Text, Worksheet)
+    plug2sheet :: PlugInfo -> Maybe (T.Text, Worksheet)
     plug2sheet ExternalPlug{} = Nothing  -- Not supported at present
-    plug2sheet (InternalPlug plug) = fmap (\x -> (pack (name plug),x)) sheet
+    plug2sheet (InternalPlug plug) = fmap (\x -> (T.pack (name plug),x)) sheet
       where 
        sheet :: Maybe Worksheet
-       --sheet = def & cellValueAt (1,1) ?~ CellText (pack ("["++name plug++"'s]"))
        sheet = case matrix of
                  Nothing -> Nothing
                  Just m -> Just def{_wsCells = fromRows . numberList . Prelude.map numberList $ m }
@@ -50,14 +49,19 @@ plugs2Sheets fSpec = M.fromList . catMaybes . Prelude.map plug2sheet $ plugInfos
                          [ if isFirstField  -- In case of the first field of the table, we put the fieldname inbetween brackets,
                                             -- to be able to find the population again by the reader of the .xlsx file
                            then Just $ "["++name fld++"]" 
-                           else Just $ name fld
+                           else Just . cleanUpRelName $ name fld
                          , Just $ name .target . fldexpr $ fld ]
-                              
+                   cleanUpRelName :: String -> String
+                   --TODO: This is a not-so-nice way to get the relationname from the fieldname.
+                   cleanUpRelName orig
+                     | isPrefixOf "tgt_" orig = drop 4 orig
+                     | isPrefixOf "src_" orig = drop 4 orig
+                     | otherwise         = orig
            content = fmap record2Cells (tblcontents (vgens fSpec) (initialPops fSpec) plug)
            record2Cells = fmap toCell
        toCell :: Maybe String -> Cell
        toCell mStr = Cell { _cellStyle = Nothing
-                         , _cellValue = fmap (\x -> CellText . pack $ x) mStr
+                         , _cellValue = fmap (\x -> CellText . T.pack $ x) mStr
                          }
        
 
