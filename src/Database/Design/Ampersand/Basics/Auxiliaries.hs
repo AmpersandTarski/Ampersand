5f9e168b37a9fdaffb67bd6eaa540746d868d521
Han Joosten <han.joosten.han@gmail.com>
2015-07-08 22:50:39 +0200

    Rewritten converse, for it did not comply to it's guarantee, that all b's were unique. The new implementation might be somewhat slower (for no Set and Map is used, but at least it is correct. I do not expect very bad performance.

--- src/Database/Design/Ampersand/Basics/Auxiliaries.hs
+++ src/Database/Design/Ampersand/Basics/Auxiliaries.hs
@@ -8,10 +8,9 @@ import Data.Graph (stronglyConnComp, SCC(CyclicSCC))
 import Data.Maybe (fromMaybe)
 import Data.Map (Map) 
 import qualified Data.Map as Map 
-import Data.Set (Set)
-import qualified Data.Set as Set 
 import Debug.Trace
 
+import GHC.Exts (groupWith)
 -- | The 'eqClass' function takes an equality test function and a list and returns a list of lists such
 -- that each sublist in the result contains only equal elements, and all equal elements are in
 -- the same sublist.  For example,
@@ -62,9 +61,11 @@ combinations (es:ess) = [ x:xs | x<-es, xs<-combinations ess]
 -- Each b in the result is unique, and so is each a per b, eg.: 
 -- converse [(1,[2,2,3]),(1,[3,4]),(2,[4,5])]  == [(2,[1]),(3,[1]),(4,[1,2]),(5,[2])]
 converse :: forall a b . (Ord a, Ord b) => [(a, [b])] -> [(b, [a])]
-converse aBss = let asPerB ::(Ord a, Ord b) =>  Map b (Set a)
-                    asPerB = foldl (.) id [ Map.insertWith Set.union b (Set.singleton a)  | (a,bs) <- aBss, b <- bs ] $ Map.empty
-                in Map.toList $ fmap Set.toList asPerB -- first convert each Set to a list, and then the whole Map to a list of tuples
+converse aBss = case groupWith fst [(b,[a])| (a,bs) <- aBss, b <- bs ] of
+                  [] -> []
+                  as -> map f as
+      where f :: [(b, [a])] -> (b, [a])
+            f bas = (fst.head $ bas,concat (map snd bas))
 
 commaEng :: String -> [String] -> String
 commaEng str [a,b,c] = a++", "++b++", "++str++" "++c
