573b5d2b8dad2679f99057df46596ebfee350781
Michiel-s <michielstornebrink@gmail.com>
2015-07-09 14:47:27 +0200

    Add API functionality for ExecEngine extension

---
+++ static/newFrontend/extensions/ExecEngine/api/ExecEngineApi.php
@@ -0,0 +1,31 @@
+<?php
+
+use Luracast\Restler\Data\Object;
+use Luracast\Restler\RestException;
+
+class ExecEngineApi{
+	
+	/****************************** PARSE FILE ******************************/
+	/**
+	 * @url GET run
+	 */
+	public function run(){
+		try{
+			$session = Session::singleton();
+			$db = Database::singleton();
+			
+			$session->setRole();
+			
+			ExecEngine::run();
+			
+			$db->closeTransaction('Run completed',true,true,false);
+			
+			$result = array('notifications' => Notifications::getAll());
+			return $result;
+			
+		}catch(Exception $e){
+			throw new RestException($e->getCode(), $e->getMessage());
+		}
+	}
+}
+?>
---
+++ static/newFrontend/extensions/ExecEngine/api/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once (__DIR__ . '/../../../fw/includes.php');
+require_once (__DIR__ . '/../ExecEngine.php');
+require_once (__DIR__ . '/../../../api/vendor/restler.php');
+use Luracast\Restler\Restler;
+use Luracast\Restler\Format\UploadFormat;
+
+$r = new Restler();
+$r->setSupportedFormats('JsonFormat', 'XmlFormat', 'HtmlFormat'); // some strange error in Restler when UploadFormat is mentioned as first parameter
+$r->addAPIClass('ExecEngineApi','');
+$r->handle();
+
+?>
