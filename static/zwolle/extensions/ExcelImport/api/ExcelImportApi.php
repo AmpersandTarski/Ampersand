1e6c7a2b18715b4c58e7727213c184ce4ac8c281
Michiel-s <michielstornebrink@gmail.com>
2015-10-19 14:51:09 +0200

    Access management on ExecEngine and ExcelImport when LOGIN_ENABLED

--- static/zwolle/extensions/ExcelImport/api/ExcelImportApi.php
+++ static/zwolle/extensions/ExcelImport/api/ExcelImportApi.php
@@ -11,6 +11,19 @@ class ExcelImportApi{
 	 */
 	public function post(){
 		try{
+			$session = Session::singleton();
+			
+			if(LOGIN_ENABLED){
+				$ok = false;
+			
+				$sessionRoles = Role::getAllSessionRoles(session_id());
+				$allowedRoles = (array)Config::get('allowedRolesForExcelImport','excelImport');
+				foreach($sessionRoles as $role){
+					if(in_array($role->label, $allowedRoles)) $ok = true;
+				}
+				if(!$ok) throw new Exception("You do not have access to import excel files", 401);
+			}
+			
 			if (is_uploaded_file($_FILES['file']['tmp_name'])){
 				// Parse:
 				$parser = new ImportExcel($_FILES['file']['tmp_name']);
