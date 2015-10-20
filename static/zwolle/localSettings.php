f23b7d82f1b9a3b8f6ce143786ab1fb6246c40d2
Rieks <rieks.joosten@tno.nl>
2015-10-20 14:11:52 +0200

    syntax coloring update, and localSettings update (logins)

--- static/zwolle/localSettings.php
+++ static/zwolle/localSettings.php
@@ -33,23 +33,27 @@ define ('COMMIT_INV_VIOLATIONS', false); // For debugging: TRUE: always commit c
  * Enable/disable built-in login functionality -- see comments in 'localSettings.php'
  * **In 'localSettings.php', the variable 'LOGIN_ENABLED' must be assigned the value 'true'**
  * This requires the following &-INTERFACE defintions:
--} INTERFACE "SessionRoles" FOR NobodyInParticular : '_SESSION';V[SESSION*Role] BOX [ "ignored" : I]
+-} INTERFACE "SessionRoles" FOR NobodyInParticular : sessionRoles[SESSION*Role] BOX [ "ignored" : I]
 {- Notes: 
    1) The name of the interface MUST be 'SessionRoles'
    2) The 'FOR NobodyInParticular' ensures that the INTERFACE does not show up in a menu.
-   3) The SRC concept of the expression between the ':' and 'BOX' MUST be SESSION
-   4) The population of the TGT-concept of that expression must contain all roles that are used 
-      in the &-script for MAINTAINing rules or INTERFACEs. 
-      One way to ensure this is to include the following statement before every 'MAINTAINS' statement:
-      ** POPULATION Role CONTAINS [ "<Rolename>" ]** -- as per Stef's solution on issue #63 (SF ticket #417)
-   5) The text 'BOX [ "ignored" : I ]' is not used, so it can be replaced with anything that the parser accepts.  
-   6) REPRESENT Role TYPE <something> is not allowed: A 'Role' may not be a primitive type (it must be an Object-type)
--}	INTERFACE "SessionUser" FOR NobodyInParticular : '_SESSION';V[SESSION*User] BOX [ "ignored" : I]
+   3) The expression between the ':' and 'BOX', must provide all pairs (s,r), where r is an activated role in SESSION s.
+   4) The SRC-concept of the expression between the ':' and 'BOX' MUST be 'SESSION'
+   5) The TGT-concept of that expression between the ':' and 'BOX' MUST be 'Role'
+   6) In order to ensure that the concept 'Role' contains all <Rolename>s that are used within the FrontEnd, e.g. for
+      - MAINTAINing rules 
+      - accessing INTERFACEs
+      - using extensions (e.g. ExcelImport, or the ExecEngine API, as specified in the config files):
+      you should include statements such as the following, and make sure all <Rolename>s are mentioned:
+      ** POPULATION Role CONTAINS [ "<Rolename1>", "<Rolename2>", ... ]**
+   7) The text 'BOX [ "ignored" : I ]' is not used, so it can be replaced with anything that the parser accepts.  
+   8) REPRESENT Role TYPE <something> is not allowed: A 'Role' may not be a scalar (it must be an Object-type)
+-} INTERFACE "SessionUser" FOR NobodyInParticular : '_SESSION';V[SESSION*User] BOX [ "ignored" : I]
 {- Notes:
    1) The name of the interface must be 'SessionUser'
    2) The 'FOR NobodyInParticular' ensures that the INTERFACE does not show up in a menu.
-   3) The SRC concept of the expression between the ':' and 'BOX' MUST be SESSION
-   4) The TGT of the interface expression must be the User
+   3) The SRC-concept of the expression between the ':' and 'BOX' MUST be 'SESSION'
+   4) The TGT-concept of the interface expression must be 'User'
    5) The text 'BOX [ "ignored" : I ]' is not used, so it can be replaced with anything that the parser accepts.
    6) REPRESENT User TYPE <something> is not allowed: A 'User' may not be a scalar (it must be an Object-type)
 -}
