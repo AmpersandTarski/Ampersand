f04cc6521f69a9c16fd18813a10fe6049fa3e4bc
Michiel-s <michielstornebrink@gmail.com>
2015-11-03 10:30:46 +0100

    Move viewer class to index.php

--- static/zwolle/fw/includes.php
+++ static/zwolle/fw/includes.php
@@ -25,7 +25,6 @@ require_once (__DIR__ . '/Role.php');
 require_once (__DIR__ . '/RuleEngine.php');
 require_once (__DIR__ . '/Session.php');
 require_once (__DIR__ . '/InterfaceObject.php');
-require_once (__DIR__ . '/Viewer.php');
 
 require_once (__DIR__ . '/../localSettings.php');
 
