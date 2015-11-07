acbd148494ded5b338e2b3d922918db8bf545cda
Han Joosten <han.joosten.han@gmail.com>
2015-11-07 19:37:51 +0100

    Now also rules.json is generated (issue #197)

--- src/Database/Design/Ampersand/Output/ToJSON/ToJson.hs
+++ src/Database/Design/Ampersand/Output/ToJSON/ToJson.hs
@@ -5,12 +5,15 @@ import Database.Design.Ampersand.Output.ToJSON.JSONutils
 import Database.Design.Ampersand.Output.ToJSON.Settings
 import Database.Design.Ampersand.Output.ToJSON.MySQLInstaller
 import Database.Design.Ampersand.Output.ToJSON.Relations
+import Database.Design.Ampersand.Output.ToJSON.Rules 
 
 generateJSONfiles :: FSpec -> IO ()
 generateJSONfiles fSpec =
  sequence_ [ writeJSON "settings" settings
            , writeJSON "mysql-installer" mySqlInstaller
-           , writeJSON "relations" relations]
+           , writeJSON "relations" relations
+           , writeJSON "rules" rules
+           ]
 
   where 
     writeJSON :: ToJSON  a => String -> a -> IO()
@@ -21,6 +24,8 @@ generateJSONfiles fSpec =
     mySqlInstaller = fromAmpersand fSpec fSpec
     relations :: Relations
     relations = fromAmpersand fSpec fSpec
+    rules :: Rules
+    rules = fromAmpersand fSpec fSpec
     
 {- Note on data structure convention
    The data definitions in this module are not ment to be exported. The idea on naming is that all names
