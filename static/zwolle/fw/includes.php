e57672eeaf71aaf82c16b295eb2c6fe9555fc8c5
Michiel-s <michielstornebrink@gmail.com>
2015-11-07 16:28:59 +0100

    Include new files

--- static/zwolle/fw/includes.php
+++ static/zwolle/fw/includes.php
@@ -16,15 +16,18 @@ require_once (__DIR__ . '/functions/JsonPatch.php'); // to compare 2 different J
 require_once (__DIR__ . '/../generics/Generics.php'); // loading the Ampersand model
 
 require_once (__DIR__ . '/Config.php');
-require_once (__DIR__ . '/Database.php');
-require_once (__DIR__ . '/Concept.php');
+require_once (__DIR__ . '/Hooks.php');
+
+require_once (__DIR__ . '/AngularApp.php');
 require_once (__DIR__ . '/Atom.php');
+require_once (__DIR__ . '/Concept.php');
+require_once (__DIR__ . '/Database.php');
+require_once (__DIR__ . '/InterfaceObject.php');
 require_once (__DIR__ . '/Notifications.php');
 require_once (__DIR__ . '/Relation.php');
 require_once (__DIR__ . '/Role.php');
 require_once (__DIR__ . '/RuleEngine.php');
 require_once (__DIR__ . '/Session.php');
-require_once (__DIR__ . '/InterfaceObject.php');
 
 require_once (__DIR__ . '/../localSettings.php');
 
