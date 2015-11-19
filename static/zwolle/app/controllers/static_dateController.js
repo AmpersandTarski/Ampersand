36835a7423fe9c98e732218b5e9e3c9642df76ff
Michiel-s <michielstornebrink@gmail.com>
2015-11-19 20:40:50 +0100

    Bugfix

--- static/zwolle/app/controllers/static_dateController.js
+++ static/zwolle/app/controllers/static_dateController.js
@@ -17,11 +17,13 @@ AmpersandApp.controller('static_dateController', function ($scope) {
 	};
 	
 	function modifyToJSON(obj){
-		obj.toJSON = function(){
-			return this.getUTCFullYear()
-		        + '-' + pad(this.getMonth() + 1) // The getMonth() method returns the month in the specified date according to local time, as a zero-based value (where zero indicates the first month of the year).
-		        + '-' + pad(this.getDate());
-		};
+		if(obj != null){
+			obj.toJSON = function(){
+				return this.getUTCFullYear()
+			        + '-' + pad(this.getMonth() + 1) // The getMonth() method returns the month in the specified date according to local time, as a zero-based value (where zero indicates the first month of the year).
+			        + '-' + pad(this.getDate());
+			};
+		}
 	};
 	
 	$scope.selected = { value : ''}; // an empty object for temporary storing the input values
