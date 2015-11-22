849da812cceb9e952e2d93c8e348512b35f4acd0
Michiel-s <michielstornebrink@gmail.com>
2015-11-22 21:11:46 +0100

    WIP on CRUD functionality

--- static/zwolle/app/controllers/static_addObjectController.js
+++ static/zwolle/app/controllers/static_addObjectController.js
@@ -1,4 +1,4 @@
-AmpersandApp.controller('addObjectController', function($scope){
+AmpersandApp.controller('addObjectController', function($scope, $rootScope, Restangular){
 	
 	$scope.selected = {}; // an empty object for temporary storing the typeahead selection
 	
@@ -9,4 +9,28 @@ AmpersandApp.controller('addObjectController', function($scope){
 		$scope.addObject(obj, property, $item, resourceId);
 		$scope.hasNoResults = false;
 	};
+	
+	// Add object as new resource in interface
+	$scope.addExistingResourceOnSelect = function ($item, $model, $label, obj, property, prepend, requestType){
+		if(prepend === 'undefined') var prepend = false;
+		requestType = requestType || $rootScope.defaultRequestType; // set requestType. This does not work if you want to pass in a falsey value i.e. false, null, undefined, 0 or ""
+		
+		// Post to backend		
+		$scope.loadingInterface.push(
+			$scope.srcAtom.all(property)
+				.post($item, {'requestType' : requestType})
+				.then(function(data){ // POST
+					$rootScope.updateNotifications(data.notifications);
+					
+					if(prepend) obj[property].unshift(Restangular.restangularizeElement($scope.srcAtom, data.content, property)); // Add to collection
+					else obj[property].push(Restangular.restangularizeElement($scope.srcAtom, data.content, property)); // Add to collection
+					
+					showHideButtons(data.invariantRulesHold, data.requestType, data.content.id);
+				})
+		);
+		
+		$scope.hasNoResults = false;
+		
+	};
+	
 });
