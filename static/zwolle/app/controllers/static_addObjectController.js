b5d756a73a1dd388e1b15b01f38078d1a6cc5431
Michiel-s <michielstornebrink@gmail.com>
2015-11-22 12:25:06 +0100

    Show/hide plus for creating new atom in Atomic-Object template

--- static/zwolle/app/controllers/static_addObjectController.js
+++ static/zwolle/app/controllers/static_addObjectController.js
@@ -2,8 +2,11 @@ AmpersandApp.controller('addObjectController', function($scope){
 	
 	$scope.selected = {}; // an empty object for temporary storing the typeahead selection
 	
+	$scope.hasNoResults = false;
+	
 	// Regular function used by Atomic-OBJECT template
 	$scope.typeaheadOnSelect = function ($item, $model, $label, obj, property, resourceId){
 		$scope.addObject(obj, property, $item, resourceId);
+		$scope.hasNoResults = false;
 	};
 });
