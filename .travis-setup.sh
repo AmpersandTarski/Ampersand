c13be82e7f46e7a1eab10707896ac73617642494
Han Joosten <han.joosten.han@gmail.com>
2015-12-21 13:46:39 +0100

    Create .travis-setup.sh

---
+++ .travis-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -eux
+
+mkdir -p ~/.local/bin;
+if [ `uname` = "Darwin" ]; then
+  curl -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin;
+else
+  curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack';
+fi;
+stack --no-terminal setup;
+
+case "$BUILD" in
+  stack)
+    ;;
+  cabal)
+mkdir -p $HOME/.cabal
+cat > $HOME/.cabal/config <<EOF
+remote-repo: hackage.haskell.org:http://hackage.fpcomplete.com/
+remote-repo-cache: $HOME/.cabal/packages
+jobs: \$ncpus
+EOF
+;;
+esac
