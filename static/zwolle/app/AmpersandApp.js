a165c76e49fdaf328822be813d94ccf25aee3aee
Michiel-s <michielstornebrink@gmail.com>
2015-12-22 17:21:58 +0100

    Improve show/hide of glyphicons

--- static/zwolle/app/AmpersandApp.js
+++ static/zwolle/app/AmpersandApp.js
@@ -97,12 +97,14 @@ AmpersandApp.value('cgBusyDefaults',{
 AmpersandApp.directive('myShowonhoverBox', function (){
 	return {
 		link : function(scope, element, attrs) {
-			element.hide(); // default hide
+			if(!element.closest('.box').hasClass('my-showonhover-box-show')) element.hide(); // default hide
 			
 			element.closest('.box').bind('mouseenter', function() {
+				element.closest('.box').addClass('my-showonhover-box-show');
 				element.show();
 			});
 			element.closest('.box').bind('mouseleave', function() {
+				element.closest('.box').removeClass('my-showonhover-box-show');
 				element.hide();
 			});
 		}
