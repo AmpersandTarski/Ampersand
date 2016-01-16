cafcac79e014eaa7c8bd85ee429973e10764759b
Rieks <rieks.joosten@tno.nl>
2016-01-16 08:01:36 +0100

    Messaging - creation of a Nonnce should be done uniformely (and make use of strong functions if they're there)

--- static/zwolle/extensions/ExecEngine/functions/MSG_CEPValidation.php
+++ static/zwolle/extensions/ExecEngine/functions/MSG_CEPValidation.php
@@ -4,6 +4,13 @@
 // Enable Messaging extension: MSG_Validation
 // Config::set('url', 'msg_validation', ''); // msg_validation URL where response needs to be filled in.
 
+function CreateCvrNonce()
+{   Notifications::addLog('PHP_MAJOR_VERSION = '.PHP_MAJOR_VERSION,'MESSAGING');
+    if (PHP_MAJOR_VERSION < 7) return(rand(1,999999));
+    Notifications::addLog('using random_int for generating the nonce','MESSAGING');
+    return(random_int(1,999999)); // 'random_int' only is available from PHP 7
+}
+
 function CreateCvrURLText()
 {	$url = Config::get('url', 'msg_validation');
     Notifications::addLog('Using URL for filling in response: '.$url,'MESSAGING');
