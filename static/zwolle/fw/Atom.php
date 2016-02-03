d7bba1fff6854d12c61d1f671b3a7fe576528a87
Michiel-s <michielstornebrink@gmail.com>
2016-02-03 13:09:02 +0100

    Default linkto data to false

--- static/zwolle/fw/Atom.php
+++ static/zwolle/fw/Atom.php
@@ -150,7 +150,7 @@ Class Atom {
 		$options['arrayType'] = isset($options['arrayType']) ? $options['arrayType'] : 'num';
 		$options['metaData'] = isset($options['metaData']) ? filter_var($options['metaData'], FILTER_VALIDATE_BOOLEAN) : true;
 		$options['navIfc'] = isset($options['navIfc']) ? filter_var($options['navIfc'], FILTER_VALIDATE_BOOLEAN) : true;
-		$options['inclLinktoData'] = isset($options['inclLinktoData']) ? filter_var($options['inclLinktoData'], FILTER_VALIDATE_BOOLEAN) : true;
+		$options['inclLinktoData'] = isset($options['inclLinktoData']) ? filter_var($options['inclLinktoData'], FILTER_VALIDATE_BOOLEAN) : false;
 		
 		$idEsc = $this->database->escape($this->id);
 		$query = "SELECT DISTINCT `tgt` FROM ($interface->expressionSQL) AS `results` WHERE `src` = '$idEsc' AND `tgt` IS NOT NULL";
