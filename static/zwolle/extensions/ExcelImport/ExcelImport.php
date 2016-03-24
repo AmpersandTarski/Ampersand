d89463a50a3f9f49123fe6ac6e81e72faa9b0005
Michiel-s <michielstornebrink@gmail.com>
2016-03-24 20:05:15 +0100

    Refactor ExelImport extension

--- static/zwolle/extensions/ExcelImport/ExcelImport.php
+++ static/zwolle/extensions/ExcelImport/ExcelImport.php
@@ -9,16 +9,24 @@ AngularApp::addJS('extensions/ExcelImport/ui/js/ExcelImport.js');
 // Config
 Config::set('allowedMimeTypes', 'excelImport', array('application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/excel'));
 
-class ImportExcel {
-	public $file;
-	private $db;
+class ExcelImport {
+	/**
+	 * 
+	 * @var string
+	 */
+    public $file;
 
-	function __construct($filename){
-		$this->file = $filename;
-		$this->db = Database::singleton();
-	}
-
-	public function ParseFile(){
+	/**
+	 * Constructor
+	 */
+	function __construct(){}
+	
+	/**
+	 * @param string $filename
+	 * @return void
+	 */
+	public function ParseFile($filename){
+	    $this->file = $filename;
 		
 		Notifications::addLog('------------------------- EXCEL IMPORT STARTED -------------------------', 'ExcelImport');
 				
@@ -26,14 +34,13 @@ class ImportExcel {
 		
 		Notifications::addLog('------------------------- END OF EXCEL IMPORT -------------------------', 'ExcelImport');
 		
-		// Close transaction => ROLLBACK or COMMIT.
-		$this->db->closeTransaction('File uploaded', true);
-		
-		return Notifications::getAll();
-		
 	}
 	
-	public function ProcessFileContent(){
+	/**
+	 * 
+	 * @return void
+	 */
+	private function ProcessFileContent(){
 		$objPHPExcel = PHPExcel_IOFactory::load($this->file);
 
 		// Format is as follows:
@@ -43,11 +50,9 @@ class ImportExcel {
 		//              <srcAtomB>,                <tgtAtom1B>,    <tgtAtomNB>
 		//              <srcAtomC>,                <tgtAtom1C>,    <tgtAtomNC>
 
-		// Output is function call: 
-		// InsPair($relation,$srcConcept,$srcAtom,$tgtConcept,$tgtAtom)
-
 		// Loop over all worksheets
 		foreach($objPHPExcel->getWorksheetIterator() as $worksheet){
+			
 			// Loop through all rows
 			$highestrow = $worksheet->getHighestRow();
 			$highestcolumn = $worksheet->getHighestColumn();
@@ -59,11 +64,12 @@ class ImportExcel {
 				if (substr($worksheet->getCell('A' . $row)->getValue(), 0, 1) === '[') break;
 			} // We are now at the beginning of a table or at the end of the file.
 	
-			$line = array(); // Line is a buffer of one or more related (subsequent) excel rows
+			$lines = array(); // Line is a buffer of one or more related (subsequent) excel rows
 	
 			while ($row <= $highestrow){
 				// Read this line as an array of values
-				$values = array(); // values is a buffer containing the cells in a single excel row 
+				
+				$line = array(); // values is a buffer containing the cells in a single excel row 
 				for ($columnnr = 0; $columnnr < $highestcolumnnr; $columnnr++){ 
 					$columnletter = PHPExcel_Cell::stringFromColumnIndex($columnnr);
 					$cell = $worksheet->getCell($columnletter . $row);
@@ -74,125 +80,111 @@ class ImportExcel {
 					// the @ is a php indicator for a unix timestamp (http://php.net/manual/en/datetime.formats.compound.php), later used for typeConversion
 					if(PHPExcel_Shared_Date::isDateTime($cell) && !empty($cellvalue)) $cellvalue = '@'.(string)PHPExcel_Shared_Date::ExcelToPHP($cellvalue); 
 					
-					$values[] = $cellvalue;
+					$line[] = $cellvalue;
 				}
-				$line[] = $values; // add line (array of values) to the line buffer
+				$lines[] = $line; // add line (array of values) to the line buffer
 	
 				$row++;
 				// Is this relation table done? Then we parse the current values into function calls and reset it
 				$firstCellInRow = (string)$worksheet->getCell('A' . $row)->getCalculatedValue();
 				if (substr($firstCellInRow, 0, 1) === '['){ 
 					// Relation table is complete, so it can be processed.
-					$this->ParseLines($line);
-					$line = array();
+					$this->ParseLines($lines);
+					$lines = array();
 				}
 			}
 			
 			// Last relation table remains to be processed.
-			$this->ParseLines($line);
-			$line = array();
+			$this->ParseLines($lines);
+			$lines = array();
 		}
 	}
-
-	// Format is as follows:
-	//		      [ <description of data> ], <relation1>,    <relationN>  
-	//              <srcConcept>,              <tgtConcept1>,  <tgtConceptN>
-	//              <srcAtomA>,                <tgtAtom1A>,    <tgtAtomNA>
-	//              <srcAtomB>,                <tgtAtom1B>,    <tgtAtomNB>
-	//              <srcAtomC>,                <tgtAtom1C>,    <tgtAtomNC>
-
-	// Output is function call: 
-	// InsPair($relation,$srcConcept,$srcAtom,$tgtConcept,$tgtAtom)
-	private function ParseLines($data)
-	{	$relation = $concept = $separator = $atom = array();
+	
+	/**
+	 * 
+	 * @param array $lines
+	 */
+	private function ParseLines($lines){
+	    $relations = $concept = $separator = array();
 		
-		foreach ($data as $linenr => $values)
-		{	$totalcolumns = count($values);
+		foreach ($lines as $linenr => $line){
+		    $totalcolumns = count($line);
 			
-			if ($linenr == 0) // First line specifies relation names:
-			{	for ($col = 0; $col < $totalcolumns; $col++)
-				{	$relation[$col] = $values[$col]; // Next: check to see if there is a trailing 'flip' ('~')
-					$flipped[$col] = ( strpos($relation[$col],'~') === (strlen($relation[$col])-1) );
-					if ($flipped[$col]) $relation[$col] = substr($relation[$col],0,strlen($relation[$col])-1);
+		    // First line specifies relation names
+			if ($linenr == 0){
+			    for ($col = 0; $col < $totalcolumns; $col++){
+			        $relations[$col] = $line[$col];
 				}
-			}elseif ($linenr == 1) // Second line specifies concepts (and optionally: separators):
-// In the Haskell importer, separators are the last character before the ']' if the concept is surrounded by such block quotes. Alternatively, you could specify a separator following such block-quotes, allowing for multiple-character separators.
-			{	for ($col = 0; $col < $totalcolumns; $col++)
-// The cell contains either 'Concept' or '[Conceptx]' where x is a separator character (e.g. ';', ',', ...)
-				{	if (  (strpos($values[$col], '[') === 0) // if we have a '[' at the first position
-					   && (strrpos($values[$col], ']') === (strlen($values[$col])-1)) // and a ']' at the last position
-					   )
-					{ // then we have a concept AND separator specification
-//						 0123456789   positions/indexes
-//						 [Concept,]   length=10, separatorpos=length-2
-						$concept[$col] = substr($values[$col], 1, strlen($values[$col])-3);
-						$separator[$col] = substr($values[$col], strlen($values[$col])-2, 1);
-						Notifications::addLog("Multiple '$concept[$col]'-atoms allowed: separator='$separator[$col]'.", 'ExcelImport');
-					}else
-					{	$concept[$col] = $values[$col];
+			
+			// Second line specifies concepts (and optionally: separators)
+			}elseif ($linenr == 1){
+			    // In the Haskell importer, separators are the last character before the ']' if the concept is surrounded by such block quotes. Alternatively, you could specify a separator following such block-quotes, allowing for multiple-character separators.
+				for ($col = 0; $col < $totalcolumns; $col++){
+				    
+				    if($line[$col] == ''){
+				        $concept[$col] = null;
+				    // The cell contains either 'Concept' or '[Conceptx]' where x is a separator character (e.g. ';', ',', ...)
+				    }elseif ((substr($line[$col], 0, 1) == '[') && (substr($line[$col], -1) == ']') ){
+				        if($col == 0) throw new Exception ("Seperator character not allowed for first column of excel import. Specified '{$line[$col]}'", 500);
+				        $concept[$col] = Concept::getConcept(substr($line[$col], 1, -2));
+						$separator[$col] = substr($line[$col], -2, 1);
+					}else{
+					    $concept[$col] = Concept::getConcept($line[$col]);
 						$separator[$col] = false;
 					}
+					
+					// Determine relations for all cols except col 0
+					if($col > 0){
+    					if($relations[$col] == ''){
+    					    $relations[$col] = null;
+    					}elseif(substr($relations[$col], -1) == '~'){ // Relation is flipped is last character is a tilde (~)
+    					    $relations[$col] = Relation::getRelation(substr($relations[$col], 0, -1), $concept[$col]->name, $concept[0]->name);
+    					    $flipped[$col] = true;
+    					}else{
+    					    $relations[$col] = Relation::getRelation($relations[$col], $concept[0]->name, $concept[$col]->name);
+    					    $flipped[$col] = false;
+    					}
+					}
 				}
-			}else // All other lines specify atoms:
-			{	for ($col = 0; $col < $totalcolumns; $col++) $atom[$col] = $values[$col];
-
-				// Don't process lines that start with an empty first cell
-				if ($atom[0] == '' OR empty($atom[0])) continue;
-
-//				// Check if this is an atom-create line, syntax = &atomname
-//				if (strpos('&', $atom[0]) === 0){ 
-				// Check if this is an atom-create line, syntax = _NEW
-				if ($atom[0] === '_NEW')
-  				{	$atom[0] = Concept::getConcept($concept[0])->createNewAtomId(); // Create a unique atom name
-					Notifications::addLog("_NEW atom created: '$atom[0]'.", 'ExcelImport');
-				}
+			
+			// All other lines specify atoms
+			}else{			    
+			    $line[0] = trim($line[0]); // Trim cell content
+			    
+			    // Determine left atom (column 0) of line
+				if ($line[0] == '') continue; // Don't process lines that start with an empty first cell
+				elseif ($line[0] == '_NEW') $leftAtom = $concept[0]->createNewAtom(); // Create a unique atom name
+				else $leftAtom = new Atom($line[0], $concept[0]->name);
 				
-				// Insert $atom[0] into the DB if it does not yet exist
-				$this->addAtomToConcept($atom[0], $concept[0]);
-
-				for ($col = 1; $col < $totalcolumns; $col++) // Now we transform the data info function calls:
-				{	if ($atom[$col] == '' OR empty($atom[$col])) continue; // Empty cells are allowed but shouldn't do anything
-					if ($concept[$col] == '' OR empty($concept[$col])) continue; // if no concept is specified, the contents of the cell should be ignored.
-					if ($relation[$col] == '' OR empty($relation[$col])) continue; // if no relation is specified, the contents of the cell should be ignored.
-//					if (strpos('&', $tgtatom) === 0){ // Check if this is an atom-create line, syntax = &atomname
-					if ($atom[$col] === '_NEW') // Check if this is an atom-create line, syntax = &atomname
-					{	$atom[$col] = $atom[0]; // '_NEW' copies the atom-value; useful for property-relations.
+				// Insert $leftAtom into the DB if it does not yet exist
+				$leftAtom->addAtom();
+				
+				// Process other columns of line
+				for ($col = 1; $col < $totalcolumns; $col++){
+				    
+					if (is_null($concept[$col])) continue; // If no concept is specified, the cell is skipped
+					if (is_null($relations[$col])) continue; // If no relation is specified, the cell is skipped
+					
+					// Determine right atom(s)
+					$rightAtoms = array();
+					
+					$cell = trim($line[$col]); // Trim cell content
+					
+					if ($cell == '') continue; // If cell is empty, it is skipped
+					elseif ($cell == '_NEW') $rightAtoms[] = $leftAtom; // If the cell contains '_NEW', the same atom as the $leftAtom is used. Useful for property-relations
+					elseif($separator[$col]){
+					    $atomsIds = explode($separator[$col],$cell);
+					    foreach($atomsIds as $atomId) $rightAtoms[] = new Atom($atomId, $concept[$col]->name);
+					}else{
+					    $rightAtoms[] = new Atom($line[$col], $concept[$col]->name);
 					}
-
-					$atoms = array();
-					if ($separator[$col])
-					{	$atoms = explode($separator[$col],$atom[$col]);
-					}else
-					{	$atoms[] = $atom[$col];
-					}
-					Notifications::addLog("atoms-array has length ".count($atoms), 'ExcelImport');
-					for ($i=0; $i < count($atoms); $i++)
-					{	$tgtatom = $atoms[$i];
-						$tgtatom = trim($tgtatom); // remove leading and trailing spaces, tabs, newlines, etc.
-						Notifications::addLog("i='$i', tgtatom='$atoms[$i]', trimmed='$tgtatom'", 'ExcelImport');
-						
-						if ($flipped[$col])
-						{	$this->insertRel($relation[$col],$tgtatom,$atom[0],$concept[$col],$concept[0]);
-			Notifications::addLog("flippdRel($relation[$col],$tgtatom,$atom[0],$concept[$col],$concept[0])", 'ExcelImport');
-						}else
-						{	$this->insertRel($relation[$col],$atom[0],$tgtatom,$concept[0],$concept[$col]);
-			Notifications::addLog("normalRel($relation[$col],$atom[0],$tgtatom,$concept[0],$concept[$col])", 'ExcelImport');
-						}
+					
+					foreach ($rightAtoms as $rightAtom){
+					    $relations[$col]->addLink($leftAtom, $rightAtom, $flipped[$col], 'ExcelImport');
 					}
 				}
-				$atom = array();
 			}
 		}
-	}
-	
-	private function addAtomToConcept($atomId, $conceptName){
-		$this->db->addAtomToConcept(new Atom($atomId, $conceptName));
-		
-	}
-	
-	private function insertRel($relationName, $srcAtom, $tgtAtom, $srcConcept, $tgtConcept){
-	    $relation = Relation::getRelation($relationName, $srcConcept, $tgtConcept);
-	    $relation->addLink(new Atom($srcAtom, $srcConcept), new Atom($tgtAtom, $tgtConcept));
 	}
 }
 
