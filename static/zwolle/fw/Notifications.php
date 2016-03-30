8856d18fcdebaea61120da97c1a6f162ba9912c4
Michiel-s <michielstornebrink@gmail.com>
2016-03-30 15:52:01 +0200

    Refactor and restructure notification class

--- static/zwolle/fw/Notifications.php
+++ static/zwolle/fw/Notifications.php
@@ -8,20 +8,28 @@ Config::set('defaultAutoHideSuccesses', 'notifications', true);
 Config::set('defaultShowErrors', 'notifications', true);
 Config::set('defaultShowInvariants', 'notifications', true);
 
-// TODO: move notifications functions to new logger class
 class Notifications {
 	
-	static $errors = array();
-	static $invariants = array();
-	static $warnings = array();
-	static $violations = array();
-	static $infos = array();	
-	static $successes = array();
+	private static $errors = array();
+	private static $invariants = array();
+	private static $warnings = array();
+	private static $signals = array();
+	private static $infos = array();	
+	private static $successes = array();
+
+/**************************************************************************************************
+ *
+ * Notifications for: user logs (info, notice (success), warning and error)
+ *
+ *************************************************************************************************/
 	
 	/**
-	 * Add notifications for user (e.g. catched exceptions)
+	 * Add notifications from userlog  user (e.g. catched exceptions)
+	 * DON't use this function in code. Log via userlogger instead => \Ampersand\Logger::getUserLogger()->...
 	 * @param int $level
 	 * @param string $message
+	 * @return void
+	 * @throws Exception when notification level is not supported
 	 */
 	public static function addNotification($level, $message){
 	    switch($level){
@@ -29,6 +37,7 @@ class Notifications {
 	            self::addInfo($message);
 	            break;
 	        case 250 : // Notice
+	            self::addSuccess($message);
 	        case 300 : // Warning
 	            self::addWarning($message);
 	            break;
@@ -44,39 +53,26 @@ class Notifications {
 	}
 	
 	/**
-	 * DEPRECATED FUNCTION
-	 * @param unknown $message
-	 * @param string $code
+	 * Add error notification for user
+	 * @param string $message
+	 * @return void
+	 * TODO: make private function
 	 */
-	public static function addError($message, $code = null){
+	public static function addError($message){
 		$errorHash = hash('md5', $message);
 		
 		self::$errors[$errorHash]['message'] = $message;
-		self::$errors[$errorHash]['code'] = $code;
 		self::$errors[$errorHash]['count']++;
-		// Provide backtrace only for the first time of this error occurrence to safe memory
-		if(Config::get('debugMode') && self::$errors[$errorHash]['count'] <= 1) self::$errors[$errorHash]['details'] = '<pre>' . print_r(debug_backtrace(), true) . '</pre>';
-		self::addLog($message, 'ERROR');
+		
+	    // Provide backtrace in debug mode (only first time)
+		if(Config::get('debugMode') && self::$errors[$errorHash]['count'] <= 1) 
+		    self::$errors[$errorHash]['details'] = '<pre>' . print_r(debug_backtrace(), true) . '</pre>';
 	}
 	
 	/**
-	 * DEPRECATED FUNCTION
-	 * @param Exception $e
-	 */
-	public static function addErrorException($e){
-	    $errorHash = hash('md5', $e->getMessage());
-	    
-	    self::$errors[$errorHash]['message'] = $e->getMessage();
-	    self::$errors[$errorHash]['code'] = $e->getCode();
-	    self::$errors[$errorHash]['count']++;
-	    if(Config::get('debugMode')) self::$errors[$errorHash]['details'] = nl2br($e->getTraceAsString());
-	    self::addLog($e->getMessage(), 'ERROR');
-	}
-	
-	/**
-	 * Add warning message for user
+	 * Add warning notification for user
 	 * @param string $message
-	 * @param int $code
+	 * @return void
 	 */
 	private static function addWarning($message){
 	    $hash = hash('md5', $message);
@@ -85,6 +81,59 @@ class Notifications {
 	    self::$warnings[$hash]['count']++;
 	}
 	
+	/**
+	 * Add success notification for user
+	 * @param string $message
+	 * @return void
+	 * TODO: make private function
+	 */
+	public static function addSuccess($message){
+	    self::$successes[] = array('message' => $message);
+	}
+	
+	/**
+	 * Add info notification for user 
+	 * @param string $message
+	 * @param string $id
+	 * @param string $aggregatedMessage
+	 * @return string
+	 * TODO: make private function
+	 */
+	public static function addInfo($message, $id = null, $aggregatedMessage = null){
+	
+	    if(isset($id)){ // ID can be integer, but also string
+	        self::$infos[$id]['rows'][] = $message;
+	        self::$infos[$id]['count'] = count(self::$infos[$id]['rows']);
+	        if(!is_null($aggregatedMessage)) self::$infos[$id]['message'] = $aggregatedMessage;
+	        	
+	        self::addLog(self::$infos[$id]['message'] .' - ' . $message, 'INFO');
+	        	
+	        return $id;
+	    }else{
+	        self::$infos[]['message'] = $message;
+	        	
+	        self::addLog($message, 'INFO');
+	        	
+	        end(self::$infos); // pointer to end of array (i.e. new  inserted element)
+	        return key(self::$infos); // return key of current element
+	    }
+	
+	}
+	
+	/**
+	 * DEPRECATED FUNCTION
+	 * @param string $message
+	 * @param string $type
+	 */
+	public static function addLog($message, $type = 'LOG'){
+	
+	}
+
+/**************************************************************************************************
+ * 
+ * Notifications for: invariant and signal rule violations
+ * 
+ *************************************************************************************************/
 	/**
 	 * 
 	 * @param Violation $violation
@@ -105,90 +154,26 @@ class Notifications {
 	public static function addSignal($violation){
 		$ruleHash = hash('md5', $violation->rule->id);
 		
-		self::$violations[$ruleHash]['ruleMessage'] = $violation->rule->getViolationMessage();
-		self::$violations[$ruleHash]['tuples'][] = array('violationMessage' => ($violationMessage = $violation->getViolationMessage())
+		self::$signals[$ruleHash]['ruleMessage'] = $violation->rule->getViolationMessage();
+		self::$signals[$ruleHash]['tuples'][] = array('violationMessage' => ($violationMessage = $violation->getViolationMessage())
 		                                                ,'links' => $violation->getLinks());
 		self::addLog($violationMessage, 'VIOLATION');
 	}
 	
-	public static function addInfo($message, $id = null, $aggregatedMessage = null){
-		
-		if(isset($id)){ // ID can be integer, but also string
-			self::$infos[$id]['rows'][] = $message;
-			self::$infos[$id]['count'] = count(self::$infos[$id]['rows']);
-			if(!is_null($aggregatedMessage)) self::$infos[$id]['message'] = $aggregatedMessage;
-			
-			self::addLog(self::$infos[$id]['message'] .' - ' . $message, 'INFO');
-			
-			return $id;
-		}else{
-			self::$infos[]['message'] = $message;
-			
-			self::addLog($message, 'INFO');
-			
-			end(self::$infos); // pointer to end of array (i.e. new  inserted element)
-			return key(self::$infos); // return key of current element
-		}
-		
-	}
-	
-	public static function addSuccess($message, $id = null, $aggregatedMessage = null){
-		
-		if(isset($id)){ // ID can be integer, but also string
-			self::$successes[$id]['rows'][] = $message;
-			if(!is_null($aggregatedMessage)) self::$successes[$id]['message'] = $aggregatedMessage;
-			
-			self::addLog(self::$successes[$id]['message'] .' - ' . $message, 'SUCCESS');
-			
-			return $id;
-		}else{
-			self::$successes[]['message'] = $message;
-			
-			self::addLog($message, 'SUCCESS');
-			
-			end(self::$successes); // pointer to end of array (i.e. new  inserted element)
-			return key(self::$successes); // return key of current element
-		}
-		
-	}
-	
-	/**
-	 * DEPRECATED FUNCTION
-	 * @param string $message
-	 * @param string $type
-	 */
-	public static function addLog($message, $type = 'LOG'){
-		
-	}
-	
-	public static function getErrors(){
-		return array_values(self::$errors);
-	}
-	public static function getWarnings(){
-	    return array_values(self::$warnings);
-	}
-	public static function getInvariants(){
-		return array_values(self::$invariants);
-	}
-	public static function getViolations(){
-		return array_values(self::$violations);
-	}
-	public static function getInfos(){
-		return array_values(self::$infos);
-	}
-	public static function getSuccesses(){
-		return array_values(self::$successes);
-	}
+/**************************************************************************************************
+ *
+ * Get notifications and default settings
+ *
+ *************************************************************************************************/
 	
 	public static function getAll(){
-		$all['errors'] = self::getErrors();
-		$all['invariants'] = self::getInvariants();
-		$all['warnings'] = self::getWarnings();
-		$all['violations'] = self::getViolations();
-		$all['infos'] = self::getInfos();
-		$all['successes'] = self::getSuccesses();
-		
-		return $all;
+	    return array( 'errors' => array_values(self::$errors)
+	                , 'warnings' => array_values(self::$warnings)
+	                , 'infos' => array_values(self::$infos)
+	                , 'successes' => array_values(self::$successes)
+	                , 'invariants' => array_values(self::$invariants)
+	                , 'violations' => array_values(self::$signals)
+	                );
 	}
 	
 	public static function getDefaultSettings(){
