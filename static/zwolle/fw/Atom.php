d1cd3a2740c4e84adebff8772bb6b836ed71cb7e
Michiel-s <michielstornebrink@gmail.com>
2016-04-01 14:32:55 +0200

    Same as previous commit

--- static/zwolle/fw/Atom.php
+++ static/zwolle/fw/Atom.php
@@ -555,7 +555,7 @@ Class Atom {
 						throw new Exception("Unknown patch operation '" . $patch['op'] ."'. Supported are: 'replace', 'add' and 'remove'", 501);
 				}
 			}catch (Exception $e){
-				$this->logger->error($e);
+				$this->logger->error($e->getMessage());
 				$errorCount++;
 			}
 		}
