5979962d8a892ef5aaa2a22392b2ca355fda3fa3
Han Joosten <han.joosten.han@gmail.com>
2016-04-18 22:22:13 +0200

    Fixes issue #337 (Need better way to unpack files from Ampersand). This will probably also tackle issue #248

--- src/Database/Design/Ampersand/Prototype/WriteStaticFiles.hs
+++ src/Database/Design/Ampersand/Prototype/WriteStaticFiles.hs
@@ -1,29 +1,36 @@
 {-# LANGUAGE CPP #-}
 module Database.Design.Ampersand.Prototype.WriteStaticFiles (writeStaticFiles) where
 
-import Control.Monad
 import System.FilePath
 import System.Directory
 import qualified Data.ByteString.Char8 as BS
 import Database.Design.Ampersand (Options(..),verboseLn)
 import Prelude hiding (writeFile,readFile,getContents)
 import Database.Design.Ampersand.Prototype.StaticFiles_Generated
+import qualified Data.Conduit.List as CL
+import Conduit
 
 writeStaticFiles :: Options -> IO()
 writeStaticFiles opts =
   if genStaticFiles opts
-  then sequence_ [ writeStaticFile opts sf 
-                 | sf <- filter isRequired allStaticFiles
-                 ]
+  then runResourceT $ source $$ sink 
   else verboseLn opts "Skipping static files (because of command line argument)"
- where isRequired :: StaticFile -> Bool
-       isRequired sf = fileKind sf == ZwolleFrontEnd
-writeStaticFile :: Options -> StaticFile -> IO()
-writeStaticFile opts sf =
-  do { createDirectoryIfMissing True (takeDirectory (absFilePath opts sf))
-     ; write (absFilePath opts sf) (contentString sf)
-     }
- where write a b = BS.writeFile a (BS.pack b)
+ where
+    source :: Source (ResourceT IO) StaticFile
+    source = yieldMany $ filter isRequired allStaticFiles
+    sink :: Sink StaticFile (ResourceT IO) ()
+    sink = CL.mapM_ writeStaticFile
 
-absFilePath :: Options -> StaticFile -> FilePath
-absFilePath opts sf = combine (dirPrototype opts) (filePath sf)
+    writeStaticFile :: StaticFile -> (ResourceT IO) ()
+    writeStaticFile sf = 
+       do { lift $ createDirectoryIfMissing True (takeDirectory absFilePath)
+          ; lift $ write absFilePath (contentString sf)
+          }
+      where write a b = BS.writeFile a (BS.pack b)
+            absFilePath :: FilePath
+            absFilePath = combine (dirPrototype opts) (filePath sf)
+    isRequired :: StaticFile -> Bool
+    isRequired sf = fileKind sf == ZwolleFrontEnd
+
+
+
