a294468c7e0a056e4fadc3a3022bffafcbea680d
Michiel-s <michielstornebrink@gmail.com>
2016-05-20 12:46:54 +0200

    Bugfix in atom ifc path. Fixes problem for edits in interfaces by reference

--- static/zwolle/src/Ampersand/Core/Atom.php
+++ static/zwolle/src/Ampersand/Core/Atom.php
@@ -148,7 +148,7 @@ class Atom {
 		if(is_null($this->parentIfc)){
 		  $this->path = 'resources/' . $this->concept->name . '/' . $this->getJsonRepresentation();
 		}else{
-		  $this->path = $this->parentIfc->isRef() ? $this->parentIfc->path : $this->parentIfc->path . '/' . $this->getJsonRepresentation();
+		  $this->path = $this->parentIfc->path . '/' . $this->getJsonRepresentation();
 		}
 	}
 	
