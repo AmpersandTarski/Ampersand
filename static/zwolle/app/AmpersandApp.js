a675a0c50d41278667f6f5b304fdea06418b6c4c
Michiel-s <michielstornebrink@gmail.com>
2016-07-26 14:14:50 +0200

    Include angular-marked lib in app

--- static/zwolle/app/AmpersandApp.js
+++ static/zwolle/app/AmpersandApp.js
@@ -1,5 +1,5 @@
 // when using minified angular modules, use module('myApp', []).controller('MyController', ['myService', function (myService) { ...
-var AmpersandApp = angular.module('AmpersandApp', ['ngResource', 'ngRoute', 'ngSanitize', 'restangular', 'ui.bootstrap', 'uiSwitch', 'cgBusy', 'siTable', 'ng-code-mirror', 'ngStorage', 'angularFileUpload', 'agGrid', 'ui.bootstrap.datetimepicker']);
+var AmpersandApp = angular.module('AmpersandApp', ['ngResource', 'ngRoute', 'ngSanitize', 'restangular', 'ui.bootstrap', 'uiSwitch', 'cgBusy', 'siTable', 'ng-code-mirror', 'ngStorage', 'angularFileUpload', 'agGrid', 'ui.bootstrap.datetimepicker', 'hc.marked']);
 
 AmpersandApp.config(function($routeProvider) {
 	$routeProvider
