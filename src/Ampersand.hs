980bfdec244d7952a3be5ba615eff6e97adf96a1
Han Joosten <han.joosten.han@gmail.com>
2016-08-23 15:24:20 +0200

    Full export of library. move tests to app directory.

--- src/Ampersand.hs
+++ src/Ampersand.hs
@@ -3,25 +3,26 @@ module Ampersand
   , module Ampersand.Basics
   , module Ampersand.Classes
   , module Ampersand.Components
---  , module Ampersand.Core
   , module Ampersand.FSpec
---  , module Ampersand.Graphic
   , module Ampersand.Input
   , module Ampersand.Misc
   , module Ampersand.Output
---  , module Ampersand.Prototype
---  , module Ampersand.Test
+
+  , module Ampersand.Test.TestScripts
+  , module Ampersand.Test.Parser.ParserTest
+  , module Ampersand.Test.Parser.QuickChecks
+  , module Ampersand.Test.Regression
   ) where
 import Ampersand.ADL1
 import Ampersand.Basics
 import Ampersand.Classes
 import Ampersand.Components
---import Ampersand.Core
 import Ampersand.FSpec
---import Ampersand.Graphic
 import Ampersand.Input
 import Ampersand.Misc
 import Ampersand.Output
---import Ampersand.Prototype
---import Ampersand.Test
 
+import Ampersand.Test.Regression
+import Ampersand.Test.TestScripts (getTestScripts,testAmpersandScripts)
+import Ampersand.Test.Parser.ParserTest (parseScripts)
+import Ampersand.Test.Parser.QuickChecks (parserQuickChecks)
