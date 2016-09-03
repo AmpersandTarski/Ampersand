a3ba505b7b9112d476087fc6ccc1f4663ccb3caf
Han Joosten <han.joosten.han@gmail.com>
2016-09-03 15:08:17 +0200

    Populate allRules[Context*Rule]

--- src/Ampersand/FSpec/ShowMeatGrinder.hs
+++ src/Ampersand/FSpec/ShowMeatGrinder.hs
@@ -458,6 +458,8 @@ instance MetaPopulations Rule where
  metaPops fSpec rul =
       [ Comment " "
       , Comment $ " Rule `"++name rul++"` "
+      , Pop "allRules" "Context" "Rule" [Sur]
+             [(dirtyId ctx ctx, dirtyId ctx rul)] 
       , Pop "name"  "Rule" "RuleID" [Uni,Tot]
              [(dirtyId ctx rul, (show.name) rul)]
       , Pop "ruleAdl"  "Rule" "Adl" [Uni,Tot]
