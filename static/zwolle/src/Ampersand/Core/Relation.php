2bcbdc1c2df32c721a62450d4304fc96128afeb8
Michiel-s <michielstornebrink@gmail.com>
2016-10-04 12:54:00 +0200

    #546 Bugfix comment in error message

--- static/zwolle/src/Ampersand/Core/Relation.php
+++ static/zwolle/src/Ampersand/Core/Relation.php
@@ -267,8 +267,8 @@ class Relation {
         $tgtAtom = $isFlipped ? $leftAtom : $rightAtom;
          
         // Checks
-        if(!in_array($srcAtom->concept, $this->srcConcept->getSpecializationsIncl())) throw new Exception ("Cannot insert link ({$srcAtom->__toString()},{$tgtAtom->__toString()}) into relation '{$this->__toString()}', because source concept does not match relation source or its specializations", 500);
-        if(!in_array($tgtAtom->concept, $this->tgtConcept->getSpecializationsIncl())) throw new Exception ("Cannot insert link ({$srcAtom->__toString()},{$tgtAtom->__toString()}) into relation '{$this->__toString()}', because target concept does not match relation target or its specializations", 500);
+        if(!in_array($srcAtom->concept, $this->srcConcept->getSpecializationsIncl())) throw new Exception ("Cannot delete link ({$srcAtom->__toString()},{$tgtAtom->__toString()}) from relation '{$this->__toString()}', because source concept does not match relation source or its specializations", 500);
+        if(!in_array($tgtAtom->concept, $this->tgtConcept->getSpecializationsIncl())) throw new Exception ("Cannot delete link ({$srcAtom->__toString()},{$tgtAtom->__toString()}) from relation '{$this->__toString()}', because target concept does not match relation target or its specializations", 500);
         
         // Delete link from relation table
         $this->db->deleteLink($this, $srcAtom, $tgtAtom);
