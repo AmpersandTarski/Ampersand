fc8b3951c17a8de26d6e27b7e2fc62be1b997dfa
Michiel-s <michielstornebrink@gmail.com>
2016-11-12 18:44:19 +0100

    Replace tabs by spaces

--- static/zwolle/app/controllers/static_uploadFileController.js
+++ static/zwolle/app/controllers/static_uploadFileController.js
@@ -1,22 +1,21 @@
-AmpersandApp.controller('uploadFileController', function($scope, $rootScope, $localStorage, FileUploader){
-	  
-  // File uploader stuff
-  $scope.FileUploader = new FileUploader({
-	alias 				: 'file', // fieldname as used in $_FILES['file']
-	formData 			: [{'roleId[]' : $rootScope.getActiveRoleIds()}], // the '[]' in param 'roleIds[]' is needed by the API to process it as array
-	removeAfterUpload 	: true,
-	autoUpload 			: true
-  });
-  
-  $scope.FileUploader.onSuccessItem = function(fileItem, response, status, headers) {
-	$scope.updateNotifications(response.notifications);
-	
-	// Add response content (newly created FileObject) to ifc list in resource
-	fileItem.resource[fileItem.ifc].push(response.content);
-
-  };
-  
-  $scope.FileUploader.onErrorItem = function(item, response, status, headers){
-	$rootScope.addError(response.error.message, response.error.code, true);
-  };
+AmpersandApp.controller('uploadFileController', function($scope, $rootScope, FileUploader){
+    
+    // File uploader stuff
+    $scope.FileUploader = new FileUploader({
+        alias : 'file', // fieldname as used in $_FILES['file']
+        formData : [{'roleId[]' : $rootScope.getActiveRoleIds()}], // the '[]' in param 'roleIds[]' is needed by the API to process it as array
+        removeAfterUpload : true,
+        autoUpload : true
+    });
+    
+    $scope.FileUploader.onSuccessItem = function(fileItem, response, status, headers){
+        $scope.updateNotifications(response.notifications);
+        
+        // Add response content (newly created FileObject) to ifc list in resource
+        fileItem.resource[fileItem.ifc].push(response.content);
+    };
+    
+    $scope.FileUploader.onErrorItem = function(item, response, status, headers){
+        $rootScope.addError(response.error.message, response.error.code, true);
+    };
 });
