ba1988e2b6ef91a08526134bee7e7356e47bc06d
Han Joosten <han.joosten.han@gmail.com>
2016-12-11 22:21:56 +0100

    Fix in testsuite, to cope with still enabled {+ ... -} notation.

--- src/Ampersand/Test/Parser/ArbitraryTree.hs
+++ src/Ampersand/Test/Parser/ArbitraryTree.hs
@@ -346,7 +346,9 @@ instance Arbitrary P_Markup where
     arbitrary = P_Markup <$> arbitrary <*> arbitrary <*> safeStr `suchThat` noEndMarkup
      where 
        noEndMarkup :: String -> Bool
-       noEndMarkup = not . isInfixOf "+}"
+       noEndMarkup str = and [ not (isInfixOf "+}" str)
+                             , not (isInfixOf "-}" str)  -- to cope with obsolete {+ ... -} notation. 
+                             ]
 
 instance Arbitrary PandocFormat where
     arbitrary = elements [HTML, ReST, LaTeX, Markdown]
