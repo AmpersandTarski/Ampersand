e43c7d07e66478728fb008f6e85d5b318ea331fe
Han Joosten <han.joosten.han@gmail.com>
2016-12-11 19:17:40 +0100

    Revert "Disallow (just temporarily) the use of {+ ... -}  in markup stuff. (Now we have {+ ... +} as replacement."
    This reverts commit 05a4935c14e4e848c44d129b742a152e291cec7e.

--- src/Ampersand/Input/ADL1/Lexer.hs
+++ src/Ampersand/Input/ADL1/Lexer.hs
@@ -235,8 +235,7 @@ lexNestComment _ p []          = lexerError UnterminatedComment p
 --TODO: Also accept {+ ... +} as delimiters
 lexMarkup :: Lexer -> Lexer
 lexMarkup = lexMarkup' ""
- where 
-    -- lexMarkup' str _ p ('-':'}':s) = returnToken (LexMarkup str) p mainLexer (addPos 2 p)  s -- for backwards compatibility with old `{+ ... -}` notation.
+ where lexMarkup' str _ p ('-':'}':s) = returnToken (LexMarkup str) p mainLexer (addPos 2 p)  s -- for backwards compatibility with old `{+ ... -}` notation.
        lexMarkup' str _ p ('+':'}':s) = returnToken (LexMarkup str) p mainLexer (addPos 2 p)  s
        lexMarkup' str c p (x:s)       = lexMarkup' (str++[x]) c (updatePos p x) s
        lexMarkup' _   _ p []          = lexerError UnterminatedMarkup p
