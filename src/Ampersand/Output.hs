59f8c0819898ecc43e855fa41f195fd4782f731a
Han Joosten <han.joosten.han@gmail.com>
2017-07-15 09:16:51 +0200

    first stub

--- src/Ampersand/Output.hs
+++ src/Ampersand/Output.hs
@@ -1,5 +1,6 @@
 module Ampersand.Output (module X) where
 import Ampersand.Output.FSpec2Pandoc as X (fSpec2Pandoc)
+import Ampersand.Output.FSpec2Solidity as X (fSpec2Solidity)
 import Ampersand.Output.PandocAux as X (writepandoc)
 import Ampersand.Output.FSpec2Excel as X
 import Ampersand.Output.Population2Xlsx as X
