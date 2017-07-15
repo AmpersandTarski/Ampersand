327bebb1da9d0db8bc9a9d6726852c55394381c5
Han Joosten <han.joosten.han@gmail.com>
2017-07-15 09:16:16 +0200

    releasenotes

---
+++ src/Ampersand/Output/FSpec2Solidity.hs
@@ -0,0 +1,9 @@
+module Ampersand.Output.FSpec2Solidity (fSpec2Solidity)
+where
+import Ampersand.Misc
+import Ampersand.FSpec
+import Ampersand.FSpec.FPA
+import Ampersand.Basics
+
+fSpec2Solidity :: FSpec -> [String]
+fSpec2Solidity fSpec = ["TODO"]
