996cd7f0c3aac6ba4186dd4cc142017dfa1d323c
Michiel Stornebrink <michielstornebrink@gmail.com>
2017-12-21 22:53:51 +0100

    Rename Ampersand class into AmpersandApp

--- static/zwolle/src/Ampersand/AmpersandApp.php
+++ static/zwolle/src/Ampersand/AmpersandApp.php
@@ -2,7 +2,7 @@
 
 namespace Ampersand;
 
-class Ampersand
+class AmpersandApp
 {
     /**
      * Specifies the required version of the localsettings file that
--- static/zwolle/src/bootstrap.php
+++ static/zwolle/src/bootstrap.php
@@ -31,6 +31,6 @@ require_once (__DIR__ . '/defaultSettings.php');
 
 // Include project specific settings (i.e. localSettings.php file)
 require_once (__DIR__ . '/../localSettings.php');
-if(!defined('LOCALSETTINGS_VERSION') || Ampersand::REQ_LOCALSETTINGS_VERSION > LOCALSETTINGS_VERSION) throw new Exception("New version of localSettings.php required. Please update to format of v" . number_format (Ampersand::REQ_LOCALSETTINGS_VERSION, 1), 500);
+if(!defined('LOCALSETTINGS_VERSION') || AmpersandApp::REQ_LOCALSETTINGS_VERSION > LOCALSETTINGS_VERSION) throw new Exception("New version of localSettings.php required. Please update to format of v" . number_format (AmpersandApp::REQ_LOCALSETTINGS_VERSION, 1), 500);
 
 ?>
