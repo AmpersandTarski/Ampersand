81a6f78f0e973e9c8be7d94a683c2c9a61d33389
Michiel Stornebrink <michielstornebrink@gmail.com>
2017-12-21 23:26:29 +0100

    Add singleton pattern for AmpersandApp class

--- static/zwolle/src/Ampersand/AmpersandApp.php
+++ static/zwolle/src/Ampersand/AmpersandApp.php
@@ -15,9 +15,30 @@ class AmpersandApp
      * @var \Psr\Log\LoggerInterface
      */
     protected $logger;
+    /**
+     * Contains reference to ampersand application (singleton pattern)
+     * @var AmpersandApp
+     */
+    private static $_instance = null;
     
-    public function __construct(){
+    private function __construct(){
         $this->logger = Logger::getLogger('APPLICATION');
-        
+    }
+
+    /**
+     * Use Database::singleton() instead
+     * Singleton pattern: private function to prevent any copy/clone of database instance
+     */
+    private function __clone(){}
+    
+    /**
+     * Function to return the ampersand application instance
+     * Singleton pattern: use this static function to get the single instance of this class
+     * @return AmpersandApp
+     */
+    public static function singleton(){
+        if(!is_object (self::$_instance)) self::$_instance = new AmpersandApp();
+        else return self::$_instance;
+    }
     }
 }
