6ffa9dec2b29b333eefbf73718bebf7208ee71af
Michiel Stornebrink <michielstornebrink@gmail.com>
2017-12-25 00:28:18 +0100

    Move ExecEngine functions

--- static/zwolle/extensions/ExecEngine/ExecEngine.php
+++ static/zwolle/extensions/ExecEngine/ExecEngine.php
@@ -7,11 +7,6 @@ use Ampersand\AngularApp;
 use Ampersand\Config;
 use Ampersand\Role;
 
-// Load the ExecEngine functions
-$files = \Ampersand\Helper\getDirectoryList(__DIR__ . '/functions');
-foreach ($files as $file){
-    if (substr($file,-3) !== 'php') continue;
-    require_once $path = __DIR__ . '/functions/' . $file;
-}
+
 
 ?>
