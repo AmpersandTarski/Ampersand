55416b3001fc01e9c27c9563dc9386b0ff61a26c
Michiel Stornebrink <michielstornebrink@gmail.com>
2017-12-25 00:01:31 +0100

    Integrate ExecEngine run inside function that closes transaction. Not using Hooks anymore

--- static/zwolle/extensions/ExecEngine/ExecEngine.php
+++ static/zwolle/extensions/ExecEngine/ExecEngine.php
@@ -7,10 +7,6 @@ use Ampersand\AngularApp;
 use Ampersand\Config;
 use Ampersand\Role;
 
-// Define hooks
-$hook1 = array('class' => '\Ampersand\Rule\ExecEngine', 'function' => 'run', 'filename' => 'ExecEngine.php', 'filepath' => 'src/Ampersand/Rule', 'params' => array());
-Hooks::addHook('preCloseTransaction', $hook1);
-
 // Load the ExecEngine functions
 $files = \Ampersand\Helper\getDirectoryList(__DIR__ . '/functions');
 foreach ($files as $file){
