9aa0a53648510811025cf198a6b923d4bce94b01
Michiel Stornebrink <michielstornebrink@gmail.com>
2018-01-07 20:40:31 +0100

    Bugfix <switch> in navigation bar

--- static/zwolle/app/lib/angular/angular-ui-switch/angular-ui-switch-adapted.js
+++ static/zwolle/app/lib/angular/angular-ui-switch/angular-ui-switch-adapted.js
@@ -8,18 +8,13 @@ angular.module('uiSwitch', [])
   , template: function(element, attrs) {
       var html = '';
       html += '<a href=""';
-      html +=   attrs.ngModel ? ' ng-click="' + attrs.ngModel + '=!' + attrs.ngModel + '"' : '';
+      html +=   (attrs.ngModel && !attrs.ngClick) ? ' ng-click="' + attrs.ngModel + '=!' + attrs.ngModel + '"' : '';
       html += '>';
       html += '<span';
       html +=   ' class="switch' + (attrs.class ? ' ' + attrs.class : '') + '"';
       html +=   ' ng-class="{ checked:' + attrs.ngModel + ' }"';
       html +=   '>';
       html +=   '<small></small>';
-      html +=   '<input type="checkbox"';
-      html +=     attrs.id ? ' id="' + attrs.id + '"' : '';
-      html +=     attrs.name ? ' name="' + attrs.name + '"' : '';
-      html +=     attrs.ngModel ? ' ng-model="' + attrs.ngModel + '"' : '';
-      html +=     ' style="display:none" />';
       html += '</span>';
       html += '<span ng-transclude></span>';
       html += '</a>';
