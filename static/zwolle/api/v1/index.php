9df8bc71b581538b2dbdaa29d6c30872a94d8018
Michiel Stornebrink <michielstornebrink@gmail.com>
2018-01-23 19:51:11 +0100

    Update API entry script using Slim v3 instead of v2

--- static/zwolle/api/v1/index.php
+++ static/zwolle/api/v1/index.php
@@ -3,6 +3,10 @@
 use Ampersand\Misc\Config;
 use Ampersand\Log\Logger;
 use Ampersand\Log\Notifications;
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Container;
 
 require_once (__DIR__ . '/../../src/bootstrap.php');
 
@@ -11,63 +15,65 @@ require_once (__DIR__ . '/../../src/bootstrap.php');
  */
 global $container;
 
-// Code to add special http response codes that are not supported by Slim
-class NewResponse extends \Slim\Http\Response {
-    public static function addResponseCode($code, $message){
-        parent::$messages[$code] = "{$code} {$message}";
-    }
-}
-NewResponse::addResponseCode(440, "Login Timeout");
+$apiContainer = new Container();
 
-// Create and configure Slim app (version 2.x)
-$app = new \Slim\Slim(array(
-    'debug' => Config::get('debugMode')
-));
+// Custom NotFound handler
+$apiContainer['notFoundHandler'] = function($c){
+    return function(Request $request, Response $response) use ($c) {
+        return $c['response']
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'application/json')
+            ->write(json_encode(
+                [ 'error' => 404
+                , 'msg' => "API endpoint not found: {$request->getMethod()} {$request->getUri()}. Note! virtual path is case sensitive"
+                ]
+            ));
+    };
+};
 
-$app->add(new \Slim\Middleware\ContentTypes([
-    'application/json' => function($input){return json_decode($input, false);} // use json_decode without assoc option.
-    ])
-);
-$app->response->headers->set('Content-Type', 'application/json');
+$apiContainer['errorHandler'] = function ($c) use ($container) {
+    return function (Request $request, Response $response, Exception $e) use ($c, $container) {
+        try{
+            Logger::getLogger("API")->error($e->getMessage());
+            
+            switch ($e->getCode()) {
+                case 401: // Unauthorized
+                case 403: // Forbidden
+                    if(Config::get('loginEnabled') && !$container['ampersand_app']->getSession()->sessionUserLoggedIn()){
+                        $code = 401;
+                        $message = "Please login to access this page";
+                    }else{
+                        $code = 403;
+                        $message = "You do not have access to this page";
+                    }
+                    break;
+                default:
+                    $code = $e->getCode();
+                    $message = $e->getMessage();
+                    break;
+            }
+            
+            return $c['response']->withJson(['error' => $code, 'msg' => $message, 'notifications' => Notifications::getAll()], $code, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 
-// Error handler
-$app->error(function (Exception $e) use ($app, $container) {
-    try{
-        Logger::getLogger("API")->error($e->getMessage());
-        
-        switch ($e->getCode()) {
-            case 401: // Unauthorized
-            case 403: // Forbidden
-                if(Config::get('loginEnabled') && !$container['ampersand_app']->getSession()->sessionUserLoggedIn()){
-                    $code = 401;
-                    $message = "Please login to access this page";
-                }else{
-                    $code = 403;
-                    $message = "You do not have access to this page";
-                }
-                break;
-            default:
-                $code = $e->getCode();
-                $message = $e->getMessage();
-                break;
+        }catch(Exception $b){
+            Logger::getLogger("API")->error($b->getMessage());
+
+            return $c['response']->withJson(['error' => $b->getCode(), 'msg' => $b->getMessage(), 'notifications' => []], 500, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
         }
-        
-        $notifications = Notifications::getAll();
-        
-        $app->response->setStatus($code);
-        print json_encode(array('error' => $code, 'msg' => $message, 'notifications' => $notifications));
-    }catch(Exception $b){
-        $app->response->setStatus(500);
-        Logger::getLogger("API")->error($b->getMessage());
-        print json_encode(array('error' => $b->getCode(), 'msg' => $b->getMessage(), 'notifications' => array()));
-    }
-    
-});
+    };
+};
 
-// Not found handler
-$app->notFound(function () use ($app) {
-    $app->response->setStatus(404);
-    print json_encode(array('error' => 404, 'msg' => "API endpoint not found: {$app->request->getMethod()} {$app->request->getPathInfo()}. Note! virtual path is case sensitive"));
+// Settings
+$apiContainer->get('settings')->replace(['displayErrorDetails' => Config::get('debugMode')]);
+
+// Create and configure Slim app (version 3.x)
+$app = new App($apiContainer);
+
+// Add middleware to set default content type for response
+$app->add(function (Request $req,  Response $res, callable $next) {
+    $res = $res->withHeader('Content-Type', 'application/json;charset=utf-8');
+    $newResponse = $next($req, $res);
+    return $newResponse;
 });
 
 include (__DIR__ . '/resources.php'); // API calls starting with '/resource/'
