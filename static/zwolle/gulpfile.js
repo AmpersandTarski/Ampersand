28c408cdcb266150cbd2f65417e570628256c6af
Michiel Stornebrink <michielstornebrink@gmail.com>
2018-02-15 18:24:31 +0100

    Add Gulp as tool to build a dist of the front-end app

---
+++ static/zwolle/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+var sourcemaps = require('gulp-sourcemaps')
+var uglify = require('gulp-uglify-es').default
+var ngAnnotate = require('gulp-ng-annotate')
+
+gulp.task('js', function () {
+  gulp.src(['app/AmpersandApp.js', 'app/components/**/*.js', 'app/js/*.js'])
+    .pipe(sourcemaps.init())
+      .pipe(concat('app.js'))
+      .pipe(ngAnnotate())
+      .pipe(uglify())
+      .on('error', function(err){
+          console.error(err.toString());
+      })
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('app/dist'))
+})
+
+gulp.task('watch', ['js'], function () {
+  gulp.watch('app/**/*.js', ['js'])
+})
