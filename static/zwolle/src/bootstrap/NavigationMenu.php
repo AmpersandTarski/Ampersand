72652df12a27a09b51aa45aac7c453c8b0cafcee
Michiel Stornebrink <michielstornebrink@gmail.com>
2018-02-16 10:27:41 +0100

    Bugfix

--- static/zwolle/src/bootstrap/NavigationMenu.php
+++ static/zwolle/src/bootstrap/NavigationMenu.php
@@ -9,29 +9,29 @@ global $container;
 
 // Navigation menu settings
 $angularApp = $container['angular_app'];
-$angularApp->addMenuItem('refresh', 'app/components/admin/installer-menu-item.html', 
+$angularApp->addMenuItem('refresh', 'app/src/admin/installer-menu-item.html', 
     function($app){
         return !Config::get('productionEnv');
     });
 
-$angularApp->addMenuItem('refresh', 'app/components/admin/check-rules-menu-item.html',
+$angularApp->addMenuItem('refresh', 'app/src/admin/check-rules-menu-item.html',
     function($app){
         return !Config::get('productionEnv');
     });
 
-$angularApp->addMenuItem('refresh', 'app/components/admin/execengine-menu-item.html',
+$angularApp->addMenuItem('refresh', 'app/src/admin/execengine-menu-item.html',
     function(\Ampersand\AmpersandApp $app){
         $roles = Config::get('allowedRolesForRunFunction','execEngine');
         return $app->hasActiveRole($roles);
     });
 
-$angularApp->addMenuItem('ext', 'app/components/importer/menu-item.html', 
+$angularApp->addMenuItem('ext', 'app/src/importer/menu-item.html', 
     function(\Ampersand\AmpersandApp $app){
         $roles = Config::get('allowedRolesForImporter');
         return $app->hasActiveRole($roles);
     });
 
-$angularApp->addMenuItem('ext', 'app/components/admin/exporter-menu-item.html',
+$angularApp->addMenuItem('ext', 'app/src/admin/exporter-menu-item.html',
     function($app){
         return !Config::get('productionEnv');
     });
