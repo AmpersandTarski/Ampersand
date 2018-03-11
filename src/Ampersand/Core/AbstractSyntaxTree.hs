35ea6e81a76fd192a6eebecab0e4bbca16591d18
Han Joosten <han.joosten.han@gmail.com>
2018-03-11 12:18:04 +0100

    minor

--- src/Ampersand/Core/AbstractSyntaxTree.hs
+++ src/Ampersand/Core/AbstractSyntaxTree.hs
@@ -80,7 +80,7 @@ data A_Context
    = ACtx{ ctxnm :: String           -- ^ The name of this context
          , ctxpos :: [Origin]        -- ^ The origin of the context. A context can be a merge of a file including other files c.q. a list of Origin.
          , ctxlang :: Lang           -- ^ The default language used in this context.
-         , ctxmarkup :: PandocFormat -- ^ The default markup format for free text in this context (currently: LaTeX, ...)
+         , ctxmarkup :: PandocFormat -- ^ The default markup format for free text in this context.
          , ctxpats :: [Pattern]      -- ^ The patterns defined in this context
          , ctxrs :: Rules           -- ^ All user defined rules in this context, but outside patterns and outside processes
          , ctxds :: Relations        -- ^ The relations that are declared in this context, outside the scope of patterns
