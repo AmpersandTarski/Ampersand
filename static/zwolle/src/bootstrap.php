f5e0577ade57a2211669b523a93b3950a9256626
Michiel Stornebrink <michielstornebrink@gmail.com>
2018-03-16 09:14:22 +0100

    Remove shutdown function

--- static/zwolle/src/bootstrap.php
+++ static/zwolle/src/bootstrap.php
@@ -5,19 +5,6 @@ use Pimple\Container;
 use Ampersand\AngularApp;
 use Ampersand\Log\Logger;
 
-register_shutdown_function(function () {
-    $error = error_get_last();
-    if ($error['type'] & (E_ERROR | E_PARSE)) {
-        $protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
-        http_response_code(500);
-        header("{$protocol}  500 {$error['message']}");
-        print json_encode(['error' => 500
-                           , 'msg' => "{$error['message']} in {$error['file']}:{$error['line']}"
-                           ]);
-        exit;
-    }
-});
-
 // Check PHP version
 if (version_compare(PHP_VERSION, '7.0.0', '<')) {
     throw new Exception("PHP version >= 7.0 required. You are on " . PHP_VERSION, 500);
