c8a938c39bd75b3eabb988fe7fbe4d8ae042d635
Michiel Stornebrink <>
2018-05-06 15:08:36 +0200

    Move cgBusy config to module.js file

--- static/zwolle/app/src/module.js
+++ static/zwolle/app/src/module.js
@@ -87,4 +87,11 @@ angular.module('AmpersandApp', ['ngResource', 'ngRoute', 'ngSanitize', 'restangu
         }
         return original.apply($location, [url]);
     };
+}).value('cgBusyDefaults',{
+    message:'Loading...',
+    backdrop: true,
+    //templateUrl: 'my_custom_template.html',
+    //delay: 500, // in ms
+    minDuration: 500, // in ms
+    // wrapperClass: 'my-class my-class2'
 });
