cd64cd3c0c539ec8b0feed9456176b4adb952df2
Michiel Stornebrink <>
2018-06-18 21:15:53 +0200

    Remove everything in static/zwolle folder

--- static/zwolle/app/src/shared/myNavTo/myNavToOtherInterfaces.directive.js
+++
@@ -1,8 +0,0 @@
-angular.module('AmpersandApp')
-.directive('myNavToOtherInterfaces', function(){
-    return {
-        restrict : 'E',
-        scope  : {resource : '=', target : '@'}, // '=' => two-way bind, '@' => evaluates string (use {{}} in html) 
-        templateUrl : 'app/src/shared/myNavTo/myNavToOtherInterfaces.html'
-    };
-});
