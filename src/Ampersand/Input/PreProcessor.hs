825bcc09f0fbbc760231a964472c6b1feadce1a9
Bart Marinissen <bart.marinissen@tno.nl>
2018-09-20 16:29:22 +0200

    Change preprocessor to use Parsec.
    Technically, the lexer could also be replaced, but the current version
    seems fine. Perhaps if we want more flexibility in the commands, or
    want to retain comments that are after the guard in the output.
    
    The function turning a block back into a (partially commented)
    string could probably also be replaced by a stateful parser. Not at all
    necessary though.
    
    Finally, we should return something like Either ParseError String from
    preProcess so we can propagate parsing errors rather than crashing.

--- src/Ampersand/Input/PreProcessor.hs
+++ src/Ampersand/Input/PreProcessor.hs
@@ -7,85 +7,124 @@ import Data.List
 import Data.String
 import Data.Char
 import Data.Maybe
-import GHC.Base
+import Data.Bool
+import Data.Either
+import Data.Functor
+import Control.Monad hiding (guard)
+import Control.Applicative hiding ( many )
+import Text.Parsec hiding ( (<|>) )
+import Prelude
 
 type PreProcDefine = String
 
 preProcess :: [PreProcDefine] -> String -> String
-preProcess defs = block2file defs True . file2block
+preProcess defs = block2file defs True . (either (error . show) id) <$> file2block ""
  
--- Run the full parser
-file2block :: String -> Block
-file2block = parseBlock . map parseLine . lines
+-- Run the parser
+file2block :: String -> String -> Either ParseError Block
+file2block fileName = parseFile fileName . map lexLine . lines
 
--- Turn a block back
-block2file :: [PreProcDefine] -> Bool -> Block -> String
-block2file defs shown = concat . map (blockElem2string defs shown)
+-- LEXER
+data LexLine = Codeline String
+             | IfNotStart Guard
+             | IfStart Guard
+             | IfEnd
+instance Show LexLine where
+  show = showLex
 
--- Do we want to implement the relevant strings e.g. "--# IF" as constants?
+showLex :: LexLine -> String
+showLex (Codeline x)   = x
+showLex (IfNotStart x) = "If Not " ++ guard x
+showLex (IfStart x)    = "If "     ++ guard x
+showLex (IfEnd)        = "End If"
 
--- "Implement" the grammar in Haskell Types
+parseIfStartLine :: String -> Maybe LexLine
+parseIfStartLine x = fmap    (IfStart . Guard . head . words)    (stripPrefix "--#IF "    $ dropWhile isSpace x)
+
+parseNotIfStartLine :: String -> Maybe LexLine
+parseNotIfStartLine x = fmap (IfNotStart . Guard . head . words) (stripPrefix "--#IFNOT " $ dropWhile isSpace  x)
+
+parseIfEndLine :: String -> Maybe LexLine
+parseIfEndLine x = fmap (const IfEnd) (stripPrefix "--#ENDIF" $ dropWhile isSpace x)
+
+lexLine :: String -> LexLine
+lexLine line = fromMaybe (Codeline line) $ (parseIfStartLine line
+                                          <|> parseNotIfStartLine line
+                                          <|> parseIfEndLine line)
+
+-- PARSER
 newtype Guard = Guard String
+guard :: Guard -> String
+guard (Guard x) = x
 
 data BlockElem = LineElem String
                | IfElem IfBlock
-               | IfNotElem NotIfBlock
+               | IfNotElem IfNotBlock
 
 type Block   = [ BlockElem ]
 
 data IfBlock    = IfBlock    Guard Block
-data NotIfBlock = IfNotBlock Guard Block
+data IfNotBlock = IfNotBlock Guard Block
 
-data Line = Codeline String
-          | IfNotStart Guard
-          | IfStart Guard
-          | IfEnd
- 
--- First, define a function that reads our primitive 'line'
+type Parser a = Parsec [LexLine] () a
 
--- We now do (f x) <|> (g x), would be nicer to have (f <|> g) x
--- would require <|> :: (a -> Maybe b) -> (a -> Maybe b) -> (a -> Maybe b)
--- Monad >=> comes close but not quite
-parseLine :: String -> Line
-parseLine line = fromMaybe (Codeline line) $ (parseIfStartLine line
-                                          <|> parseNotIfStartLine line
-                                          <|> parseIfEndLine line)
+myToken :: (LexLine -> Maybe a) -> Parser a
+myToken constructor = tokenPrim showLex (\pos _ _ -> incSourceLine pos 1) constructor
 
---f :: (a -> Maybe b) -> (a -> Maybe b) -> (a -> Maybe b)
+codeLine :: Parser BlockElem
+codeLine = myToken ((fmap LineElem) <$> line2string)
+  where
+  line2string :: LexLine -> Maybe String
+  line2string (Codeline s) = Just s
+  line2string _          = Nothing
 
-parseIfStartLine :: String -> Maybe Line
-parseIfStartLine x = fmap    (IfStart . Guard . head . words)    (stripPrefix "--#IF "    $ dropWhile isSpace x)
+ifStart :: Parser Guard
+ifStart = myToken guard2string
+  where
+  guard2string (IfStart g) = Just g
+  guard2string _           = Nothing
 
-parseNotIfStartLine :: String -> Maybe Line
-parseNotIfStartLine x = fmap (IfNotStart . Guard . head . words) (stripPrefix "--#IFNOT " $ dropWhile isSpace  x)
+ifNotStart :: Parser Guard
+ifNotStart = myToken guard2string
+  where
+  guard2string (IfNotStart g) = Just g
+  guard2string _              = Nothing
 
-parseIfEndLine :: String -> Maybe Line
-parseIfEndLine x = fmap (const IfEnd) (stripPrefix "--#ENDIF" $ dropWhile isSpace x)
+ifEnd :: Parser ()
+ifEnd = myToken (matchIfEnd)
+  where
+  matchIfEnd IfEnd = Just ()
+  matchIfEnd _     = Nothing
 
--- Next, define a function that processes our data type [Line]
+ifBlock :: Parser IfBlock
+ifBlock = do
+  guard' <- ifStart;
+  lines' <- many blockElem
+  _     <- ifEnd
+  return (IfBlock guard' lines')
 
--- Use blockParser to turn a list of lines into a list of BlockElem
--- Note that blockParser could consume more than one line
--- Fold after build?
-parseBlock :: [Line] -> Block
-parseBlock [] = []
-parseBlock lineList = (\(x, y) -> y ++ parseBlock x) . blockParser $ lineList
+ifNotBlock :: Parser IfNotBlock
+ifNotBlock = do
+  guard' <- ifNotStart;
+  lines' <- block
+  _     <- ifEnd
+  return (IfNotBlock guard' lines')
 
-blockParser :: [Line] -> ([Line], [BlockElem] )
-blockParser [] = ([], [])
-blockParser (line:rest) = case line of
-    Codeline plainLine -> (rest, [LineElem plainLine])
-    IfStart guard      -> let (blockLines, remainingLines) = break endsIfBlock rest in
-                              ( remainingLines, [IfElem    $ IfBlock    guard (parseBlock blockLines)] )
-    IfNotStart guard   -> let (blockLines, remainingLines) = break endsIfBlock rest in
-                              ( remainingLines, [IfNotElem $ IfNotBlock guard (parseBlock blockLines)] )
-    IfEnd              -> (rest, [] )
+blockElem :: Parser BlockElem
+blockElem = choice [codeLine, IfElem <$> ifBlock, IfNotElem <$> ifNotBlock ] <?> "a block element"
 
-endsIfBlock :: Line -> Bool
-endsIfBlock IfEnd = True
-endsIfBlock _     = False
+block :: Parser Block
+block = many blockElem
 
+parseFile :: String -> [LexLine] -> (Either ParseError Block)
+parseFile fileName = parse (block <* eof) fileName
 
+-- Turn Blocks Back into text
+
+-- Turn a block back
+-- Could be done with a stateful monad where
+block2file :: [PreProcDefine] -> Bool -> Block -> String
+block2file defs shown = concat . map (blockElem2string defs shown)
 
 -- Handle single entry in a block
 -- Responsible for adding newlines
@@ -94,11 +133,11 @@ blockElem2string :: [PreProcDefine] -> Bool ->                BlockElem -> Strin
 blockElem2string _    True  (LineElem line) = line ++ "\n"
 blockElem2string _    False (LineElem line) = "--hiden by preprocc " ++ line ++ "\n"
 -- Lots of unpacking to get to the IfBlock
-blockElem2string defs hiding  (IfElem    (IfBlock    (Guard guard) block)) =
-    "--#IF " ++ guard ++ "\n" ++
-    (block2file defs (hiding &&     (guard `elem` defs)) block) ++
+blockElem2string defs showing  (IfElem    (IfBlock    (Guard guard') block')) =
+    "--#IF " ++ guard' ++ "\n" ++
+    (block2file defs (showing &&     (guard' `elem` defs)) block') ++
     "--#ENDIF\n"
-blockElem2string defs hiding  (IfNotElem (IfNotBlock (Guard guard) block)) =
-    "--#IFNOT " ++ guard ++ "\n" ++
-    (block2file defs (hiding && not (guard `elem` defs)) block) ++
+blockElem2string defs showing  (IfNotElem (IfNotBlock (Guard guard') block')) =
+    "--#IFNOT " ++ guard' ++ "\n" ++
+    (block2file defs (showing && not (guard' `elem` defs)) block') ++
     "--#ENDIF\n"
