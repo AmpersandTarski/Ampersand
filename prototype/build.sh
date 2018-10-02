9699d137f2862be8aee2c2717804b87451d6ffff
Stef Joosten <stef.joosten@ou.nl>
2018-10-02 19:12:30 +0200

    put docker files in Ampersand's root to enable automated baking of images
    On docker hub (https://hub.docker.com/u/ampersandtarski/dashboard/) we
    want to include webhooks to trigger baking of images with every release
    of ampersand.

---
+++ prototype/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+#
+# Build docker images for:
+#   1. a container with just the ampersand compiler;
+#   2. a container with latex, graphviz and other modules for running RAP;
+#   3. a container with MariaDB
+# Run this build script from the repository's root:
+#   ampersandadmin@hostname:~$  cd RAP 
+#   ampersandadmin@hostname:~$  ./prototype/build.sh 
+
+set -e
+
+echo "Building ampersandtarski/ampersand-prototype:latest"
+docker build --tag ampersandtarski/ampersand-prototype:latest prototype
+
+echo "Building ampersandtarski/ampersand-prototype:texlive"
+docker build --tag ampersandtarski/ampersand-prototype:texlive prototype/texlive
+
+echo "Building ampersandtarski/ampersand-prototype-db"
+docker build --tag ampersandtarski/ampersand-prototype-db:latest prototype/prototype-db
---
+++ prototype/texlive/texlive.profile
@@ -0,0 +1,11 @@
+TEXDIR /texlive
+TEXMFSYSVAR /texlive/texmf-var
+TEXMFSYSCONFIG /texlive/texmf-config
+TEXMFLOCAL /texlive/texmf-local
+TEXMFHOME $TEXMFLOCAL
+TEXMFVAR $TEXMFSYSVAR
+TEXMFCONFIG $TEXMFSYSCONFIG
+instopt_portable 1
+
+tlpdbopt_install_docfiles 0
+tlpdbopt_install_srcfiles 0
