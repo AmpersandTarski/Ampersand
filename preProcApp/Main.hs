2487783f05952698382cb3017b88046ef95622ad
bart <bart.marinissen@tno.nl>
2018-10-03 05:30:29 -0700

    Fix output of manual preprocessor app.

--- preProcApp/Main.hs
+++ preProcApp/Main.hs
@@ -11,4 +11,4 @@ main =
     filename:defs <- getArgs;
     input       <- readUTF8File filename
     inputString <- return $ either id id input
-    putStr $ show (preProcess filename defs inputString) ++ "\n"
+    putStr $ either show id (preProcess' filename defs inputString) ++ "\n"
