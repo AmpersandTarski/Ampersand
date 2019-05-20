d32393fd91d0ab053aef83f8521a7db1d2122031
hanjoosten <han.joosten.han@gmail.com>
2019-05-20 20:33:29 +0200

    Temporary bugfix for issue #964

--- src/Ampersand/ADL1/Expression.hs
+++ src/Ampersand/ADL1/Expression.hs
@@ -229,7 +229,9 @@ exprPrd2list (EPrd (l,r)) = exprPrd2list l <> exprPrd2list r
 exprPrd2list r            = r NEL.:| []
 
 insParentheses :: Expression -> Expression
-insParentheses = insPar 0
+insParentheses = 
+  id -- TODO: Needs a fix. isParantheses is broken. It causes a fatal. See issue #964
+     -- insPar 0
    where
      wrap :: Integer -> Integer -> Expression -> Expression
      wrap i j e' = if i<=j then e' else EBrk (insPar 0 e')
