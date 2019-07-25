db3f428c2d47a3a15bfa48da1e7c81d18684ed37
hanjoosten <han.joosten.han@gmail.com>
2019-07-25 14:36:17 +0200

    wip

---
+++ src/Ampersand/Options/ProtoParser.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module Ampersand.Options.ProtoParser where
+
+import           Options.Applicative
+import           Ampersand.Commands.Proto (ProtoCommand(..), ProtoOpts (..))
+import           Ampersand.Basics
+import           Ampersand.Options.Utils
+
+-- | Command-line parser for the proto command.
+protoOptsParser :: ProtoCommand -> String -> Parser ProtoOpts
+protoOptsParser Proto defDBName = 
+    ProtoOpts <$> dbName <*> host <*> login 
+              <*> password <*> forceReinstall <*> outputLanguage
+              <*> rootFile 
+  where
+    dbName = strOption
+        ( long "dbName"
+        <> short 'd'
+        <> metavar "DATABASENAME"
+        <> value defDBName
+        <> showDefault
+        <> help "Name of the schema of the database that is generated as part of the prototype." )
+    host = strOption
+        ( long "sqlHost"
+        <> metavar "HOSTNAME"
+        <> value "localhost"
+        <> showDefault
+        <> help "Name of the host of the database." )
+    login = strOption
+        ( long "sqlLogin"
+        <> metavar "USER"
+        <> value "ampersand"
+        <> showDefault
+        <> help "Name of the database user. (Defaults to `ampersand`)" )
+    password = strOption
+        ( long "sqlPwd"
+        <> metavar "PASSWORD"
+        <> value "ampersand"
+        <> showDefault
+        <> help "Password for the database user. (Defaults to `ampersand`)" )
+    forceReinstall = switch
+        ( long "force-reinstall-framework"
+        <> help ("Re-install the prototype framework. This discards any previously "<>
+                "installed version.")
+        )
+
