35c453693e06afc05639f55ff1c0552dae797d74
Han Joosten <han.joosten.han@gmail.com>
2020-05-31 22:03:50 +0200

    fix #988

--- src/Ampersand/Options/DaemonParser.hs
+++ src/Ampersand/Options/DaemonParser.hs
@@ -6,15 +6,18 @@ import           Ampersand.Misc.HasClasses
 import           Ampersand.Basics
 import           Ampersand.Options.Utils
 import           Ampersand.Options.FSpecGenOptsParser
+import           Options.Applicative.Builder.Extra (boolFlags)
 
 -- | Command-line parser for the daemon command.
 daemonOptsParser :: Parser DaemonOpts
 daemonOptsParser = 
-   ( \outputLanguage daemonConfig fSpecGenOpts -> DaemonOpts
+   ( \outputLanguage daemonConfig fSpecGenOpts 
+      showWarnings-> DaemonOpts
             { x2OutputLanguage = outputLanguage
             , xdaemonConfig = daemonConfig
             , x2fSpecGenOpts = fSpecGenOpts
-            }) 
+            , xshowWarnings = showWarnings
+          }) 
   <$> outputLanguageP
   <*> strOption
         ( long "daemonconfig"
@@ -24,5 +27,8 @@ daemonOptsParser =
         <> help ("The config file contains the list of files to be monitored.")
         )
   <*> fSpecGenOptsParser True
+  <*> boolFlags True "warnings"
+        ( "show warnings in the output, if any. "
+        ) mempty
 
 
