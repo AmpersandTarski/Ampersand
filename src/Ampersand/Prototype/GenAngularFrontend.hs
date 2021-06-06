dceb2e6d0f564932e257391e6b21941431dfca64
hanjoosten <han.joosten.han@gmail.com>
2021-06-06 10:40:50 +0200

    remove warnings

--- src/Ampersand/Prototype/GenAngularFrontend.hs
+++ src/Ampersand/Prototype/GenAngularFrontend.hs
@@ -1,35 +1,27 @@
-{-# LANGUAGE OverloadedStrings #-}
+-- {-# LANGUAGE OverloadedStrings #-}
 module Ampersand.Prototype.GenAngularFrontend (genComponents,genAngularModule) 
 
 where
 
-import           Ampersand.ADL1
+-- import           Ampersand.ADL1
 import           Ampersand.Basics
-import           Ampersand.Classes.Relational
-import           Ampersand.Core.ShowAStruct
+-- import           Ampersand.Classes.Relational
+-- import           Ampersand.Core.ShowAStruct
 import           Ampersand.FSpec.FSpec
-import           Ampersand.FSpec.ToFSpec.NormalForms
-import           Ampersand.Misc.HasClasses
-import           Ampersand.Output.FSpec2SQL (databaseStructureSql)
-import           Ampersand.Output.ToJSON.ToJson
+-- import           Ampersand.Misc.HasClasses
 import           Ampersand.Prototype.ProtoUtil
-import           Ampersand.Runners (logLevel)
-import           Ampersand.Types.Config
-import           Codec.Archive.Zip
-import           Data.Hashable (hash)
-import           Network.HTTP.Simple
-import qualified RIO.ByteString.Lazy  as BL
-import qualified RIO.Text as T
-import qualified RIO.List as L
-import           RIO.Time
-import           System.Directory
-import           System.FilePath
-import           Text.StringTemplate(Stringable, StringTemplate, setAttribute, newSTMP, checkTemplateDeep, render)
+-- import           Ampersand.Runners (logLevel)
+-- import           Ampersand.Types.Config
+-- import qualified RIO.Text as T
+-- import qualified RIO.List as L
+-- import           System.Directory
+-- import           System.FilePath
+-- import           Text.StringTemplate(Stringable, StringTemplate, setAttribute, newSTMP, checkTemplateDeep, render)
 import           Text.StringTemplate.GenericStandard () -- only import instances
 
 
-genComponents :: (HasRunner env, HasDirPrototype env) => FSpec -> [FEInterface] -> RIO env ()
+genComponents :: FSpec -> [FEInterface] -> RIO env ()
 genComponents = undefined
 
-genAngularModule :: (HasRunner env, HasDirPrototype env) => FSpec -> [FEInterface] -> RIO env ()
+genAngularModule :: FSpec -> [FEInterface] -> RIO env ()
 genAngularModule = undefined
