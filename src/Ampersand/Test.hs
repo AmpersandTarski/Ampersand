1e307e1c4e5af2d5b5bdb640ffbbd88e9064ec1a
hanjoosten <han.joosten.han@gmail.com>
2021-06-12 17:10:11 +0000

    better formatted error message

--- src/Ampersand/Test.hs
+++ src/Ampersand/Test.hs
@@ -1,11 +1,14 @@
 module Ampersand.Test
-   ( module Ampersand.Test.Regression
-   , module Ampersand.Test.TestScripts
-   , module Ampersand.Test.Parser.ParserTest
-   , module Ampersand.Test.Parser.QuickChecks
-   ) 
+  ( module Ampersand.Test.Regression,
+    module Ampersand.Test.TestScripts,
+    module Ampersand.Test.Parser.ParserTest,
+    module Ampersand.Test.Parser.QuickChecks,
+  )
 where
-import Ampersand.Test.Regression
-import Ampersand.Test.TestScripts        (getTestScripts, testAmpersandScripts)
+
 import Ampersand.Test.Parser.ParserTest
-import Ampersand.Test.Parser.QuickChecks (parserQuickChecks)
+  ( parseScripts,
+    showErrors,
+  )
+import Ampersand.Test.Parser.QuickChecks (doAllQuickCheckPropertyTests)import Ampersand.Test.Regression (regressionTest)
+import Ampersand.Test.TestScripts (getTestScripts, testAmpersandScripts)
