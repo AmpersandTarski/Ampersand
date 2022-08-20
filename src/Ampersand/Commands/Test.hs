567f23fa7b87da474bec4277d14295f94a937be3
hanjoosten <han.joosten.han@gmail.com>
2022-08-20 07:13:59 +0000

    redo accidentally commit of disablement of testing

--- src/Ampersand/Commands/Test.hs
+++ src/Ampersand/Commands/Test.hs
@@ -17,8 +17,7 @@ import Ampersand.Types.Config (HasRunner)
 test :: (HasTestOpts env, HasRunner env) => RIO env ()
 test = do
   parserRoundtripTest
-
---  regressionTest
+  regressionTest
 
 parserRoundtripTest :: (HasRunner env) => RIO env ()
 parserRoundtripTest = do
