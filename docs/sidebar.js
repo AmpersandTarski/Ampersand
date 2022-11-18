9592c79f0d04ce351f995a064bf863815bee65da
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-11-18 17:02:48 +0100

    Add sidebar config that will be picked up by Docusaurus

---
+++ docs/sidebar.js
@@ -0,0 +1,10 @@
+// This file will be picked up by the Docusaurus build step to construct the complete sidebar
+module.exports = {
+  sidebar: [
+    { // Normal syntax:
+      type: 'doc',
+      id: 'Ampersand/Test', // document ID
+      label: 'Test', // sidebar label
+    },
+  ],
+};
