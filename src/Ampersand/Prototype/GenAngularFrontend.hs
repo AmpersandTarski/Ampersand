92ec9a7dbb2f6804f3b6ce56e6a4167dc5f3523a
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-12-31 15:56:04 +0100

    Add function to generate .component.ts file for each interface

--- src/Ampersand/Prototype/GenAngularFrontend.hs
+++ src/Ampersand/Prototype/GenAngularFrontend.hs
@@ -18,8 +18,41 @@ genComponents :: (HasLogFunc env) => FSpec -> [FEInterface] -> RIO env ()
 genComponents fSpec = mapM_ (genComponent fSpec)
 
 genComponent :: (HasLogFunc env) => FSpec -> FEInterface -> RIO env ()
-genComponent _ ifc = do
-  logError . display $ "Still TODO: Generate component for " <> ifcName ifc
+genComponent fspec ifc = do
+  getComponentTs fspec ifc
+  logError . display $ "Still TODO: Generate html file for " <> ifcNamePascalComponent ifc
+
+getComponentTs :: (HasLogFunc env) => FSpec -> FEInterface -> RIO env ()
+getComponentTs fspec interf = do
+  let controlerTemplateName = "interface.component.ts.txt"
+  template <- readTemplate controlerTemplateName
+  mapM_ (logDebug . display) (showTemplate template)
+  runner <- view runnerL
+  let loglevel' = logLevel runner
+  let contents =
+        renderTemplate Nothing template $
+          setAttribute "contextName" (fsName fSpec)
+            . setAttribute "isRoot" (isTopLevel . source . ifcExp $ interf)
+            . setAttribute "roles" (map show . feiRoles $ interf) -- show string, since StringTemplate does not elegantly allow to quote and separate
+            . setAttribute "ampersandVersionStr" (longVersion appVersion)
+            . setAttribute "ifcName" (ifcName interf)
+            . setAttribute "ifcNamePascal" (ifcNamePascal interf)
+            . setAttribute "ifcNameKebab" (ifcNameKebab interf)
+            . setAttribute "ifcLabel" (ifcLabel interf) -- no escaping for labels in templates needed
+            . setAttribute "expAdl" (showA . toExpr . ifcExp $ interf)
+            . setAttribute "exprIsUni" (exprIsUni (feiObj interf))
+            . setAttribute "source" (idWithoutType . source . ifcExp $ interf)
+            . setAttribute "target" (idWithoutType . target . ifcExp $ interf)
+            . setAttribute "crudC" (objCrudC (feiObj interf))
+            . setAttribute "crudR" (objCrudR (feiObj interf))
+            . setAttribute "crudU" (objCrudU (feiObj interf))
+            . setAttribute "crudD" (objCrudD (feiObj interf))
+            . setAttribute "verbose" (loglevel' == LevelDebug)
+            . setAttribute "loglevel" (show loglevel')
+            . setAttribute "usedTemplate" controlerTemplateName
+  let filename = T.unpack (ifcNameKebab interf) <> ".component.ts"
+  writePrototypeAppFile filename contents
+  logDebug . display $ "Generated file " <> filename
 
 genAngularModule :: (HasRunner env, HasDirPrototype env) => FSpec -> [FEInterface] -> RIO env ()
 genAngularModule fSpec ifcs = do
