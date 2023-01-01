b1c084c4bd6adbc5962f36e3f7495cb72984139a
Michiel Stornebrink <michielstornebrink@gmail.com>
2023-01-01 13:01:13 +0100

    Add implementation of component view (html file) generator

--- src/Ampersand/Prototype/GenAngularFrontend.hs
+++ src/Ampersand/Prototype/GenAngularFrontend.hs
@@ -104,5 +104,106 @@ genAngularModule fSpec ifcs = do
   writePrototypeAppFile "project.module.ts" contents
   logDebug . display $ "Generated file project.module.ts"
 
-
 genViewObject :: (HasRunner env, HasDirPrototype env) => FSpec -> Int -> FEObject -> RIO env [Text]
+genViewObject fSpec depth obj =
+  case obj of
+    FEObjE {} -> do
+      runner <- view runnerL
+      let loglevel' = logLevel runner
+      let atomicAndBoxAttrs :: StringTemplate String -> StringTemplate String
+          atomicAndBoxAttrs =
+            setAttribute "exprIsUni" (exprIsUni obj)
+              . setAttribute "exprIsTot" (exprIsTot obj)
+              . setAttribute "name" (escapeIdentifier . objName $ obj)
+              . setAttribute "label" (objName obj) -- no escaping for labels in templates needed
+              . setAttribute "expAdl" (showA . toExpr . objExp $ obj)
+              . setAttribute "source" (idWithoutType . source . objExp $ obj)
+              . setAttribute "target" (idWithoutType . target . objExp $ obj)
+              . setAttribute "crudC" (objCrudC obj)
+              . setAttribute "crudR" (objCrudR obj)
+              . setAttribute "crudU" (objCrudU obj)
+              . setAttribute "crudD" (objCrudD obj)
+              . setAttribute "verbose" (loglevel' == LevelDebug)
+              . setAttribute "loglevel" (show loglevel')
+      case atomicOrBox obj of
+        FEAtomic {} -> do
+          {-
+              logDebug (getOpts fSpec) $ replicate depth ' ' <> "ATOMIC "<>show nm <>
+                                            " [" <> name src <> "*"<> name tgt <> "], " <>
+                                            (if isEditable then "" else "not ") <> "editable"
+          -}
+          -- For now, we choose specific template based on target concept. This will probably be too weak.
+          -- (we might want a single concept to could have multiple presentations, e.g. BOOL as checkbox or as string)
+          -- logInfo $ nm <> ":" <> show mPrimTemplate
+          conceptTemplate <- getTemplateForObject
+          let (templateFilename, _) = fromMaybe (conceptTemplate, []) (objMPrimTemplate . atomicOrBox $ obj) -- Atomic is the default template
+          template <- readTemplate templateFilename
+
+          return . indentation
+            . T.lines
+            . renderTemplate Nothing template
+            $ atomicAndBoxAttrs
+        FEBox
+          { boxHeader = header,
+            boxSubObjs = subObjs
+          } -> do
+            subObjAttrs <- mapM genView_SubObject subObjs
+
+            parentTemplate <- readTemplate $ "Box-" <> T.unpack (btType header) <.> "html"
+
+            return . indentation
+              . T.lines
+              . renderTemplate (Just . btKeys $ header) parentTemplate
+              $ atomicAndBoxAttrs
+                . setAttribute "isRoot" (depth == 0)
+                . setAttribute "subObjects" subObjAttrs
+    FEObjT {} -> pure []
+  where
+    indentation :: [Text] -> [Text]
+    indentation = map (T.replicate (if depth == 0 then 4 else 16) " " <>)
+    genView_SubObject ::
+      (HasRunner env, HasDirPrototype env) =>
+      FEObject ->
+      RIO env SubObjectAttr2
+    genView_SubObject subObj =
+      case subObj of
+        FEObjE {} ->
+          do
+            lns <- genViewObject fSpec (depth + 1) subObj
+            return
+              SubObjAttr
+                { subObjName = escapeIdentifier $ objName subObj,
+                  subObjLabel = objName subObj, -- no escaping for labels in templates needed
+                  subObjContents = T.intercalate "\n" lns,
+                  subObjExprIsUni = exprIsUni subObj
+                }
+        FEObjT {} ->
+          do
+            return
+              SubObjAttr
+                { subObjName = escapeIdentifier $ objName subObj,
+                  subObjLabel = objName subObj,
+                  subObjContents = objTxt subObj,
+                  subObjExprIsUni = True
+                }
+    getTemplateForObject ::
+      (HasDirPrototype env) =>
+      RIO env FilePath
+    getTemplateForObject
+      | relIsProp obj && (not . exprIsIdent) obj -- special 'checkbox-like' template for propery relations
+        =
+        return $ "View-PROPERTY" <> ".html"
+      | otherwise = getTemplateForConcept . target . objExp $ obj
+    getTemplateForConcept ::
+      (HasDirPrototype env) =>
+      A_Concept ->
+      RIO env FilePath
+    getTemplateForConcept cpt = do
+      exists <- doesTemplateExist cptfn
+      return $
+        if exists
+          then cptfn
+          else "Atomic-" <> show ttp <.> "html"
+      where
+        ttp = cptTType fSpec cpt
+        cptfn = "Concept-" <> T.unpack (name cpt) <.> "html"
