58fe9fba1cb91e1ac516d7ce5e9926cc42adab98
hanjoosten <han.joosten@ordina.nl>
2023-01-22 19:11:13 +0000

    remove prototype sidebars

--- docs/sidebar.js
+++ docs/sidebar.js
@@ -164,29 +164,6 @@ module.exports = {
     )
     ),
 
-
-
-    prototypeGuideSidebar: [
-        {
-            label: 'How to create a prototype',
-            type: 'category',
-            items: [
-                'prototype/README',
-                'prototype/config',
-                'prototype/file-system',
-                'prototype/event-dispatcher',
-                'prototype/interface-templates',
-                'prototype/generics',
-                'prototype/ampersand-compiler',
-            ]
-        },
-    ],
-    prototypeReferenceSidebar: [
-
-    ],
-    // prototypeMainSidebar: [
-
-    // ],
     toolsWeUseMainSidebar: [
         {
             label: 'Tools for contributors',
