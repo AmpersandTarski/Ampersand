4d2a4b1f8b6d90a3182f06c9479bc8185724ab92
Han Joosten <han.joosten.han@gmail.com>
2025-06-07 00:13:44 +0200

    Concepts with definitions

--- src/Ampersand/Input/SemWeb/Turtle.hs
+++ src/Ampersand/Input/SemWeb/Turtle.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
 module Ampersand.Input.SemWeb.Turtle
   ( parseTurtleFile,
   )
@@ -5,8 +7,6 @@ where
 
 import Ampersand.Basics
 import Ampersand.Core.ParseTree
-  ( P_Context (..),
-  )
 import Ampersand.Input.ADL1.CtxError
 import Data.RDF
 import RIO.Directory (doesFileExist)
@@ -19,7 +19,17 @@ readTurtle filePath = do
   exists <- liftIO $ doesFileExist filePath
   if exists
     then do
-      let defBaseUrl = Nothing
+      raw <- readUTF8File filePath
+
+      let defBaseUrl =
+            ( case raw of
+                Left _ -> Nothing
+                Right content -> case filter isBaseLine . T.lines $ content of
+                  [baseline] -> case take 1 . reverse . take 2 . T.words $ baseline of
+                    [x] -> Just (BaseUrl x)
+                    _ -> Nothing
+                  _ -> Nothing
+            )
           defMappings = Nothing
           parser = TurtleParser defBaseUrl defMappings
       result <- liftIO $ parseFile parser filePath
@@ -33,6 +43,9 @@ readTurtle filePath = do
           [ "While looking for " <> T.pack filePath,
             "   File does not exist."
           ]
+  where
+    isBaseLine :: Text -> Bool
+    isBaseLine = T.isPrefixOf "@base"
 
 -- | Parse a Turtle file and convert it into a 'P_Context'.
 parseTurtleFile :: FilePath -> RIO env (Guarded P_Context)
@@ -76,18 +89,69 @@ getContext filePath guardedGraph = do
         ctx_gs = mempty,
         ctx_enfs = mempty,
         ctx_ds = mempty,
-        ctx_cs = mempty
+        ctx_cs = conceptDefs (CONTEXT nm) graph
       }
   where
     mkError :: Text -> Guarded a
     mkError = mkTurtleParseError filePath
-    rdfType :: Node
-    rdfType = unode "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
-
-    owlOntology :: Node
-    owlOntology = unode "http://www.w3.org/2002/07/owl#Ontology"
+    orig = Origin $ "Somewhere in " <> T.pack filePath
     ontologyName :: RDF TList -> Guarded Name
-    ontologyName graph = case query graph Nothing (Just rdfType) (Just owlOntology) of
+    ontologyName graph = case select graph Nothing rdfType owlOntology of
       [] -> mkError "No ontology triple found in Turtle file"
       [Triple (UNode s) _ _] -> pure . fst . suggestName ContextName . toText1Unsafe $ s
       _ -> mkError "Multiple ontology triples found in Turtle file"
+    conceptDefs :: DefinitionContainer -> RDF TList -> [PConceptDef]
+    conceptDefs frm graph =
+      [ PConceptDef
+          { cdname = nm,
+            cdmean = mempty,
+            cdlbl = l,
+            cdfrom = frm,
+            cddef2 = PCDDefLegacy def2 "",
+            pos = orig
+          }
+        | cpt <- map subjectOf $ select graph Nothing rdfType owlClass,
+          cpt
+            `elem` map subjectOf (select graph (is cpt) rdfType owlNamedIndividual),
+          lbl <- mapMaybe (getLiteralText . objectOf) $ select graph (is cpt) rdfsLabel Nothing,
+          let (nm, l) = suggestName ContextName . toText1Unsafe $ lbl,
+          def2 <- mapMaybe (getLiteralText . objectOf) $ select graph (is cpt) skosDefinition Nothing
+      ]
+
+getLiteralText :: Node -> Maybe Text
+getLiteralText n = case n of
+  LNode (PlainL txt) -> Just txt
+  LNode (PlainLL txt _) -> Just txt
+  LNode (TypedL txt _) -> Just txt
+  _ -> Nothing
+
+-- getExactlyOneMatchingTriple :: RDF TList -> Maybe Node -> Node -> Node -> Guarded Triple
+
+selector :: Text -> NodeSelector
+selector txt = Just fun
+  where
+    fun x = x == unode txt
+
+is :: Node -> NodeSelector
+is n = Just (== n)
+
+owlClass :: NodeSelector
+owlClass = selector "http://www.w3.org/2002/07/owl#Class"
+
+owlNamedIndividual :: NodeSelector
+owlNamedIndividual = selector "http://www.w3.org/2002/07/owl#NamedIndividual"
+
+owlOntology :: NodeSelector
+owlOntology = selector "http://www.w3.org/2002/07/owl#Ontology"
+
+rdfsLabel :: NodeSelector
+rdfsLabel = selector "http://www.w3.org/2000/01/rdf-schema#label"
+
+rdfsSubClassOf :: NodeSelector
+rdfsSubClassOf = selector "http://www.w3.org/2000/01/rdf-schema#subClassOf"
+
+skosDefinition :: NodeSelector
+skosDefinition = selector "http://www.w3.org/2004/02/skos/core#definition"
+
+rdfType :: NodeSelector
+rdfType = selector "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
