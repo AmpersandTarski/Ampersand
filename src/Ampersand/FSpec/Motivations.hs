5fef2a67ea5f969ffd0e3293f99fbe07cc465f3a
Han Joosten <han.joosten.han@gmail.com>
2025-07-17 16:50:00 +0200

    unused  LANGUAGE pragma

--- src/Ampersand/FSpec/Motivations.hs
+++ src/Ampersand/FSpec/Motivations.hs
@@ -1,6 +1,5 @@
 -- TODO -> Maybe this module is useful at more places than just func spec rendering.
 --        In that case it's not a Rendering module and it needs to be replaced
-{-# LANGUAGE InstanceSigs #-}
 
 module Ampersand.FSpec.Motivations
   ( Motivated (purposesOf),
